34ee6c28f147eba19e80b129bcc046e85b6e65b0
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2002-11-24 02:23:30 +0000

    - temporary save

---
+++ modules/netinfo.php
@@ -0,0 +1,36 @@
+<? // $Id$
+
+/*
+ * LMS version 1.0
+ *
+ *  (C) Copyright 2002 Rulez.PL Development Team
+ *  (C) Copyright 2001-2002 NetX ACN
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$network = $LMS->GetNetworkRecord($_GET[id]);
+
+$layout[pagetitle]="Informacja o sieci";
+$SMARTY->assign("layout",$layout);
+$SMARTY->assign("network",$network);
+$SMARTY->display("header.html");
+$SMARTY->display("netinfo.html");
+$SMARTY->display("footer.html");
+?><pre><? print_r($network); ?>
