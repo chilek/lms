9ebf81940929342f05a629cc2bc9f6b6afbd00bc
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2002-11-29 19:22:07 +0000

    - :D

---
+++ lib/TipOfTheDay.php
@@ -0,0 +1,82 @@
+<?
+
+/*
+ * LMS version 1.0
+ *
+ *  (C) Copyright 2002 Rulez.PL Development Team
+ *  (C) Copyright 2001-2002 NetX ACN
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$tipoftheday[] = "Mo¿esz siê pokaleczyæ wymachuj±c siekier±?";
+$tipoftheday[] = "Komputer nie pod³±czony do ¼ród³a zasilania zdecydowanie odmówi dzia³ania.";
+$tipoftheday[] = "Do¶wiadczenie to co¶, co zdobywasz tu¿ po chwili w której go potrzebowa³e¶.";
+$tipoftheday[] = "Najlepsz± metod± do zainspirowania odkrywczych my¶li, jest zaklejenie koperty.";
+$tipoftheday[] = "Wszystko zabiera znacznie wiêcej czasu, ni¿ by siê wydawa³o.";
+$tipoftheday[] = "Zawsze kiedy masz w³a¶nie co¶ zrobiæ, okazuje siê, ¿e najpierw musisz zrobiæ co¶ innego.";
+$tipoftheday[] = "Ka¿de rozwi±zanie rodzi nowe problemy.";
+$tipoftheday[] = "G³upcy s± tak pomys³owi, ¿e niemo¿liwe jest stworzenie czego¶, z czym ka¿dy g³upi sobie poradzi.";
+$tipoftheday[] = "Matka Natura jest suk±.";
+$tipoftheday[] = "Rzeczy ulegaj± zniszczeniu, wprost proporcjonalnie do swej warto¶ci.";
+$tipoftheday[] = "Wszystkie sprawy szlag trafia jednocze¶nie.";
+$tipoftheday[] = "Je¿eli wydaje ci siê, ¿e ju¿ gorzej byæ nie mo¿e - na pewno bêdzie.";
+$tipoftheday[] = "Zawsze, kiedy wydaje ci siê, ¿e bêdzie dobrze - kompletnie siê mylisz.";
+$tipoftheday[] = "Zawsze, kiedy wydaje ci siê, ¿e bêdzie ¼le te¿ siê mylisz - bêdzie jeszcze gorzej.";
+$tipoftheday[] = "Bardzo du¿o mo¿na zauwa¿yæ, gdy siê patrzy.";
+$tipoftheday[] = "Je¶li co¶ siê wydarza to znaczy, ¿e jest to mo¿liwe. Zatem w walce miêdzy tob±, a ¶wiatem, stañ po stronie ¶wiata.";
+$tipoftheday[] = "W poszukiwaniu rozwi±zania problemu, najbardziej pomocna jest znajomo¶æ odpowiedzi.";
+$tipoftheday[] = "Ka¿dy przedmiot niezale¿nie od po³o¿enia, mo¿e w dowolnej chwili zadzia³aæ w zupe³nie nieoczekiwany sposób z przyczyn, które s± albo ca³kowicie niejasne, albo zupe³nie tajemnicze.";
+$tipoftheday[] = "Wymiary bêd± zawsze podane w najmniej u¿ytecznych jednostkach.";
+$tipoftheday[] = "Ka¿dy przewód przyciêty na d³ugo¶æ, oka¿e siê za krótki.";
+$tipoftheday[] = "Stopieñ g³upoty twojego postêpowania, jest wprost proporcjonalny do liczby przygl±daj±cych ci siê osób.";
+$tipoftheday[] = "Prawdopodobieñstwo wyczerpania siê baterii, jest wprost-proporcjonalne do zapotrzebowania na kalkulator.";
+$tipoftheday[] = "Ciê¿ar upuszczonej czê¶ci jest wprost proporcjonalny do ceny obszaru uderzonego.";
+$tipoftheday[] = "To czego szukasz, znajdziesz w ostatnim spo¶ród mo¿liwych miejsc.";
+$tipoftheday[] = "Wniosek to punkt, w którym nie masz ju¿ si³y dalej my¶leæ.";
+$tipoftheday[] = "To co mo¿e siê nie udaæ, nie uda siê na pewno.";
+$tipoftheday[] = "Cztery zjawiska blokuj± postêp ludzko¶ci - ignorancja, g³upota, komitety normalizacyjne, oraz ludzie sprzedaj±cy komputery.";
+$tipoftheday[] = "Istniej± trzy rodzaje k³amstw - k³amstwa odruchowe, powszechne oraz testy komputerowe.";
+$tipoftheday[] = "Cz³owiek pracuj±cy z komputerem, nie postêpuje zgodnie z wymaganiami komputera.";
+$tipoftheday[] = "Systemy odporne na idiotów, obs³ugiwane s± w³a¶nie przez nich.";
+$tipoftheday[] = "Roz³o¿enie dowolnego urz±dzenia na czê¶ci jest proste, ponowne jego z³o¿enie, tak ¿eby dzia³a³o, jest niemo¿liwe.";
+$tipoftheday[] = "Ka¿dy skomplikowany monta¿ wymaga trzech r±k.";
+$tipoftheday[] = "Ka¿dy prosty monta¿ wymaga czterech r±k.";
+$tipoftheday[] = "Sprawy pozostawione same sobie, zmieniaj± siê ze z³ych na jeszcze gorsze.";
+$tipoftheday[] = "Wszystkie komputery PC s± kompatybilne, ale niektóre s± kompatybilniejsze od innych. Twój jest zawsze mniej kompatybilny.";
+$tipoftheday[] = "Pust± dyskietkê mo¿esz przeszukiwaæ d³ugo i bezskutecznie. D³ugo i bezskutecznie mo¿esz przeszukiwaæ tak¿e pe³n± dyskietkê. Wniosek - ka¿d± dyskietkê mo¿esz przeszukiwaæ d³ugo i bezskutecznie.";
+$tipoftheday[] = "Komputer s³u¿y do tego aby u³atwiæ ci pracê, której bez niego w ogóle by¶ nie mia³.";
+$tipoftheday[] = "Dla komputera nie ma rzeczy niemo¿liwych z wyj±tkiem tych, których od niego wymagamy.";
+$tipoftheday[] = "W ¶wiecie komputerów ¿adna awaria siê nie koñczy, poniewa¿ przechodzi zawsze w nastêpn±.";
+$tipoftheday[] = "Awaria komputera wyczekuje cierpliwie na najbardziej niedogodny moment, aby bezlito¶nie zaatakowaæ.";
+$tipoftheday[] = "W pracy z komputerem, opieranie siê na jakichkolwiek zasadach jest b³êdem.";
+$tipoftheday[] = "Drobne awarie nie isniej±, je¿eli mia³a jednak miejsce drobna awaria, oznacza to, ¿e nie pozna³e¶ jeszcze jej rzeczywistych rozmiarów.";
+$tipoftheday[] = "Im doskonalej program wykonuje swoje funkcje, tym dotkliwiej ciê zawiedzie. Ka¿dy inny program zawodzi tak samo dotkliwie. Tylko niepotrzebny nikomu program, dzia³a bez zak³óceñ.";
+$tipoftheday[] = "Masz zawsze o jeden wirus wiêcej ni¿ s±dzisz.";
+$tipoftheday[] = "Program kalkulacyjny, który daje w³a¶ciwy i przyjazny wynik - k³amie.";
+$tipoftheday[] = "Wyj±tki s± liczniejsze od regu³. Od wszystkich uznanych wyj±tków isniej± wyj±tki.";
+$tipoftheday[] = "Klientowi nigdy nie przyjdzie na my¶l ile kosztuje projekt, tylko ile mo¿na na tym projekcie zaoszczêdziæ. 
+Je¿eli uda³o ci siê wprowadziæ w programie wymagane przez klienta poprawki, wtedy on z nich zrezygnuje. ¯aden klient nie wie czego w³a¶ciwie chce. Ka¿dy klient wie dok³adnie czego nie chce. ¯aden klient nie chce tego co masz ju¿ gotowe. Nie wie tak¿e co chcia³by mieæ zamiast tego. Klient, który najmniej p³aci, marudzi najwiêcej. Klient ¿±da najwiêkszych zmian dok³adnie wtedy, kiedy produkt jest ju¿ gotowy.";
+$tipoftheday[] = "Na polecenie \"napisz tak lub nie\" u¿ytkownik wpisuje \"tak lub nie\". Na polecenie \"wci¶nij Enter\" u¿ytkownik wpisuje \"Enter\". Na polecenie \"naci¶nij dowolny klawisz\", u¿ytkownik naciska na klawisz \"reset\". Wniosek: Nie ma programów ca³kowicie idiotoodpornych.";
+$tipoftheday[] = "W zespole programistów ka¿dy ma genialny plan rozwi±zania problemu. Wszystkie plany siê wzajemnie wykluczaj± i ¿aden nie prowadzi do poprawnego rozwi±zania. Liczba osób w zespole programistycznym ma tendencje wzrastaj±ce, niezale¿nie od ilo¶ci pracy.";
+
+mt_srand ((double) microtime()* 100000000);
+$layout[tip] = $tipoftheday[mt_rand(0,sizeof($tipoftheday)-1)];
+
+
