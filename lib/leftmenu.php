48b1ee088d2d42eab7d496890a8b65f7ea8503d2
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2003-04-14 11:30:01 +0000

    - fixing problems with Smarty-2.5.0 (thanks for messju mohr)

--- lib/leftmenu.php
+++ lib/leftmenu.php
@@ -38,25 +38,25 @@ $menu[name]	[] = "U¿ytkownicy";
 $menu[img]	[] = "user.gif";
 $menu[link]	[] = "?m=userlist";
 $menu[accesskey][] = "u";
-$menu[tip]	[] = "<B>U¿ytkownicy: lista, wyszukiwanie, dodanie nowego</B>";
+$menu[tip]	[] = "U¿ytkownicy: lista, wyszukiwanie, dodanie nowego";
 
 $menu[name]	[] = "Komputery";
 $menu[img]	[] = "node.gif";
 $menu[link]	[] = "?m=nodelist";
 $menu[accesskey][] = "k";
-$menu[tip]	[] = "<B>Komputery: lista, wyszukiwanie, dodawanie</B>";
+$menu[tip]	[] = "Komputery: lista, wyszukiwanie, dodawanie";
 
 $menu[name]	[] = "Sieci IP";
 $menu[img]	[] = "ip.gif";
 $menu[link]	[] = "?m=netlist";
 $menu[accesskey][] = "s";
-$menu[tip]	[] = "<B>Zarz±dzanie klasami adresowymi IP</B>";
+$menu[tip]	[] = "Zarz±dzanie klasami adresowymi IP";
 
 $menu[name]	[] = "Taryfy i finanse";
 $menu[img]	[] = "money.gif";
 $menu[link]	[] = "?m=tarifflist";
 $menu[accesskey][] = "t";
-$menu[tip]	[] = "<B>Zarz±dzanie taryfami oraz finansami sieci</B>";
+$menu[tip]	[] = "Zarz±dzanie taryfami oraz finansami sieci";
 
 $menu[name]	[] = "Mailing";
 $menu[img]	[] = "mail.gif";
@@ -74,12 +74,12 @@ $menu[name]	[] = "Bazy danych";
 $menu[img]	[] = "db.gif";
 $menu[link]	[] = "?m=dblist";
 $menu[accesskey][] = "b";
-$menu[tip]	[] = "<B>Zarz±dzanie kopiami zapasowymi bazy danych</B>";
+$menu[tip]	[] = "Zarz±dzanie kopiami zapasowymi bazy danych";
 
 $menu[name]	[] = "Administratorzy";
 $menu[img]	[] = "admins.gif";
 $menu[link]     [] = "?m=adminlist";
 $menu[accesskey][] = "d";
-$menu[tip]	[] = "<B>Konta administratorów systemu</B>";
+$menu[tip]	[] = "Konta administratorów systemu";
 
 ?>
