8656ebd83de3a1edaa218ab8bc9a341372d09a75
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2003-04-21 23:36:26 +0000

    - fix: dodanie bookmarka w powrotnym redirectie

--- modules/nodeset.php
+++ modules/nodeset.php
@@ -3,12 +3,14 @@
 	if($LMS->UserExists($_GET[ownerid]))
 	{
 		$LMS->NodeSetU($_GET[ownerid],$_GET[access]);
+		$backid = $_GET[ownerid];
 	}
 	if($LMS->NodeExists($_GET[id]))
 	{
 		$LMS->NodeSet($_GET[id]);
+		$backid = $_GET[id];
 	}
 
-	header("Location: ?".$_SESSION[backto]."#".$_GET[ownerid]);
+	header("Location: ?".$_SESSION[backto]."#".$backid);
 
 ?>
