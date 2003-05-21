c29e32fd8eee152d9ca7b38002f953258da3fc74
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2003-05-21 12:05:32 +0000

    - new

---
+++ devel/cvs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# $Id$
+#
+# Pokazuje przez less'a diffy wszystkich plików które zmienione s± i je commituje
+#
+
+export CVSEDITOR="vim"
+rm -v `find .|grep bak$`
+rm -v `find .|grep "~$"`
+for i in `cvs up|grep ^M|cut -d\  -f2-`
+do
+	cvs diff -u $i|less
+	cvs ci $i
+done
