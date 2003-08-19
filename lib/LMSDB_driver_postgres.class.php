4ab79b5ff3b89f85d55f33fa61c9957256d82ccf
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2003-08-19 01:00:13 +0000

    - untested driver for pgsql

---
+++ lib/LMSDB_driver_postgres.class.php
@@ -0,0 +1,106 @@
+<?
+
+/*
+ * LMS version 1.1-cvs
+ *
+ *  (C) Copyright 2001-2003 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+/*
+ * To jest pseudo-driver dla LMSDB, dla bazy danych 'postgres'.
+ */
+
+class LMSDB_driver_postgres extends LMSDB_common
+{
+	var $_loaded = TRUE;
+	var $_dbtype = 'postgres';
+
+	function LMSDB_driver_postgres($dbhost,$dbuser,$dbpasswd,$dbname)
+	{
+		$this->_version .= ' (core: '.eregi_replace('^.Revision: ([0-9.]+).*','\1',$this->_revision).' / driver: '.$this->_dbtype.' '.eregi_replace('^.Revision: ([0-9.]+).*','\1','$Revision$').')';
+		$this->Connect($dbhost,$dbuser,$dbpasswd,$dbname);
+	}
+
+	function _driver_connect($dbhost,$dbuser,$dbpasswd,$dbname)
+	{
+		if($this->_dblink = pg_connect('host='.$dbhost.' user='.$dbuser.' password='.$dbpasswd.' dbname='.$dbname))
+		{
+			$this->_dbhost = $dbhost;
+			$this->_dbuser = $dbuser;
+			$this->_dbname = $dbname;
+		}
+		return $this->_dblink;
+	}
+	
+	function _driver_execute($query)
+	{
+		if($this->_result = pg_query($this->_dblink,$query))
+		{
+			$this->_query = $query;
+			$this->_error = FALSE;
+		}else
+			$this->_error = pg_last_error($this->_dblink);
+
+		return $this->_result;
+	}
+
+	function _driver_fetchrow_assoc()
+	{
+		if(! $this->_error)
+			return pg_fetch_array($this->_result,PGSQL_ASSOC);
+		else
+			return FALSE;
+	}
+
+	function _driver_fetchrow_num()
+	{
+		if(! $this->_error)
+			return pg_fetch_array($this->_result,PGSQL_NUM);
+		else
+			return FALSE;
+	}
+
+	function _driver_now()
+	{
+		return 'EXTRACT(EPOCH FROM CURRENT_TIMESTAMP(0))';
+	}
+
+	function _driver_like()
+	{
+		return 'ILIKE';
+	}
+
+	function _driver_concat($input)
+	{
+		$return = implode('+',$input);
+		return $return;
+	}
+		
+}
+
+/* 
+ * $Log$
+ * Revision 1.1  2003/08/19 01:00:13  lukasz
+ * - untested driver for pgsql
+ *
+ */
+
+?>
