23de12ea3f0ee7d8b5cb79f00d9ccfcabf9e332a
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2003-09-05 02:07:04 +0000

    - massive attack: s/this->ADB->/this->DB->/g

--- lib/Session.class.php
+++ lib/Session.class.php
@@ -37,18 +37,18 @@ class Session {
 	var $error;
 	var $_version = NULL;
 
-	function Session($ADB,$timeout = 600)
+	function Session($DB,$timeout = 600)
 	{
 		$this->_version = eregi_replace('^.Revision: ([0-9.]+).*','\1','$Revision$');
 		session_start();
-		$this->ADB = $ADB;
+		$this->DB = $DB;
 		$loginform = $_POST[loginform];
 		if(isset($loginform)){
 			$this->login = $loginform[login];
 			$this->passwd = $loginform[pwd];
 			$_SESSION[session_timestamp] = time();
 			writesyslog("Login attempt by ".$this->login,LOG_INFO);
-		}elseif($this->ADB->GetOne('SELECT COUNT(id) FROM admins') == 0){
+		}elseif($this->DB->GetOne('SELECT COUNT(id) FROM admins') == 0){
 			$this->islogged = TRUE;
 			$this->passwd = 'EMPTY';
 			$this->logname = 'nie zalogowany';
@@ -61,18 +61,18 @@ class Session {
 		
 		if($this->VerifyPassword()&&$this->TimeOut($timeout)){
 			$this->islogged = TRUE;
-			$admindata = $this->ADB->GetRow("SELECT id, name FROM admins WHERE login=?",array($this->login));
+			$admindata = $this->DB->GetRow("SELECT id, name FROM admins WHERE login=?",array($this->login));
 			$this->id = $admindata[id];
 			$this->logname = $admindata[name];
 			$this->last = $_SESSION[session_last];
 			$this->lastip = $_SESSION[session_lastip];
 			if(isset($loginform))
 			{
-				$admindata = $this->ADB->GetRow("SELECT lastlogindate, lastloginip FROM admins WHERE id=?",array($this->id));
+				$admindata = $this->DB->GetRow("SELECT lastlogindate, lastloginip FROM admins WHERE id=?",array($this->id));
 				$this->last = $admindata[lastlogindate];
 				$this->lastip = $admindata[lastloginip];
 				
-				$this->ADB->Execute("UPDATE admins SET lastlogindate=?, lastloginip=? WHERE id=?",array(time(),$_SERVER[REMOTE_ADDR],$this->id));
+				$this->DB->Execute("UPDATE admins SET lastlogindate=?, lastloginip=? WHERE id=?",array(time(),$_SERVER[REMOTE_ADDR],$this->id));
 				writesyslog("User ".$this->login." logged in",LOG_INFO);
 			}
 			$_SESSION[session_login] = $this->login;
@@ -84,7 +84,7 @@ class Session {
 			if(isset($loginform))
 			{
 				writesyslog("Bad password for ".$this->login,LOG_WARNING);
-				$this->ADB->Execute("UPDATE admins SET failedlogindate=?, failedloginip=? WHERE login=?",array(time(),$_SERVER[REMOTE_ADDR],$this->login));
+				$this->DB->Execute("UPDATE admins SET failedlogindate=?, failedloginip=? WHERE login=?",array(time(),$_SERVER[REMOTE_ADDR],$this->login));
 			}
 			$this->LogOut();
 		}
@@ -116,8 +116,8 @@ class Session {
 	
 	function VerifyPassword()
 	{
-		$dbpasswd = $this->ADB->GetOne("SELECT passwd FROM admins WHERE login=?",array($this->login));
-		$dblogin = $this->ADB->GetOne("SELECT login FROM admins WHERE login=?",array($this->login));
+		$dbpasswd = $this->DB->GetOne("SELECT passwd FROM admins WHERE login=?",array($this->login));
+		$dblogin = $this->DB->GetOne("SELECT login FROM admins WHERE login=?",array($this->login));
 		if (crypt($this->passwd,$dbpasswd)==$dbpasswd)
 			return TRUE;
 		else 
@@ -133,6 +133,9 @@ class Session {
 
 /*
  * $Log$
+ * Revision 1.38  2003/09/05 02:07:05  lukasz
+ * - massive attack: s/this->ADB->/this->DB->/g
+ *
  * Revision 1.37  2003/08/24 13:12:54  lukasz
  * - massive attack: s/<?/<?php/g - that was causing problems on some fucked
  *   redhat's :>
