4778dca76d5da7a7fa8db2f2137d798629fa04d2
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2003-09-08 03:12:22 +0000

    - dodane {sum array=$array column="columnname" default="default value"}

---
+++ lib/smarty_addons.php
@@ -0,0 +1,50 @@
+<?
+
+/*
+ * LMS version 1.1-cvs
+ *
+ *  (C) Copyright 2001-2003 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+// Funkcje dodatkowe, do roszerzenia Smarty.
+
+function sum($args, $SMARTY)
+{
+	$array = $args['array'];
+	$column = $args['column'];
+	$default = (isset($args['default']) ? $args['default'] : 0);
+	if($array)
+		foreach($array as $row)
+			$result += $row[$column];
+	return (isset($result) ? $result : $default);
+}
+
+$SMARTY->register_function('sum','sum');
+
+/*
+ * $Log$
+ * Revision 1.1  2003/09/08 03:12:22  lukasz
+ * - dodane {sum array=$array column="columnname" default="default value"}
+ *
+ *
+ */
+
+?>
