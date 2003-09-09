752964dc05f49cbd30feb0d4c336d393158cb4b8
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2003-09-09 01:26:38 +0000

    - new file

---
+++ modules/upgrade01.php
@@ -0,0 +1,39 @@
+<?
+
+/*
+ * LMS version 1.1-cvs
+ *
+ *  (C) Copyright 2001-2003 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$SMARTY->display('header.html');
+?><PRE><B>Upgrade bazy danych: zmiana formatu finansów:</B>
+<?
+
+$users = $DB->GetAll('SELECT * FROM users');
+$tariffs = $DB->GetAllByKey('SELECT * FROM tariffs','id');
+foreach($users as $idx => $row)
+{
+	echo $row[lastname]." ".$row[name].": ";
+	echo "taryfa: '".$tariffs[$row[tariff]][name]."'... ";
+	$LMS->AddAssignment(array('tariffid' => $row[tariff], 'at' => $row[payday], 'userid' => $row[id], 'peroid' => 0));
+	echo "ok.\n";
+}
