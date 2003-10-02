ca800f1361d335da9a52f38ba2a0b1127e3c3ea0
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2003-10-02 22:00:49 +0000

    - z doc/sgml

---
+++ devel/runme.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Skrypt s³u¿y do tworzenia dokumentacji w ró¿nych formatach
+# (html, txt) na podstawie ¼ród³owych plików sgml
+# Przed u¿yciem nale¿y zainstalowaæ w systemie sgml-tools
+# z openjade oraz elinks (tylko dla formatu txt)
+#
+
+cd doc/sgml
+
+case "$1" in
+
+    'html')	####### sgml -> html #######################
+        jade -t sgml -d lms.dsl index.sgml
+	if [ $? -ne 0 ]
+	    then exit 1
+	fi
+	mv ./*.html ../html/
+	exit 0
+    ;;
+
+    'txt')	####### sgml -> text #######################
+	jade -t sgml -V nochunks -d lms.dsl index.sgml > index.html
+	if [ $? -ne 0 ]
+	    then exit 1
+	fi
+	elinks -dump index.html > ../txt/manual
+        if [$? -ne 0 ]
+	    then
+	    rm index.html
+	    exit 2
+	fi
+	rm index.html
+	exit 0
+    ;;
+    
+    *)
+	echo -e "runme.sh: Brak parametru.\nSposób u¿ycia: runme.sh html|txt"
+    ;;
+esac
