0bf02103befe837e3d8bec7a8ab79f78dfddd701
Marcin Król <lexx@polarnet.gliwice.pl>
2003-10-06 18:44:18 +0000

    - stare i dzialajace

--- modules/faktura.php
+++ modules/faktura.php
@@ -79,92 +79,58 @@ function slownie($liczba) {
  if (($liczba>0) or (strlen($slow)<1)) {$slow .=$z[$liczba];}
  return $slow;
 }
-	   
+
 $cash=$LMS->GetCashByID($_GET[id]);
 $userinfo=$LMS->GetUser($cash['userid']);
 
-// Zczytujemy plik
-$plik = fopen ($_CONFIG[finances]['template'],"r");
-while (!feof ($plik)){   
-    $szablon .= fgets ($plik, 1500);
-}
-fclose($plik);
+if (! $szablon = @file($_CONFIG[finances]['template'])) { echo "Nie umiem odczytac szablonu, ustawiles wszystko w lms.ini?"; die; }; 
 
-$roboczy = $szablon;
+if(substr($_CONFIG[finances]['template'],-4)==".rtf"){  
+    header('Content-type: text/rtf');
+    header('Content-Disposition: attachment; filename=faktura_'.$userinfo['lastname']."_".$_GET[id]."_".date("Y",$cash['time']).'.rtf');
+    $trans = array("¡"=>"\'a5","Æ"=>"\'c6","Ê"=>"\'ca","£"=>"\'a3","Ñ"=>"\'d1","Ó"=>"\'d3","¦"=>"\'8c","¯"=>"\'af","¬"=>"\'8f","±"=>"\'b9","æ"=>"\'e6","ê"=>"\'ea","³"=>"\'b3","ñ"=>"\'f1","ó"=>"\'f3","¶"=>"\'9c","¿"=>"\'bf","¼"=>"\'9f");
+} else $trans = array();    
 
-$trans = array();    
-
-// Najpierw rozwi±¿emy sobie rzeczy które s± takie same w ca³ej fakturze
-
-$roboczy = str_replace('%nabywca',strtr($userinfo['username'],$trans),$roboczy);
-$roboczy = str_replace('%nab_adres_cd',strtr($userinfo['zip']." ".$userinfo['city'],$trans),$roboczy);
-$roboczy = str_replace('%nab_adres',strtr($userinfo['address'],$trans),$roboczy);
-$roboczy = str_replace('%nip',strtr($userinfo['nip'],$trans),$roboczy);
-$roboczy = str_replace('%nr_klienta',strtr($_GET[id],$trans),$roboczy);
-$roboczy = str_replace('%sprzedawca',strtr($_CONFIG[finances]['name'],$trans),$roboczy);
-$roboczy = str_replace('%sprzed_adres_cd',strtr($_CONFIG[finances]['zip']." ".$_CONFIG[finances]['city'],$trans),$roboczy);
-$roboczy = str_replace('%sprzed_adres',strtr($_CONFIG[finances]['address'],$trans),$roboczy);
-$roboczy = str_replace('%numer',strtr($_GET[id]."/".date("Y",$cash['time']),$trans),$roboczy);
-$roboczy = str_replace('%data',strtr(date("d.m.Y"),$trans),$roboczy);
-$roboczy = str_replace('%termin',strtr(date("d.m.Y",mktime(0,0,0,date("m"),date("d")+$_CONFIG[finances]['deadline'],date("Y"))),$trans),$roboczy);
-$roboczy = str_replace('%dni',strtr($_CONFIG[finances]['deadline'],$trans),$roboczy);
-$roboczy = str_replace('%konto',strtr($_CONFIG[finances]['bank']." ".$_CONFIG[finances]['account'],$trans),$roboczy);
-$roboczy = str_replace('%wystawil',strtr($layout['logname'],$trans),$roboczy);
-$roboczy = str_replace('%stopka',strtr($_CONFIG[finances]['footer'],$trans),$roboczy);
-$roboczy = str_replace('%vat','7%',$roboczy);
-
-// A teraz potniemy na kawa³ki i zmienimy jedynie to co w tabelce
-
-$kawalki = explode('<!-- body -->',$roboczy);
-$head=$kawalki[0];
-$kawalki = explode('<!-- table -->',$kawalki[1]);
-$body1=$kawalki[0];
-$kawalki = explode('<!-- /table -->',$kawalki[1]);
-$table=$kawalki[0];
-$kawalki = explode('<!-- break -->',$kawalki[1]);
-$body2=$kawalki[0];
-$kawalki = explode('<!-- tail -->',$kawalki[1]);
-$break=$kawalki[0];
-$tail=$kawalki[1];
-
-echo $head;
-echo $body1;
-
-$line=1;
-foreach($LMS->ADB->GetAll("SELECT * FROM `cash` WHERE `userid`=".$cash['userid']) as $row) {
-    $roboczy = $table;
-    $roboczy = str_replace('%nr',$line,$roboczy);
-    $roboczy = str_replace('%usluga',strtr($row['comment'],$trans),$roboczy);
-    $roboczy = str_replace('%okres',strtr(strftime("01-%b-%Y",$row['time']).' - '.strftime("%d-%b-%Y",mktime(0,0,0,date("m",$row['time'])+1,0,date("Y",$row['time']))),$trans),$roboczy);
-    $roboczy = str_replace('%netto',sprintf("%1.2f",$row['value']),$roboczy);
-    $roboczy = str_replace('%wvat',sprintf("%1.2f",$row['value']*0.07),$roboczy);
-    $roboczy = str_replace('%brutto',sprintf("%1.2f",$row['value']*1.07),$roboczy);
-    $line++;
-    $sumnetto=$sumnetto+$row['value'];
-    echo $roboczy;
-}
-
-$body2 = str_replace('%sumnetto',sprintf("%1.2f",$sumnetto),$body2);
-$body2 = str_replace('%sumwvat',sprintf("%1.2f",$sumnetto*0.07),$body2);
-$body2 = str_replace('%sumbrutto',sprintf("%1.2f",$sumnetto*1.07),$body2);
-$kesz=explode(".",sprintf("%1.2f",$sumnetto*1.07));
+$szablon = str_replace('%nabywca',strtr($userinfo['username'],$trans),$szablon);
+$szablon = str_replace('%nab_adres_cd',strtr($userinfo['zip']." ".$userinfo['city'],$trans),$szablon);
+$szablon = str_replace('%nab_adres',strtr($userinfo['address'],$trans),$szablon);
+$szablon = str_replace('%nip',strtr($userinfo['nip'],$trans),$szablon);
+$szablon = str_replace('%nr_klienta',strtr($_GET[id],$trans),$szablon);
+$szablon = str_replace('%sprzedawca',strtr($_CONFIG[finances]['name'],$trans),$szablon);
+$szablon = str_replace('%sprzed_adres_cd',strtr($_CONFIG[finances]['zip']." ".$_CONFIG[finances]['city'],$trans),$szablon);
+$szablon = str_replace('%sprzed_adres',strtr($_CONFIG[finances]['address'],$trans),$szablon);
+$szablon = str_replace('%numer',strtr($_GET[id]."/".date("Y",$cash['time']),$trans),$szablon);
+$szablon = str_replace('%data',strtr(date("d.m.Y"),$trans),$szablon);
+$szablon = str_replace('%termin',strtr(date("d.m.Y",mktime(0,0,0,date("m"),date("d")+$_CONFIG[finances]['deadline'],date("Y"))),$trans),$szablon);
+$szablon = str_replace('%dni',strtr($_CONFIG[finances]['deadline'],$trans),$szablon);
+$szablon = str_replace('%usluga',strtr($cash['comment'],$trans),$szablon);
+$szablon = str_replace('%od',strtr(strftime("01-%b-%Y",$cash['time']),$trans),$szablon);
+$szablon = str_replace('%do',strtr(strftime("%d-%b-%Y",mktime(0,0,0,date("m",$cash['time'])+1,0,date("Y",$cash['time']))),$trans),$szablon);
+$szablon = str_replace('%netto',strtr(sprintf("%1.2f",$cash['value']),$trans),$szablon);
+$szablon = str_replace('%brutto',strtr(sprintf("%1.2f",$cash['value']*1.07),$trans),$szablon);
+$szablon = str_replace('%vat',strtr(sprintf("%1.2f",$cash['value']*0.07),$trans),$szablon);
+$kesz=explode(".",sprintf("%1.2f",$cash['value']*1.07));
 if ($kesz[1]>0) {
-    $body2 = str_replace('%slownie',strtr(slownie($kesz[0])." z³ ".$kesz[1]." gr",$trans),$body2);
+    $szablon = str_replace('%slownie',strtr(slownie($kesz[0])." z³ ".$kesz[1]." gr",$trans),$szablon);
 } else {
-    $body2 = str_replace('%slownie',strtr(slownie($kesz[0])." z³",$trans),$body2);
+    $szablon = str_replace('%slownie',strtr(slownie($kesz[0])." z³",$trans),$szablon);
 }			    
+$szablon = str_replace('%konto',strtr($_CONFIG[finances]['bank']." ".$_CONFIG[finances]['account'],$trans),$szablon);
+$szablon = str_replace('%wystawil',strtr($layout['logname'],$trans),$szablon);
+$szablon = str_replace('%stopka',strtr($_CONFIG[finances]['footer'],$trans),$szablon);
 
-
-echo $body2;
-echo $tail;
+while (list ($line_num, $line) = each ($szablon)) {
+    echo $line;
+}
 
 /*
  * $Log$
- * Revision 1.7  2003/09/21 18:07:47  lexx
- * - netdev
+ * Revision 1.8  2003/10/06 18:44:18  lexx
+ * - stare i dzialajace
  *
  * Revision 1.6  2003/08/18 16:52:19  lukasz
  * - added CVS Log tags
  *
  */
 ?>
+
