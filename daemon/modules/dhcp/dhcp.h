34baaf0a2debf41de7a23fcbdaf5a555d8ca4507
Aleksander Machniak <alec@alec.pl>
2003-10-27 20:57:02 +0000

    new module

---
+++ daemon/modules/dhcp/dhcp.h
@@ -0,0 +1,18 @@
+
+struct dhcp_module
+{
+	MODULE base;
+
+	unsigned char * prefix;
+	unsigned char * append;
+	unsigned char * subnetstart;
+	unsigned char * subnetend;
+	unsigned char * gateline;
+	unsigned char * dnsline;
+	unsigned char * domainline;
+	unsigned char * winsline;
+	unsigned char * rangeline;
+	unsigned char * host;
+	unsigned char * tmpfile;
+	unsigned char * command;
+};
