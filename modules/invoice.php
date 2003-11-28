e6cbd22b565915db8f1ecd1902d8121c681b3f29
Aleksander Machniak <alec@alec.pl>
2003-11-28 21:40:48 +0000

    co wy na to?

--- modules/invoice.php
+++ modules/invoice.php
@@ -24,9 +24,26 @@
  *  $Id$
  */
 
-$layout[pagetitle] = 'Faktura nr '.$_GET[id].'/LMS/'.$_GET[year];
+$layout[pagetitle] = 'Faktura VAT nr '.$_GET[id].'/LMS/'.$_GET[year];
 
 $invoice = $LMS->GetInvoiceContent($_GET[id],$_GET[year]);
+
+$invoice[content][vat] = $invoice[content][value] * 0.07;
+$invoice[content][brutto] = $invoice[content][vat] + $invoice[content][value];
+
+$kesz = explode(".",sprintf("%1.2f",$invoice[content][brutto]));
+$invoice[content][text] = $LMS->NumberSpell($kesz[0])." ".$kesz[1]."/100 z³otych";
+
+$invoice[provider][name] 	= $_CONFIG[finances][name];
+$invoice[provider][address] 	= $_CONFIG[financesces][address];
+$invoice[provider][zip] 	= $_CONFIG[finances][zip];
+$invoice[provider][city] 	= $_CONFIG[finances][city];
+$invoice[provider][footer] 	= $_CONFIG[finances][footer];
+$invoice[provider][bank] 	= $_CONFIG[finances][bank];
+$invoice[provider][account] 	= $_CONFIG[finances][account];
+$invoice[deadline] 		= $_CONFIG[finances][deadline];
+
+setlocale(LC_ALL, "pl_PL");
 
 $SMARTY->assign('layout',$layout);
 $SMARTY->assign('invoice',$invoice);
