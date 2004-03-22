b9c0457a3fd8ab8a1ac059ba1aed543ba1198cb7
Aleksander Machniak <alec@alec.pl>
2004-03-22 09:28:30 +0000

    - cleanup

--- daemon/modules/hostfile/hostfile.c
+++ daemon/modules/hostfile/hostfile.c
@@ -45,7 +45,7 @@ void reload(GLOBAL *g, struct hostfile_module *hm)
 	while( n>1 ) {
 		
 		n = sscanf(netnames, "%s %[a-zA-Z0-9- ]", netname, netnames);
-printf("%s-%s\n",netname, netnames);
+
 		if( strlen(netname) ) {
 
 			if( res = g->db_pquery("SELECT name, domain, address, INET_ATON(mask) AS mask FROM networks WHERE UPPER(name)=UPPER('?')",netname)) {
@@ -64,24 +64,7 @@ printf("%s-%s\n",netname, netnames);
 		}
 	}
 	free(netname); free(netnames);
-/*
-	for( netname=strtok(hm->networks," "); netname!=NULL; netname=strtok(NULL," ") ) { 
 
-		if( (res = g->db_pquery("SELECT name, domain, address, INET_ATON(mask) AS mask FROM networks WHERE UPPER(name)=UPPER('?')",netname))!=NULL ) {
-
-			if(res->nrows) {
-			
-			    	nets = (struct net *) realloc(nets, (sizeof(struct net) * (nc+1)));
-				nets[nc].name = strdup(g->db_get_data(res,0,"name"));
-				nets[nc].domain = strdup(g->db_get_data(res,0,"domain"));
-				nets[nc].address = inet_addr(g->db_get_data(res,0,"address"));
-				nets[nc].mask = inet_addr(g->db_get_data(res,0,"mask"));
-				nc++;
-			}
-			g->db_free(res);
-		}
-	}
-*/
 	if(!nc)
 		if( (res = g->db_query("SELECT name, domain, address, INET_ATON(mask) AS mask FROM networks"))!=NULL ) {
 
