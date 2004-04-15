a03fd68a0e09aaad0352f781da0dcab6e5c7b438
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2004-04-15 06:17:44 +0000

    - AS sum w GetInvoicesList()

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -1338,7 +1338,7 @@ to mo¿na zrobiæ jednym zapytaniem, patrz ni¿ej
 	{
 		if($result = $this->DB->GetAll('SELECT id, number, cdate, customerid, name, address, zip, city, finished, SUM(value*count) AS value, COUNT(invoiceid) AS count FROM invoices LEFT JOIN invoicecontents ON invoiceid = id WHERE finished = 1 GROUP BY id, number, cdate, customerid, name, address, zip, city, finished ORDER BY cdate ASC'))
 		{
-			$inv_paid = $this->DB->GetAllByKey('SELECT invoiceid AS id, SUM(CASE type WHEN 3 THEN value ELSE -value END) FROM cash WHERE invoiceid!=0 GROUP BY invoiceid','id');
+			$inv_paid = $this->DB->GetAllByKey('SELECT invoiceid AS id, SUM(CASE type WHEN 3 THEN value ELSE -value END) AS sum FROM cash WHERE invoiceid!=0 GROUP BY invoiceid','id');
 			foreach($result as $idx => $row)
 			{
 				$result[$idx]['year'] = date('Y',$row['cdate']);
