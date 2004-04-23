46acc7c1eb2b1bed52fc8e8060b72f58cb3edc2f
Aleksander Machniak <alec@alec.pl>
2004-04-23 08:30:56 +0000

    - dodane pole deleted do tabeli admins, nie usuwamy danych administratorow

---
+++ lib/upgradedb/postgres.2004042300.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * LMS version 1.3-cvs
+ *
+ *  (C) Copyright 2001-2004 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+$DB->Execute("
+    BEGIN;
+    ALTER TABLE admins ADD deleted smallint;
+    ALTER TABLE admins ALTER deleted SET DEFAULT 0;
+    UPDATE admins SET deleted=0;
+    ALTER TABLE admins ALTER deleted SET NOT NULL;
+    UPDATE dbinfo SET keyvalue = '2004042300' WHERE keytype = 'dbversion';
+    COMMIT;
+");
+
+?>
