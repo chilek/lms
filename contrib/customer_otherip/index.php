e2ede30c4bcde6f791930025144fcd7b9c05e1a6
Krzysztof Drewicz <hunter@pld-linux.org>
2004-06-12 12:46:30 +0000

    --user balance'n'info panel for ip other than his own.

---
+++ contrib/customer_otherip/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<!--// $Id$ //-->
+<html><head>
+<meta name="GENERATOR" content="LMS">
+<meta http-equiv="Content-Language" content="pl">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2">
+<title>::: Witamy w LMS nitronet.pl :::</title>
+<STYLE type="text/css"><!--
+BODY		{ font-size: 8pt; font-family: Tahoma, Verdana, Arial, Helvetica; }
+TD		{ font-size: 8pt; font-family: Tahoma, Verdana, Arial, Helvetica; }
+TABLE		{ border-collapse: collapse; }
+A		{ text-decoration: none; }
+A:hover		{ text-decoration: underline; color: #336600; }
+P		{ font-size:  8pt; font-family: Tahoma, Verdana, Arial, Helvetica; text-align: center; }
+INPUT		{ font-size:  8pt; font-family: Tahoma, Verdana, Arial, Helvetica; border-style: solid; border-width: 1pt; font-weight: bold; background-color: #EBE4D6; }
+//-->
+</STYLE>
+</head>
+<body topmargin="0" leftmargin="0" marginheight="0" marginwidth="0" bgcolor="#EBE4D6" link="#800000" vlink="#800000" alink="#336600">
+	<TABLE WIDTH="100%" HEIGHT="100%">
+		<TR>
+			<TD>
+
+   <P>
+                                        <IMG SRC="http://lms.rulez.pl/img/logo.png" BORDER="0" ALT="[ Witamy w LMS 1.0 ;-) ]">
+                                </P>
+                                <P>
+                                <B>
+                                        &copy; 2002 Rulez Development Team - <a href="http://www.rulez.pl/" target="_blank">http://www.rulez.pl</A><BR>
+                                        &copy; 2001-2003 ASK NetX - <a href="http://www.netx.waw.pl/" target="_blank">http://www.netx.waw.pl</A><BR>
+                                        &copy; 2001-2004 LMS Developers <a href="http://lms.rulez.pl" target="_blank">lms</A>
+				
+				
+				</B>
+				</P>
+				<P><DIV ALIGN="CENTER"><CENTER>
+<H1>
+W celu sprawdzenia stanu konta, prosimy wypełnić poniższy formularz podając numer umowy oraz PIN, który przesłany został na Państwa skrzynkę e-mail.
+<!--						{if $error}<FONT COLOR="red"><B>{$error}</B></FONT><BR>{/if} -->
+							<TABLE>
+						<FORM METHOD="POST" NAME="loginform" ACTION="balanceview.php">
+								<TR>
+									<TD align="right">
+										<B>Nr Umowy:&nbsp;</B>
+									</TD>
+									<TD>
+										<INPUT TYPE="TEXT" NAME="loginform[login]" SIZE="20" ACCESSKEY="l"><BR>
+									</TD>
+								</TR>
+								<TR>
+									<TD align="right">
+										<B>PIN:&nbsp;</B>
+									</TD>
+									<TD>
+										<INPUT TYPE="PASSWORD" NAME="loginform[pwd]" SIZE="20"><BR>
+									</TD>
+								</TR>
+								<TR>
+									<TD COLSPAN="2">
+										&nbsp;
+									</TD>
+								</TR>
+                                                                        <TD COLSPAN="2" align="CENTER">
+                                                                                <INPUT TYPE="SUBMIT" NAME="loginform[submit]" VALUE="Moje Konto -- Logowanie">
+                                                                        </TD>
+								</TR>
+
+
+
+							</TABLE>
+
+				</CENTER></DIV></P>
+				<P>
+					<BR>
+					<BR>
+					<BR>
+					<A HREF="http://nitronet.pl"><FONT COLOR="#666666">strona domowa</FONT></A>
+				</p>
+			</TD>
+		</TR>
+	</TABLE>
+</body></html>
