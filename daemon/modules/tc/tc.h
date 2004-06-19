7f1fb06d83d28848393e48c49b50edc31a6fe7f9
Aleksander Machniak <alec@alec.pl>
2004-06-19 16:18:51 +0000

    - new tc module

--- daemon/modules/tc/tc.h
+++ daemon/modules/tc/tc.h
@@ -1,3 +1,17 @@
+struct net
+{
+	unsigned char *name;
+	unsigned char *domain;
+	unsigned char *interface;
+	unsigned long address;
+	unsigned long mask;
+};
+
+struct group
+{
+	unsigned char *name;
+	int id;
+};
 
 struct tc_module
 {
@@ -5,4 +19,11 @@ struct tc_module
 
 	unsigned char *file;
 	unsigned char *command;
+	unsigned char *begin;
+	unsigned char *end;
+	unsigned char *host;
+	unsigned char *host_plimit;
+	unsigned char *host_climit;
+	unsigned char *networks;
+	unsigned char *usergroups;
 };
