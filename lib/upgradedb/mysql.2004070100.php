2ae2ed8b0ee55174cda0622273083f44513c7e7e
Aleksander Machniak <alec@alec.pl>
2004-07-01 10:38:05 +0000

    - RT vel. Helpdesk - to be continued

---
+++ lib/upgradedb/mysql.2004070100.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * LMS version 1.3-cvs
+ *
+ *  (C) Copyright 2001-2004 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$DB->Execute("CREATE TABLE rtrights (
+    id INT(11) NOT NULL auto_increment, 
+    adminid INT(11) DEFAULT 0 NOT NULL,
+    queueid INT(11) DEFAULT 0 NOT NULL,
+    rights INT(11) DEFAULT 0 NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (adminid, queueid)
+    )
+");
+$DB->Execute("ALTER TABLE rtqueues ADD description TEXT DEFAULT '' NOT NULL");
+
+$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?",array('2004070100', 'dbversion'));
+
+?>
