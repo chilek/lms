62295080b5e74cfda576d523df1f2766862c713f
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2004-07-05 23:26:57 +0000

    - cleanups

--- modules/traffic.php
+++ modules/traffic.php
@@ -23,7 +23,7 @@
  *  $Id$
  */
 
-$layout['pagetitle'] = "Statystyki wykorzystania ³±cza";
+$layout['pagetitle'] = 'Statystyki wykorzystania ³±cza';
 
 $bars = 1;
 
@@ -32,27 +32,27 @@ if (isset($_GET['bar']) && isset($_POST['order']))
 	
 switch($_GET['bar'])
 {
-	case "hour":
-		$traffic = $LMS->Traffic( "?NOW?-(60*60)", "?NOW?", 0,
-			isset($_SESSION['trafficorder']) ? $_SESSION['trafficorder'] : "download");
+	case 'hour':
+		$traffic = $LMS->Traffic( '?NOW?-(60*60)', '?NOW?', 0,
+			isset($_SESSION['trafficorder']) ? $_SESSION['trafficorder'] : 'download');
 	break;
 
-	case "day":
-		$traffic = $LMS->Traffic( "?NOW?-(60*60*24)","?NOW?",  0,
-			isset($_SESSION['trafficorder']) ? $_SESSION['trafficorder'] : "download");
+	case 'day':
+		$traffic = $LMS->Traffic( '?NOW?-(60*60*24)','?NOW?',  0,
+			isset($_SESSION['trafficorder']) ? $_SESSION['trafficorder'] : 'download');
 	break;
 
-	case "month":
-		$traffic = $LMS->Traffic( "?NOW?-(60*60*24*30)", "?NOW?", 0,
-			isset($_SESSION['trafficorder']) ? $_SESSION['trafficorder'] : "download");
+	case 'month':
+		$traffic = $LMS->Traffic( '?NOW?-(60*60*24*30)', '?NOW?', 0,
+			isset($_SESSION['trafficorder']) ? $_SESSION['trafficorder'] : 'download');
 	break;
 
-	case "year":
-		$traffic = $LMS->Traffic( "?NOW?-(60*60*24*365)", "?NOW?", 0,
-			isset($_SESSION['trafficorder']) ? $_SESSION['trafficorder'] : "download");
+	case 'year':
+		$traffic = $LMS->Traffic( '?NOW?-(60*60*24*365)', '?NOW?', 0,
+			isset($_SESSION['trafficorder']) ? $_SESSION['trafficorder'] : 'download');
 	break;
 
-	case "user":
+	case 'user':
 		$traffic = $LMS->Traffic($_POST['from'], $_POST['to'], $_POST['net'], $_POST['order'], $_POST['limit']);
 	break;
 
@@ -68,8 +68,8 @@ $upload = $traffic['upload'];
 
 // fuck this anyway... Maybe i write function in LMS:: for this, but not now
 
-$starttime = $DB->GetOne("SELECT MIN(dt) FROM stats");
-$endtime = $DB->GetOne("SELECT MAX(dt) FROM stats");
+$starttime = $DB->GetOne('SELECT MIN(dt) FROM stats');
+$endtime = $DB->GetOne('SELECT MAX(dt) FROM stats');
 $startyear = date('Y',$starttime);
 $endyear = date('Y',$endtime);
 
@@ -83,7 +83,7 @@ $SMARTY->assign('showips',$_POST['showips']);
 $SMARTY->assign('download',$download);
 $SMARTY->assign('upload',$upload);
 $SMARTY->assign('bars',$bars);
-$SMARTY->assign('trafficorder', isset($_SESSION['trafficorder']) ? $_SESSION['trafficorder'] : "download");
+$SMARTY->assign('trafficorder', isset($_SESSION['trafficorder']) ? $_SESSION['trafficorder'] : 'download');
 $SMARTY->display('traffic.html');
 
 ?>
