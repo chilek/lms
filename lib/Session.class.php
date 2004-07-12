b5c2de2c1550821cc6d95765c04710dcaff38966
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2004-07-12 17:25:25 +0000

    - cosmetics

--- lib/Session.class.php
+++ lib/Session.class.php
@@ -41,15 +41,15 @@ class Session {
 	{
 		session_start();
 		$this->DB = &$DB;
-		if($_GET['override'])
-			$loginform = $_GET['loginform'];
+		if($_GET[''override''])
+			$loginform = $_GET[''loginform''];
 		else
-			$loginform = $_POST['loginform'];
+			$loginform = $_POST[''loginform''];
 		if(isset($loginform))
 		{
-			$this->login = $loginform[login];
-			$this->passwd = $loginform[pwd];
-			$_SESSION[session_timestamp] = time();
+			$this->login = $loginform['login'];
+			$this->passwd = $loginform['pwd'];
+			$_SESSION['session_timestamp'] = time();
 			writesyslog("Login attempt by ".$this->login,LOG_INFO);
 		}
 		elseif($this->DB->GetOne('SELECT COUNT(id) FROM admins') == 0)
@@ -57,41 +57,41 @@ class Session {
 			$this->islogged = TRUE;
 			$this->passwd = 'EMPTY';
 			$this->logname = 'nie zalogowany';
-			$_GET[m] = 'adminadd';
+			$_GET['m'] = 'adminadd';
 			return TRUE;
 		}
 		else
 		{
-			$this->login = $_SESSION[session_login];
-			$this->passwd = $_SESSION[session_passwd];
+			$this->login = $_SESSION['session_login'];
+			$this->passwd = $_SESSION['session_passwd'];
 		}
 		
 		if($this->VerifyPassword()&&$this->TimeOut($timeout)){
 			$this->islogged = TRUE;
 			$admindata = $this->DB->GetRow("SELECT id, name FROM admins WHERE login=?",array($this->login));
-			$this->id = $admindata[id];
-			$this->logname = $admindata[name];
-			$this->last = $_SESSION[session_last];
-			$this->lastip = $_SESSION[session_lastip];
+			$this->id = $admindata['id'];
+			$this->logname = $admindata['name'];
+			$this->last = $_SESSION['session_last'];
+			$this->lastip = $_SESSION['session_lastip'];
 			if(isset($loginform))
 			{
 				$admindata = $this->DB->GetRow("SELECT lastlogindate, lastloginip FROM admins WHERE id=?",array($this->id));
-				$this->last = $admindata[lastlogindate];
-				$this->lastip = $admindata[lastloginip];
+				$this->last = $admindata['lastlogindate'];
+				$this->lastip = $admindata['lastloginip'];
 				
-				$this->DB->Execute("UPDATE admins SET lastlogindate=?, lastloginip=? WHERE id=?",array(time(),$_SERVER[REMOTE_ADDR],$this->id));
+				$this->DB->Execute("UPDATE admins SET lastlogindate=?, lastloginip=? WHERE id=?",array(time(),$_SERVER['REMOTE_ADDR'],$this->id));
 				writesyslog("User ".$this->login." logged in",LOG_INFO);
 			}
-			$_SESSION[session_login] = $this->login;
-			$_SESSION[session_passwd] = $this->passwd;
-			$_SESSION[session_last] = $this->last;
-			$_SESSION[session_lastip] = $this->lastip;
+			$_SESSION['session_login'] = $this->login;
+			$_SESSION['session_passwd'] = $this->passwd;
+			$_SESSION['session_last'] = $this->last;
+			$_SESSION['session_lastip'] = $this->lastip;
 		}else{
 			$this->islogged = FALSE;
 			if(isset($loginform))
 			{
 				writesyslog("Bad password for ".$this->login,LOG_WARNING);
-				$this->DB->Execute("UPDATE admins SET failedlogindate=?, failedloginip=? WHERE login=?",array(time(),$_SERVER[REMOTE_ADDR],$this->login));
+				$this->DB->Execute("UPDATE admins SET failedlogindate=?, failedloginip=? WHERE login=?",array(time(),$_SERVER['REMOTE_ADDR'],$this->login));
 			}
 			$this->LogOut();
 		}
@@ -114,14 +114,14 @@ class Session {
 	
 	function TimeOut($timeout = 600)
 	{
-		if( (time()-$_SESSION[session_timestamp]) > $timeout )
+		if( (time() - $_SESSION['session_timestamp']) > $timeout )
 		{
-			$this->error="Przekroczy³e¶ limit czasu bezczynno¶ci (".$timeout." sekund).";
+			$this->error = "Przekroczy³e¶ limit czasu bezczynno¶ci (".$timeout." sekund).";
 			return FALSE;
 		}
 		else
 		{
-			$_SESSION[session_timestamp] = time();
+			$_SESSION['session_timestamp'] = time();
 			return TRUE;
 		}
 	}
