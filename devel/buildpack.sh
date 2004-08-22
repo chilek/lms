ae41717f6978f7617ba5ab7a7df31c23077f7903
Aleksander Machniak <alec@alec.pl>
2004-08-22 10:30:32 +0000

    - Smarty-2.6.3

--- devel/buildpack.sh
+++ devel/buildpack.sh
@@ -54,10 +54,10 @@ chmod 777 templates_c backups
 cd ..
 tar -czf $WORKDIR/lms-$LMSVER.tar.gz lms
 cd lms/lib
-wget http://smarty.php.net/distributions/Smarty-2.6.1.tar.gz
-tar -xzf Smarty-2.6.1.tar.gz
-mv Smarty-2.6.1/libs Smarty
-rm -Rf Smarty-2.6.1 Smarty-2.6.1.tar.gz
+wget http://smarty.php.net/distributions/Smarty-2.6.3.tar.gz
+tar -xzf Smarty-2.6.3.tar.gz
+mv Smarty-2.6.3/libs Smarty
+rm -Rf Smarty-2.6.3 Smarty-2.6.3.tar.gz
 cd ../
 rm -Rf $NOTDISTRIB
 cd ../
