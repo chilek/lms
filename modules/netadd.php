b1edd1b5918cc4c1aee451aab8676a9d6f6e4863
Aleksander Machniak <alec@alec.pl>
2004-08-30 16:45:16 +0000

    - kosmetyka ""->''

--- modules/netadd.php
+++ modules/netadd.php
@@ -34,91 +34,90 @@ if(isset($netadd))
 	}
 
 	if(
-			$netadd['name']=="" &&
-			$netadd['address']=="" &&
-			$netadd['dns']=="" &&
-			$netadd[dns2]=="" &&
-			$netadd['domain']=="" &&
-			$netadd['gateway']=="" &&
-			$netadd['wins']=="" &&
-			$netadd['dhcpstart']=="" &&
-			$netadd['dhcpend']==""
+			$netadd['name']=='' &&
+			$netadd['address']=='' &&
+			$netadd['dns']=='' &&
+			$netadd['dns2']=='' &&
+			$netadd['domain']=='' &&
+			$netadd['gateway']=='' &&
+			$netadd['wins']=='' &&
+			$netadd['dhcpstart']=='' &&
+			$netadd['dhcpend']==''
 	)
-		
-	header("Location: ?m=netadd");
+		header('Location: ?m=netadd');
 
 
-	if($netadd['name']=="")
-		$error['name'] = "Musisz podaæ nazwê sieci!";
+	if($netadd['name']=='')
+		$error['name'] = 'Musisz podaæ nazwê sieci!';
 	elseif(!eregi("^[.a-z0-9-]+$",$netadd['name']))
-		$error['name'] = "Podana nazwa sieci zawiera niepoprawne znaki!";
+		$error['name'] = 'Podana nazwa sieci zawiera niepoprawne znaki!';
 	
-	if($netadd['domain'] != "" && !eregi("^[.a-z0-9-]+$",$netadd['domain']))
-		$error['domain'] = "Podana nazwa domeny zawiera niepoprawne znaki!";
+	if($netadd['domain'] != '' && !eregi("^[.a-z0-9-]+$",$netadd['domain']))
+		$error['domain'] = 'Podana nazwa domeny zawiera niepoprawne znaki!';
 	
 	if(!check_ip($netadd['address']))
-		$error['address'] = "Podany adres IP sieci jest nieprawid³owy!";
+		$error['address'] = 'Podany adres IP sieci jest nieprawid³owy!';
 	else
 	{
 		if(getnetaddr($netadd['address'],prefix2mask($netadd['prefix']))!=$netadd['address'])
 		{
-			$error['address'] = "Podany adres nie jest pocz±tkowym adresem sieci, ustawiam na ".getnetaddr($netadd['address'],prefix2mask($netadd['prefix']));
+			$error['address'] = 'Podany adres nie jest pocz±tkowym adresem sieci, ustawiam na '.getnetaddr($netadd['address'],prefix2mask($netadd['prefix']));
 			$netadd['address'] = getnetaddr($netadd['address'],prefix2mask($netadd['prefix']));
 		}
 		else
 		{
 			if($LMS->NetworkOverlaps($netadd['address'],prefix2mask($netadd['prefix'])))
-				$error['address'] = "Podany adres sieci pokrywa siê z inn± sieci±!";
+				$error['address'] = 'Podany adres sieci pokrywa siê z inn± sieci±!';
 		}
 	}
 
 	if($netadd['interface'] != '' && !eregi('^[a-z0-9]+$',$netadd['interface']))
-		$error['interface'] = "Niepoprawna nazwa interfejsu!";
+		$error['interface'] = 'Niepoprawna nazwa interfejsu!';
 
-	if($netadd['dns']!="" && !check_ip($netadd['dns']))
-		$error['dns'] = "Podany adres IP serwera DNS jest nieprawid³owy!";
+	if($netadd['dns']!='' && !check_ip($netadd['dns']))
+		$error['dns'] = 'Podany adres IP serwera DNS jest nieprawid³owy!';
 	
-	if($netadd[dns2]!="" && !check_ip($netadd[dns2]))
-		$error[dns2] = "Podany adres IP serwera DNS jest nieprawid³owy!";
+	if($netadd['dns2']!='' && !check_ip($netadd['dns2']))
+		$error['dns2'] = 'Podany adres IP serwera DNS jest nieprawid³owy!';
 	
-	if($netadd['wins']!="" && !check_ip($netadd['wins']))
-		$error['wins'] = "Podany adres IP serwera WINS jest nieprawid³owy!";
+	if($netadd['wins']!='' && !check_ip($netadd['wins']))
+		$error['wins'] = 'Podany adres IP serwera WINS jest nieprawid³owy!';
 	
-	if($netadd['gateway']!="")
+	if($netadd['gateway']!='')
 		if(!check_ip($netadd['gateway']))
-			$error['gateway'] = "Podany adres IP bramy jest nieprawid³owy!";
+			$error['gateway'] = 'Podany adres IP bramy jest nieprawid³owy!';
 	elseif(!isipin($netadd['gateway'],getnetaddr($netadd['address'],prefix2mask($netadd['prefix'])),prefix2mask($netadd['prefix'])))
-		$error['gateway'] = "Podany adres bramy nie pasuje do adresu sieci!";
+		$error['gateway'] = 'Podany adres bramy nie pasuje do adresu sieci!';
 	
-	if($netadd['dhcpstart']!="")
+	if($netadd['dhcpstart']!='')
 		if(!check_ip($netadd['dhcpstart']))
-			$error['dhcpstart'] = "Podany adres IP pocz±tku zakresu DHCP jest nieprawid³owy!";
+			$error['dhcpstart'] = 'Podany adres IP pocz±tku zakresu DHCP jest nieprawid³owy!';
 	elseif(!isipin($netadd['dhcpstart'],getnetaddr($netadd['address'],prefix2mask($netadd['prefix'])),prefix2mask($netadd['prefix'])) && $netadd['address']!="")
-		$error['dhcpstart'] = "Podany adres IP pocz±tku zakresu DHCP nie nale¿y do tej sieci!";
+		$error['dhcpstart'] = 'Podany adres IP pocz±tku zakresu DHCP nie nale¿y do tej sieci!';
 	
-	if($netadd['dhcpend']!="")
+	if($netadd['dhcpend']!='')
 		if(!check_ip($netadd['dhcpend']))
-			$error['dhcpend'] = "Podany adres IP koñca zakresu DHCP jest nieprawid³owy!";
+			$error['dhcpend'] = 'Podany adres IP koñca zakresu DHCP jest nieprawid³owy!';
 	elseif(!isipin($netadd['dhcpend'],getnetaddr($netadd['address'],prefix2mask($netadd['prefix'])),prefix2mask($netadd['prefix'])) && $netadd['address']!="")
-		$error['dhcpend'] = "Podany adres IP koñca zakresu DHCP nie nale¿y do tej sieci!";
+		$error['dhcpend'] = 'Podany adres IP koñca zakresu DHCP nie nale¿y do tej sieci!';
 	
 	if(!$error['dhcpstart'] && !$error['dhcpend'])
 	{
-		if(($netadd['dhcpstart']!="" && $netadd['dhcpend']=="")||($netadd['dhcpstart']=="" && $netadd['dhcpend']!=""))
-			$error['dhcp'] = "Musisz podaæ obydwa zakresy IP dla DHCP!";
-		if($netadd['dhcpstart']!="" && $netadd['dhcpend']!="" && !(ip_long($netadd['dhcpend']) > ip_long($netadd['dhcpstart'])))
-			$error['dhcp'] = "Koniec zakresu DHCP musi byæ wiêkszy ni¿ start!";
+		if(($netadd['dhcpstart']!='' && $netadd['dhcpend']=='')||($netadd['dhcpstart']=="" && $netadd['dhcpend']!=''))
+			$error['dhcp'] = 'Musisz podaæ obydwa zakresy IP dla DHCP!';
+		if($netadd['dhcpstart']!='' && $netadd['dhcpend']!='' && !(ip_long($netadd['dhcpend']) > ip_long($netadd['dhcpstart'])))
+			$error['dhcp'] = 'Koniec zakresu DHCP musi byæ wiêkszy ni¿ start!';
 	}
 	
 	if(!$error)
 	{
-		header("Location: ?m=netinfo&id=".$LMS->NetworkAdd($netadd));
+		header('Location: ?m=netinfo&id='.$LMS->NetworkAdd($netadd));
 		die;
 	}
 
 }
 
-$layout['pagetitle'] = "Nowa sieæ";
+$layout['pagetitle'] = 'Nowa sieæ';
 
 $prefixlist = $LMS->GetPrefixList();
 $netlist = $LMS->GetNetworkList();
