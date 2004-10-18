8fcbc7e9cca9eb3666f53133d1ad28f33155ced3
Aleksander Machniak <alec@alec.pl>
2004-10-18 12:32:29 +0000

    - wireless connections

--- lib/upgradedb.php
+++ lib/upgradedb.php
@@ -24,7 +24,7 @@
  *  $Id$
  */
 
-define(DBVERSION, '2004090800'); // here should be always the newest version of database!
+define(DBVERSION, '2004101800'); // here should be always the newest version of database!
 // it placed here to avoid read disk every time when we call this file.
 
 /*
