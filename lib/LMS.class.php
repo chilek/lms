636bb72cf4c4201e40a81db31a07a0629217606d
Aleksander Machniak <alec@alec.pl>
2004-10-28 12:59:48 +0000

    - experymentalny driver do mssql'a

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -3093,7 +3093,7 @@ class LMS
 	function RTStats()
 	{
 		return $this->DB->GetRow('SELECT COUNT(CASE state WHEN 0 THEN 1 END) AS new,
-						    COUNT(CASE state WHEN 1 THEN 1 END) AS open,
+						    COUNT(CASE state WHEN 1 THEN 1 END) AS opened,
 						    COUNT(CASE state WHEN 2 THEN 1 END) AS resolved,
 						    COUNT(CASE state WHEN 3 THEN 1 END) AS dead
 					     FROM rttickets');
