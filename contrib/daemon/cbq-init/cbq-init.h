2fe60e0b54c8ab1db959bb7fe33e0a937fd2af94
Aleksander Machniak <alec@alec.pl>
2004-11-12 20:36:30 +0000

    - cbq-init

---
+++ contrib/daemon/cbq-init/cbq-init.h
@@ -0,0 +1,29 @@
+struct net
+{
+	unsigned char *name;
+	unsigned char *interface;
+	unsigned long address;
+	unsigned long mask;
+};
+
+struct group
+{
+	unsigned char *name;
+	int id;
+};
+
+struct cbq_module
+{
+	MODULE base;
+
+	unsigned char *path;
+	unsigned char *command;
+	unsigned char *cbq_down;
+	unsigned char *cbq_up;
+	unsigned char *mark_rule;
+	unsigned char *mark_file;
+	unsigned char *mark_file_begin;
+	unsigned char *mark_file_end;
+	unsigned char *networks;
+	unsigned char *usergroups;
+};
