2f27ee7dbdf0c896e7c1a93a9995ae3181c18e9d
Aleksander Machniak <alec@alec.pl>
2004-11-17 20:32:07 +0000

    - passwd.lastlogin: timestamp -> integer

---
+++ lib/upgradedb/mysql.2004111700.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * LMS version 1.5-cvs
+ *
+ *  (C) Copyright 2001-2004 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$DB->Execute("ALTER TABLE passwd ADD ll INT(11)");
+$DB->Execute("UPDATE passwd SET ll=UNIX_TIMESTAMP(lastlogin)");
+$DB->Execute("ALTER TABLE passwd DROP lastlogin");
+$DB->Execute("ALTER TABLE passwd CHANGE ll lastlogin INT(11) NOT NULL DEFAULT '0'");
+
+$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?",array('2004111700', 'dbversion'));
+
+?>
