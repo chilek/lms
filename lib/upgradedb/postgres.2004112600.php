fcd6738e78af69167562a9ad54a52892e132c5aa
Aleksander Machniak <alec@alec.pl>
2004-11-26 13:28:38 +0000

    - konfiguracja lms-ui

---
+++ lib/upgradedb/postgres.2004112600.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * LMS version 1.5-cvs
+ *
+ *  (C) Copyright 2001-2004 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$DB->BeginTrans();
+$DB->Execute("
+    CREATE SEQUENCE uiconfig_id_seq;
+    CREATE TABLE uiconfig (
+	id integer DEFAULT nextval('uiconfig_id_seq'::text) NOT NULL,
+	section varchar(255) NOT NULL DEFAULT '',
+	var varchar(255) NOT NULL DEFAULT '',
+        value text NOT NULL DEFAULT '',
+	description text NOT NULL DEFAULT '',
+	disabled smallint NOT NULL DEFAULT 0,
+	PRIMARY KEY (id)
+    );
+    UPDATE dbinfo SET keyvalue = '2004112600' WHERE keytype = 'dbversion';
+");
+$DB->CommitTrans();
+
+?>
