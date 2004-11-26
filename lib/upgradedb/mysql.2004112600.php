1594e90193e084cd8657337ef9aee97c44fc463e
Aleksander Machniak <alec@alec.pl>
2004-11-26 13:20:40 +0000

    - konfiguracja lms-ui

---
+++ lib/upgradedb/mysql.2004112600.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * LMS version 1.5-cvs
+ *
+ *  (C) Copyright 2001-2004 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$DB->Execute("
+    CREATE TABLE uiconfig (
+	id int(11) NOT NULL auto_increment,
+	section varchar(255) NOT NULL default '',
+	var varchar(255) NOT NULL default '',
+        value text NOT NULL default '',
+	description text NOT NULL default '',
+	disabled tinyint(1) NOT NULL default '0',
+	PRIMARY KEY (id)
+    ) TYPE=MyISAM
+");
+
+$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?",array('2004112600', 'dbversion'));
+
+?>
---
+++ modules/configlist.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * LMS version 1.5-cvs
+ *
+ *  (C) Copyright 2001-2004 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+function GetConfigList()
+{
+	global $LMS;
+	$config = $LMS->DB->GetAll('SELECT id, section, var, value, description, disabled FROM uiconfig ORDER BY section, var');
+	$config['total'] = sizeof($config);
+	return $config;
+}
+
+$layout['pagetitle'] = 'Konfiguracja LMS-UI';
+
+if(!isset($_GET['o']))
+	$o = $_SESSION['clo'];
+else
+	$o = $_GET['o'];
+$_SESSION['clo'] = $o;
+
+if (isset($_SESSION['clp']) && !isset($_GET['page']))
+	$_GET['page'] = $_SESSION['clp'];
+
+$configlist = GetConfigList();
+$listdata['total'] = $configlist['total'];
+$listdata['order'] = $configlist['order'];
+$listdata['direction'] = $configlist['direction'];
+unset($configlist['total']);
+unset($configlist['order']);
+unset($configlist['direction']);
+	    
+$page = (! $_GET['page'] ? 1 : $_GET['page']); 
+$pagelimit = (! $LMS->CONFIG['phpui']['configlist_pagelimit'] ? $listdata['total'] : $LMS->CONFIG['phpui']['configlist_pagelimit']);
+$start = ($page - 1) * $pagelimit;
+
+$_SESSION['clp'] = $page;
+
+$_SESSION['backto'] = $_SERVER['QUERY_STRING'];
+
+$SMARTY->assign('pagelimit', $pagelimit);
+$SMARTY->assign('page', $page);
+$SMARTY->assign('start', $start);
+$SMARTY->assign('configlist', $configlist);
+$SMARTY->assign('listdata', $listdata);
+$SMARTY->assign('layout',$layout);
+$SMARTY->display('configlist.html');
+
+?>
