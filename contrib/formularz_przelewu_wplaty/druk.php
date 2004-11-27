6396f2ec98ceed0ac440a5ef5637a308c8b755f1
Aleksander Machniak <alec@alec.pl>
2004-11-27 19:10:14 +0000

    - druk.php

--- contrib/formularz_przelewu_wplaty/druk.php
+++ contrib/formularz_przelewu_wplaty/druk.php
@@ -5,6 +5,8 @@
 </HEAD>
 <BODY>
 
+<FORM><P style="font-family: Arial, Helvetica; font-size: 12pt; font-weight: bold;">W zale¿no¶ci od poczty/banku wpisz 1 lub 2 kopie. <input type="button" value="Drukuj" onClick="top.print();"></FORM>
+
 <?php 
 
 // ustaw prawid³ow± ¶cie¿kê do pliku common.php z katalogu /lib lmsa
@@ -12,90 +14,148 @@ require_once("../../lib/common.php");
 
 //  NRB 26 cyfr, 2 kontrolne, 8 nr banku, 16 nr konta 
 
-$KONTO_DO="00123456781234567890123456";
+$KONTO_DO="0000000000000000000000000";
 
-$ISP1_DO="XXXXXXXXXXXXXXXXXXXXXXXXXXX";
-$ISP2_DO="XXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$ISP1_DO="xxxxxxxxxxxxxxxxxxxxxxx";
+$ISP2_DO="xxxxxxxxxxxxxxxxxxxxxxx";
 
 $KWOTA = $_GET['ILE'];
 $USER_OD = $_GET['OD'];
-$USER_ID = $_GET['UID'];
+$USER_TY = "Abonament - ID:".$_GET['UID'];
 
-$KWOTA_NR = str_replace(',','.',$KWOTA);  //na wszelki wypadek
+$KWOTA_NR = str_replace(',','.',$KWOTA);  // na wszelki wypadek
 $KWOTA_ZL = to_words(floor($KWOTA_NR));
-$KWOTA_GR = to_words(($KWOTA_NR - floor($KWOTA_NR))*100);
+$KWOTA_GR = round(($KWOTA_NR - floor($KWOTA_NR))*100); 
+$CURR="PLN";
 
+
+$SHIFT=394; // drugi druczek przesuniêcie o 394
+
+
+for ( $j=0; $j<2; $j++ ) // pêtla g³ówna
+{
+// teksty na druczku:
+
+     $posx=60+$j*$SHIFT; 
+     echo('<div style="position: absolute; top: '. $posx .'px; left: 10px"><img src="przelew.png" border=0 alt="wp³ata gotówkowa"></div>');
+     $posx=63+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Arial, Helvetica;color: #FF0000;font-size: 6pt;">nazwa odbiorcy</span>');
+     $posx=96+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Arial, Helvetica; color: #FF0000; font-size: 6pt;">nazwa odbiorcy cd.</span>');
+     $posx=131+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Arial, Helvetica;color: #FF0000;font-size: 6pt;">l.k.</span>');
+     $posx=131+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 102px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">nr rachunku odbiorcy</span>');
+     $posx=163+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 352px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">kwota</span>');
+     $posx=194+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">kwota s³ownie</span>');
+     $posx=222+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">nazwa zleceniodawcy</span>');
+     $posx=253+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">nazwa zleceniodawcy cd.</span>');
+     $posx=284+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">tytu³em</span>');
+     $posx=317+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">tytu³em cd.</span>');
+     $posx=395+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 337px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">Op³ata</span>');
+     $posx=425+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 337px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">Podpis</span>');
+
+// waluta:
+
+     $posx=174+$j*$SHIFT;
+     for ( $i=0; $i<27; $i++ ) 
+     {
+          $posy=272+$i*19;
+          echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'.$CURR[$i].'</SpAn>');
+     }
+
+// nazwa beneficjenta:
+     
+     $posx=75+$j*$SHIFT;
+     for ( $i=0; $i<27; $i++ ) 
+     {
+          $posy=62+$i*19;
+          echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'.$ISP1_DO[$i].'</SpAn>');
+     }
+     
+     $posx=109+$j*$SHIFT;
+     for ( $i=0; $i<27; $i++ ) 
+     {
+          $posy=62+$i*19;
+          echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'.$ISP2_DO[$i].'</SpAn>');
+     }
+
+// numer konta beneficjenta:
+
+     $posx=141+$j*$SHIFT;
+     for ( $i=0; $i<26; $i++ )
+     {
+          $posy=62+$i*19;
+          echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'.$KONTO_DO[$i].'</SpAn>');
+     }
+
+// kwota cyfrowo:
+
+     $posx=174+$j*$SHIFT;
+     $KWOTA_SL = sprintf("%0'--12.2f",$KWOTA);
+     $KWOTA_SL = str_replace('.',',',$KWOTA_SL);
+     for ( $i=0; $i<12; $i++ ) 
+     {
+          $posy=347+$i*19;
+          echo('<SPAN style="position: absolute; top: '. $posx .'px; left: ' . $posy . 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. $KWOTA_SL[$i] .'</SPAN>');
+     }
+
+// kwota s³ownie:
+
+     $posx=205+$j*$SHIFT;
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 8pt; font-weight: bold; ";>' . $KWOTA_ZL .' '. $KWOTA_GR.'/100 z³otych</span>');
+
+// dane p³atnika:
+
+
+     if (strlen($USER_OD)>54)  // je¿eli nazwa+adres s± d³u¿sze niz 54 znaki _nie_ wpisujemy w kratki
+     {
+          $posx=235+$j*$SHIFT;
+          echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. substr($USER_OD,0,50) .'</span>');
+          $posx=265+$j*$SHIFT;
+          echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. substr($USER_OD,50,100) .'</span>');
+     }
+     else                // je¿eli nazwa+adres zmieszcz± siê w kratkach to wpisujemy w kratkach
+     {                    
+          $posx=235+$j*$SHIFT;
+          for ( $i=0; $i<27; $i++ ) 
+          {
+               $posy=62+$i*19;
+               echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. $USER_OD[$i].'</span>');
+          }
+          $posx=265+$j*$SHIFT;
+          for ( $i=27; $i<54; $i++ ) 
+          {
+               $posy=62+$i*19-513;
+               echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. $USER_OD[$i].'</span>');
+          }
+     }
+
+// tytu³em:
+
+     $posx=298+$j*$SHIFT;
+     for ( $i=0; $i<27; $i++ ) 
+     {
+          $posy=62+$i*19;
+          echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. $USER_TY[$i].'</span>');
+     }
+
+
+     $posx=327+$j*$SHIFT;   // wolna linijka
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">-</span>');
+
+} //  koniec pêtli g³ównej
 ?>
 
-<div style="position: absolute; top: 60px; left: 10px"><img src="przelew.png" border=0 alt="wp³ata gotówkowa"></div><form>W zale¿no¶ci od poczty/Banku wpisz 1,2 lub nawet 4 kopie. To nie nasz pomys³, tylko Poczty polskiej [tm]<input type="button" value="Drukuj" onClick="top.print();"></form>
-<span style="position: absolute; top: 63px; left: 62px; font-family: Arial, Helvetica;color: #FF0000;font-size: 6pt;">nazwa odbiorcy</span>
-<span style="position: absolute; top: 96px; left: 62px; font-family: Arial, Helvetica; color: #FF0000; font-size: 6pt;">nazwa odbiorcy cd.</span>
-<span style="position: absolute; top: 131px; left: 62px; font-family: Arial, Helvetica;color: #FF0000;font-size: 6pt;">l.k.</span>
-<span style="position: absolute; top: 131px; left: 102px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">nr rachunku odbiorcy</span>
-<span style="position: absolute; top: 163px; left: 352px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">kwota</span>
-<span style="position: absolute; top: 194px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">kwota s³ownie</span>
-<span style="position: absolute; top: 222px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">nazwa zleceniodawcy</span>
-<span style="position: absolute; top: 253px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">nazwa zleceniodawcy cd.</span>
-<span style="position: absolute; top: 284px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">tytu³em</span>
-<span style="position: absolute; top: 317px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">tytu³em cd.</span>
-<span style="position: absolute; top: 395px; left: 337px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">Op³ata</span>
-<span style="position: absolute; top: 425px; left: 337px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">Podpis</span>
-<span style="position: absolute; top: 75px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;"></SPAN>
-
-<?php
-//
-// NAZWA beneficjenta:
-
-for ( $i=0; $i<27; $i++ ) {
-	$pos=62+$i*19;
-	echo('<span style="position: absolute; top: 75px; left: '. $pos. ' px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'.$ISP1_DO[$i].'</SpAn>');
-	}
-
-?>
-<?php
-
-for ( $i=0; $i<27; $i++ ) {
-	$pos=62+$i*19;
-	echo('<span style="position: absolute; top: 109px; left: '. $pos. ' px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'.$ISP2_DO[$i].'</SpAn>');
-	}
-
-?>
-<!--nr konta-->
-
-<?php
-
-for ( $i=0; $i<26; $i++ ) {
-	$pos=62+$i*19;
-	echo('<span style="position: absolute; top: 141px; left: '. $pos. ' px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'.$KONTO_DO[$i].'</SpAn>');
-	}
-
-?>
-
-
-<!--KWOTA-->
-<span style="position: absolute; top: 172px; left: 272px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">P</SPAN>
-<span style="position: absolute; top: 172px; left: 291px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">L</SPAN>
-<span style="position: absolute; top: 172px; left: 310px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">N</SPAN>
-<span style="position: absolute; top: 172px; left: 329px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;"></SPAN>
-
-<span style="position: absolute; top: 172px; left: 242px; font-family: Courier, Arial, Helvetica; font-size: 16pt; font-weight: bold;">X</SPAN>
-
-<?php
-$KWOTA_SL=sprintf("%0'--12.2f",$KWOTA);
-for ( $i=0; $i<12; $i++ ) {
-	$pos=347+$i*19;
-	echo('<SPAN style="position: absolute; top: 172px; left: ' . $pos . ' px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. $KWOTA_SL[$i] . '</SPAN>');
-	}
-?>
-
-<span style="position: absolute; top: 203px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 8pt; font-weight: bold;"><?php echo "$KWOTA_ZL z³ $KWOTA_GR gr";?></span>
-
-<span style="position: absolute; top: 235px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 10pt; font-weight: bold;"><?php echo($USER_OD); ?></span>
-<span style="position: absolute; top: 265px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 10pt; font-weight: bold;">A</span>
-<span style="position: absolute; top: 298px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">Op³ata abonamentowa - ID:<?php echo($USER_ID); ?></span>
-<span style="position: absolute; top: 327px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;"></span>
-
-<span style="position: absolute; top: 527px; left: 12px; font-family: Arial, Helvetica; font-size: 12pt; font-weight: bold;">Wydrukowano przy u¿yciu LMS (http://lms.rulez.pl)</span>
-<span style="position: absolute; top: 546px; left: 12px; font-family: Arial, Helvetica; font-size: 12pt; font-weight: bold;">LMS kompletny system sieciowo-ksiêgowy dla ma³ych ISPów i ASKów, dostêpny na licencji GNU GPL</span>
+<span style="position: absolute; top: 880px; left: 12px; font-family: Arial, Helvetica; font-size: 12pt; font-weight: bold;">Wydrukowano przy u¿yciu LMS (http://lms.rulez.pl)</span>
+<span style="position: absolute; top: 900px; left: 12px; font-family: Arial, Helvetica; font-size: 12pt; font-weight: bold;">LMS kompletny system sieciowo-ksiêgowy dla ma³ych ISPów i ASKów, dostêpny na licencji GNU GPL</span>
 
 </HTML>
