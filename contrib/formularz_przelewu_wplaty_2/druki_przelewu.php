5075461b9bb8280f50771978ba1f78e57a9eb9ed
Aleksander Machniak <alec@alec.pl>
2004-12-14 21:58:07 +0000

    - copyrights

--- contrib/formularz_przelewu_wplaty_2/druki_przelewu.php
+++ contrib/formularz_przelewu_wplaty_2/druki_przelewu.php
@@ -1,14 +1,25 @@
 <?php
-
+							    
 /*
- * LMS Userpanel
+ * LMS version 1.5-cvs
  *
- *  (C) Copyright 2004 LMS Developers
+ *  (C) Copyright 2001-2004 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
- *  This program is licensed under LMS License. Please, see
- *  doc/LICENSE.pl file for information about copyright notice.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
  *
  *  $Id$
  */
@@ -123,4 +134,3 @@ foreach ($userlist as $user) {
 $pdf->ezStream();
 
 ?>
-							    
