57e80ae1a74fb83cd5d6fd7f75df84c5e842d65e
Marcin Król <lexx@polarnet.gliwice.pl>
2005-01-22 14:26:39 +0000

    - tsave

--- modules/invoice_pdf.php
+++ modules/invoice_pdf.php
@@ -87,7 +87,7 @@ function invoice_main_form_fill($x,$y,$scale)	{
     $pdf->addtext(15*$scale+$x,555*$scale+$y,30*$scale,iconv("UTF-8","ISO-8859-2",$finances['account']));
     $pdf->addtext(330*$scale+$x,495*$scale+$y,30*$scale,'X');
     $pdf->addtext(550*$scale+$x,495*$scale+$y,30*$scale,number_format($invoice['total'],2,',',''));
-    $pdf->addtext(15*$scale+$x,434*$scale+$y,30*$scale,to_words(floor($invoice['total']))." z³, ".to_words(($invoice['total']-floor($invoice['total']))*100)." gr");
+    $pdf->addtext(15*$scale+$x,434*$scale+$y,30*$scale,to_words(floor($invoice['total']))." z³, ".to_words(round(($invoice['total']-floor($invoice['total']))*100))." gr");
     $pdf->addtext(15*$scale+$x,372*$scale+$y,30*$scale,$invoice['name']);
     $pdf->addtext(15*$scale+$x,312*$scale+$y,30*$scale,$invoice['address']." ".$invoice['zip']." ".$invoice['city']);
     $tmp = $_CONFIG['invoices'];
@@ -111,6 +111,16 @@ function text_align_left($x,$y,$size,$text)
     global $pdf;
     $pdf->addText($x,$y,$size,$text);
     return($pdf->getFontHeight($size));
+}
+
+function text_wrap($x,$y,$width,$size,$text) 
+{
+    global $pdf;
+    while ($text!='') {
+	$text = $pdf->addTextWrap($x, $y, $width, $size,$text);
+	$y = $y - $pdf->getFontHeight($size);
+    }
+    return($y);
 }
 
 function invoice_dates($x,$y) {
@@ -182,6 +192,98 @@ function invoice_address_box($x,$y) {
     return $y;
 }
 
+function invoice_data_row($x,$y,$width,$font_size,$margin,$data) {
+    global $pdf;
+    $fy=$y-$margin-$pdf->GetFontHeight($font_size);    
+    $left = $x+$margin;
+    $ny = $fy;
+    for ($i = 1; $i <= 10; $i++) {
+	$ly = text_wrap($left+$margin, $fy, $data[$i]['width'], $font_size,$data[$i]['title']);
+	$left = $left + $data[$i]['width']+2*$margin;
+	if ($ly<$ny) $ny=$ly;
+    }
+    $left = $x;
+    for ($i = 1; $i <= 10; $i++) {
+	$pdf->line($left, $y, $left, $ny);
+	$left = $left + $data[$i]['width']+2*$margin;
+    }
+    $pdf->line($left, $y, $left, $ny);
+    $y=$ny;
+    $pdf->line($x,$y,$x+$width,$y);
+    return($y);
+}
+
+function invoice_data($x,$y,$width) {
+    global $invoice,$pdf;
+    $font_size=7;
+    $margin = 2;
+    $pdf->setlinestyle(1);
+    $pdf->line($x,$y,$x+$width,$y);
+    $header[1]['title'] = "Lp.";
+    $header[2]['title'] = "Nazwa wyrobu, towaru lub us³ugi:";
+    $header[3]['title'] = "PKWiU:";
+    $header[4]['title'] = "JM:";
+    $header[5]['title'] = "Ilo¶æ:";
+    $header[6]['title'] = "Cena jedn. netto:";
+    $header[7]['title'] = "Warto¶æ netto:";
+    $header[8]['title'] = "St. PTU:";
+    $header[9]['title'] = "Kwota PTU:";
+    $header[10]['title'] = "Warto¶æ brutto:";
+    for ($i = 1; $i <= 10; $i++) $header[$i]['width']=$pdf->getTextWidth($font_size,$header[$i]['title'])+2*$margin;
+    // tutaj jeszcze trzeba bêdzie sprawdziæ jak± szeroko¶æ maj± pola w tabelce pu¼niej
+    
+    // Kolumna 2 bêdzie mia³a rozmiar ustalany dynamicznie
+    $header[2]['width'] = $width-($header[1]['width']+$header[3]['width']+$header[4]['width']+$header[5]['width']+$header[6]['width']+$header[7]['width']+$header[8]['width']+$header[9]['width']+$header[10]['width']+20*$margin);
+
+    $y = invoice_data_row($x,$y,$width,$font_size,$margin,$header);
+    $lp = 1;
+    foreach ($invoice['content'] as $item) {
+	$header[1]['title'] = $lp;
+	$header[2]['title'] = $item['description'];
+	$header[3]['title'] = $item['pkwiu'];
+	$header[4]['title'] = $item['content'];
+	$header[5]['title'] = $item['count'];
+	$header[6]['title'] = iconv("UTF-8","ISO-8859-2",moneyf($item['basevalue']));
+	$header[7]['title'] = iconv("UTF-8","ISO-8859-2",moneyf($item['totalbase']));
+	$header[8]['title'] = $item['taxvalue']." %";
+	$header[9]['title'] = iconv("UTF-8","ISO-8859-2",moneyf($item['totaltax']));
+	$header[10]['title'] = iconv("UTF-8","ISO-8859-2",moneyf($item['total']));
+	
+	$lp++;
+	$y = invoice_data_row($x,$y,$width,$font_size,$margin,$header);
+    }
+    $x = $x + 12*$margin + $header[1]['width'] + $header[2]['width'] + $header[3]['width'] + $header[4]['width'] + $header[5]['width'] + $header[6]['width'];
+    $fy=$y-$margin-$pdf->GetFontHeight($font_size);    
+    text_align_right($x-$margin,$fy,$font_size,"Razem:");
+
+    $left = $x+$margin;
+    $ny = $fy;
+    $ly = text_wrap($left+$margin, $fy, $header[7]['width'], $font_size,iconv("UTF-8","ISO-8859-2",moneyf($invoice['totalbase'])));
+    $left = $left + $header[7]['width']+2*$margin;
+    if ($ly<$ny) $ny=$ly;
+    $ly = text_wrap($left+$margin, $fy, $header[8]['width'], $font_size,"X");
+    $left = $left + $header[8]['width']+2*$margin;
+    if ($ly<$ny) $ny=$ly;
+    $ly = text_wrap($left+$margin, $fy, $header[9]['width'], $font_size,iconv("UTF-8","ISO-8859-2",moneyf($invoice['totaltax'])));
+    $left = $left + $header[9]['width']+2*$margin;
+    if ($ly<$ny) $ny=$ly;
+    $ly = text_wrap($left+$margin, $fy, $header[9]['width'], $font_size,iconv("UTF-8","ISO-8859-2",moneyf($invoice['total'])));
+    $left = $left + $header[9]['width']+2*$margin;
+    if ($ly<$ny) $ny=$ly;
+    
+    $left = $x;
+    for ($i = 7; $i <= 10; $i++) {
+	$pdf->line($left, $y, $left, $ny);
+	$left = $left + $header[$i]['width']+2*$margin;
+    }
+    $pdf->line($left, $y, $left, $ny);
+    $y=$ny;
+    $pdf->line($x,$y,$x+$header[7]['width']+$header[8]['width']+$header[9]['width']+$header[10]['width']+8*$margin,$y);
+    
+    
+    //$pdf->line($x,$y,$x,$y-20);
+}
+
 function invoice_body() {
     global $pdf;
     $pdf->addJpegFromFile('/var/www/devel/lms/lib/ezpdf/formularz.jpg',0,0,595);
@@ -194,6 +296,8 @@ function invoice_body() {
     $top=invoice_seller(30,$top);
     $top=$top-20;
     $top=invoice_buyer(30,$top);
+    $top=$top-20;
+    $top=invoice_data(30,$top,440);
     
     invoice_main_form_fill(187,3,0.4);
     invoice_simple_form_fill(14,3,0.4);
@@ -224,7 +328,6 @@ $id=$pdf->getFirstPageId();
 if($_GET['print'] == 'cached' && sizeof($_POST['marks']))
 {
 	$layout['pagetitle'] = trans('Invoices');
-	$SMARTY->display('clearheader.html');
 	foreach($_POST['marks'] as $markid => $junk)
 		if($junk)
 			$ids[] = $markid;
@@ -232,22 +335,17 @@ if($_GET['print'] == 'cached' && sizeof($_POST['marks']))
 	$which = ($_GET['which'] != '' ? $_GET['which'] : trans('ORIGINAL+COPY'));
 	foreach($ids as $idx => $invoiceid)
 	{
-		echo '<PRE>';
 		$invoice = $LMS->GetInvoiceContent($invoiceid);
 		$invoice['serviceaddr'] = $LMS->GetUserServiceAddress($invoice['customerid']);
 		foreach(split('\+', $which) as $type)
 		{
-			$SMARTY->assign('type',$type);
-			$SMARTY->assign('invoice',$invoice);
-			$SMARTY->display($LMS->CONFIG['invoices']['template_file']);
+			invoice_body();
 		}
 	}
-	$SMARTY->display('clearfooter.html');
 }
 elseif($_GET['fetchallinvoices'])
 {
 	$layout['pagetitle'] = trans('Invoices');
-	$SMARTY->display('clearheader.html');
 	$which = ($_GET['which'] != '' ? $_GET['which'] : trans('ORIGINAL+COPY'));
 	
 	$ids = $LMS->DB->GetCol('SELECT id FROM invoices 
@@ -264,12 +362,9 @@ elseif($_GET['fetchallinvoices'])
 		$invoice['serviceaddr'] = $LMS->GetUserServiceAddress($invoice['customerid']);
 		foreach(split('\+', $which) as $type)
 		{
-			$SMARTY->assign('type',$type);
-			$SMARTY->assign('invoice',$invoice);
-			$SMARTY->display($LMS->CONFIG['invoices']['template_file']);
+			invoice_body();
 		}
 	}
-	$SMARTY->display('clearfooter.html');
 }
 elseif($_GET['fetchsingle'])
 {
@@ -281,11 +376,8 @@ elseif($_GET['fetchsingle'])
 	$layout['pagetitle'] = trans('Invoice No. $0', $ntempl);
 	$invoice['last'] = TRUE;
 	$invoice['serviceaddr'] = $LMS->GetUserServiceAddress($invoice['customerid']);
-	$SMARTY->assign('invoice',$invoice);
-	$SMARTY->display('clearheader.html');
-	$SMARTY->assign('type',trans('ORIGINAL'));
-	$SMARTY->display($LMS->CONFIG['invoices']['template_file']);
-	$SMARTY->display('clearfooter.html');
+	invoice_body();
+
 }
 elseif($invoice = $LMS->GetInvoiceContent($_GET['id']))
 {
@@ -295,16 +387,11 @@ elseif($invoice = $LMS->GetInvoiceContent($_GET['id']))
 	$ntempl = str_replace('%Y',$invoice['year'],$ntempl);
 	$layout['pagetitle'] = trans('Invoice No. $0', $ntempl);
 	$invoice['serviceaddr'] = $LMS->GetUserServiceAddress($invoice['customerid']);
-	//$SMARTY->assign('invoice',$invoice);
 	$type = trans('ORIGINAL');
-	//$SMARTY->display($LMS->CONFIG['invoices']['template_file']);
 	invoice_body();
 	$type = trans('COPY');
 	$invoice['last'] = TRUE;
-	//$SMARTY->assign('invoice',$invoice);
-	//$SMARTY->display($LMS->CONFIG['invoices']['template_file']);
 	invoice_body();
-	//$SMARTY->display('clearfooter.html');
 }
 else
 {
@@ -316,122 +403,3 @@ global $pdf;
 $pdf->ezStream();
 
 ?>
-<?php
-
-/*
- * LMS Userpanel
- *
- *  (C) Copyright 2004 LMS Developers
- *
- *  Please, see the doc/AUTHORS for more information about authors!
- *
- *  This program is licensed under LMS License. Please, see
- *  doc/LICENSE.pl file for information about copyright notice.
- *
- *  $Id$
- 
-
-//include ('class.ezpdf.php');
-//require_once($_LIB_DIR.'/ezpdf/class.ezpdf.php');
-
-//echo "dupa";
-
-setlocale(LC_ALL,'en_US');
-
-require_once('/var/www/devel/lms/lib/ezpdf/class.ezpdf.php');
-
-
-$diff=array(177=>'aogonek',161=>'Aogonek',230=>'cacute',198=>'Cacute',234=>'eogonek',202=>'Eogonek',
-241=>'nacute',209=>'Nacute',179=>'lslash',163=>'Lslash',182=>'sacute',166=>'Sacute',
-188=>'zacute',172=>'Zacute',191=>'zdot',175=>'Zdot'); 
-
-//$pdf =& new Cezpdf('A4','landscape');
-$pdf =& new Cezpdf('A4','portrait');
-$pdf->addInfo('Producer','LMS Developers');
-$pdf->addInfo('Title','Druki przelewu/wplaty');
-$pdf->addInfo('Creator','LMS wersja 0.1');
-$pdf->ezSetMargins(0,0,0,0);
-//$pdf->selectFont('arial.afm',array('encoding'=>'WinAnsiEncoding','differences'=>$diff)); 
-$pdf->selectFont('/var/www/devel/lms/lib/ezpdf/arial.afm',array('encoding'=>'WinAnsiEncoding','differences'=>$diff)); 
-$pdf->setLineStyle(2);
-$id=$pdf->getFirstPageId();
-
-function simple_fill($x,$y,$scale)  {
-    global $pdf,$_NAME,$_ADDRESS,$_ZIP,$_CITY,$_ACCOUNT,$userinfo,$_SERVICE,$_SHORT_NAME,$control_lines;
-
-        $pdf->line(7*$scale+$x,724*$scale+$y,7*$scale+$x,694*$scale+$y);
-        $pdf->line(7*$scale+$x,724*$scale+$y,37*$scale+$x,724*$scale+$y);
-        $pdf->line(370*$scale+$x,724*$scale+$y,370*$scale+$x,694*$scale+$y);
-        $pdf->line(370*$scale+$x,724*$scale+$y,340*$scale+$x,724*$scale+$y);
-        $pdf->line(7*$scale+$x,197*$scale+$y,7*$scale+$x,227*$scale+$y);
-        $pdf->line(7*$scale+$x,197*$scale+$y,37*$scale+$x,197*$scale+$y);
-        $pdf->line(370*$scale+$x,197*$scale+$y,370*$scale+$x,227*$scale+$y);
-        $pdf->line(370*$scale+$x,197*$scale+$y,340*$scale+$x,197*$scale+$y);
-    
-    $pdf->addtext(15*$scale+$x,560*$scale+$y,30*$scale,$_SHORT_NAME);
-    $pdf->addtext(15*$scale+$x,525*$scale+$y,30*$scale, $_ADDRESS);
-    $pdf->addtext(15*$scale+$x,490*$scale+$y,30*$scale, $_ZIP." ".$_CITY);
-    $pdf->addtext(15*$scale+$x,680*$scale+$y,30*$scale, substr($_ACCOUNT,0,17));
-    $pdf->addtext(15*$scale+$x,620*$scale+$y,30*$scale, substr($_ACCOUNT,18,200));
-    $pdf->addtext(15*$scale+$x,435*$scale+$y,30*$scale,"**".number_format(-$userinfo['balance'],2,',','')."**");
-    //$pdf->addtext(15*$scale+$x,310*$scale+$y,30*$scale,$userinfo['username']);
-
-    $font_size=30;
-    while ($pdf->getTextWidth($font_size*$scale,$userinfo['username'])>135)
-        $font_size=$font_size-1;
-    $pdf->addtext(15*$scale+$x,310*$scale+$y,$font_size*$scale,$userinfo['username']);
-    $font_size=30;
-    while ($pdf->getTextWidth($font_size*$scale,$userinfo['address'])>135)
-        $font_size=$font_size-1;
-    $pdf->addtext(15*$scale+$x,275*$scale+$y,$font_size*$scale,$userinfo['address']);
-    $pdf->addtext(15*$scale+$x,240*$scale+$y,30*$scale,$userinfo['zip']." ".$userinfo['city']);
-
-    $font_size=30;
-    while ($pdf->getTextWidth($font_size*$scale,"Op³ata za rachunek: ".$userinfo['rachunek'])>135)
-        $font_size=$font_size-1;
-    $pdf->addtext(15*$scale+$x,385*$scale+$y,$font_size*$scale,"Op³ata za rachunek: ".$userinfo['rachunek']);
-
-}
-
-function main_fill($x,$y,$scale)	{
-    global $pdf,$_NAME,$_ADDRESS,$_ZIP,$_CITY,$_ACCOUNT,$userinfo,$_SERVICE;
-    $pdf->line(7*$scale+$x,724*$scale+$y,7*$scale+$x,694*$scale+$y);
-    $pdf->line(7*$scale+$x,724*$scale+$y,37*$scale+$x,724*$scale+$y);
-    $pdf->line(970*$scale+$x,724*$scale+$y,970*$scale+$x,694*$scale+$y);
-    $pdf->line(970*$scale+$x,724*$scale+$y,940*$scale+$x,724*$scale+$y);
-    $pdf->line(7*$scale+$x,172*$scale+$y,7*$scale+$x,202*$scale+$y);
-    $pdf->line(7*$scale+$x,172*$scale+$y,37*$scale+$x,172*$scale+$y);
-
-    $pdf->addtext(15*$scale+$x,680*$scale+$y,30*$scale,$_NAME);
-    $pdf->addtext(15*$scale+$x,617*$scale+$y,30*$scale, $_ADDRESS." ".$_ZIP." ".$_CITY);
-    $pdf->addtext(15*$scale+$x,555*$scale+$y,30*$scale, $_ACCOUNT);
-    $pdf->addtext(550*$scale+$x,495*$scale+$y,30*$scale,number_format(-$userinfo['balance'],2,',',''));
-    $pdf->addtext(15*$scale+$x,372*$scale+$y,30*$scale,$userinfo['username']);
-    $pdf->addtext(15*$scale+$x,312*$scale+$y,30*$scale,$userinfo['address']." ".$userinfo['zip']." ".$userinfo['city']);
-    $pdf->addtext(15*$scale+$x,250*$scale+$y,30*$scale,$_SERVICE);
-
-}
-
-$userinfo['balance']=-10;
-$userinfo['username']="Za¿ó³æ gê¶l± ja¼ñ";
-$userinfo['address']="Za¿ó³æ gê¶l± ja¼ñ";
-// Dobra, czytamy z lms.ini
-$_NAME = (! $_CONFIG[finances]['name'] ? "Nazwa nieustawiona" : $_CONFIG[finances]['name']);
-$_ADDRESS = (! $_CONFIG[finances]['address'] ? "Adres nieustawiony" : $_CONFIG[finances]['address']);
-$_ZIP = (! $_CONFIG[finances]['zip'] ? "00-000" : $_CONFIG[finances]['zip']);
-$_CITY = (! $_CONFIG[finances]['city'] ? "Miasto nieustawione" : $_CONFIG[finances]['city']);
-$_SERVICE = (! $_CONFIG[finances]['service'] ? "Nazwa us³ugi nieustawiona" : $_CONFIG[finances]['service']);
-$_ACCOUNT = (! $_CONFIG[finances]['account'] ? "123456789012345678901234567" : $_CONFIG[finances]['account']);
-
-
-//$pdf->addPngFromFile('przelew.png',20,0,418);
-$pdf->addJpegFromFile('/var/www/devel/lms/lib/ezpdf/formularz.jpg',0,0,595);
-main_fill(187,3,0.4);
-simple_fill(14,3,0.4);
-//$id=$pdf->newPage(1,$id,'after');
-
-$pdf->ezStream();
-
-?>
-							    
-*/
