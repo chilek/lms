88369e7613805d59f8bfd791af8e1f96965e9f09
Aleksander Machniak <alec@alec.pl>
2005-01-31 19:29:35 +0000

    - some fixes and number checks

--- modules/invoicenew.php
+++ modules/invoicenew.php
@@ -87,33 +87,35 @@ switch($_GET['action'])
 			$invoice['paytime'] = 14;
 
 		$invoice['number'] = $_POST['invoice']['number'];
-
+		$invoice['userid'] = $_POST['userid'];
+		
 		if($invoice['cdate']) // && !$invoice['cdatewarning'])
 		{
 			list($year, $month, $day) = split('/',$invoice['cdate']);
 			if(checkdate($month, $day, $year)) 
 			{
 				$invoice['cdate'] = mktime(date('G',time()),date('i',time()),date('s',time()),$month,$day,$year);
-				echo $invoice['year'];
+				
 				if (($oldmonth!=$month) || ($oldyear!=$year))
 				{
-					if($this->CONFIG['invoices']['monthly_numbering'])
-			    			{
-				            		$start = mktime(0, 0, 0, date('n',$invoice['cdate']), 1, date('Y',$invoice['cdate']));
-			    				$end = mktime(0, 0, 0, date('n',$invoice['cdate'])+1, 1, date('Y',$invoice['cdate']));
-						}
+					if($LMS->CONFIG['invoices']['monthly_numbering'])
+			    		{
+				        	$start = mktime(0, 0, 0, date('n',$invoice['cdate']), 1, date('Y',$invoice['cdate']));
+			    			$end = mktime(0, 0, 0, date('n',$invoice['cdate'])+1, 1, date('Y',$invoice['cdate']));
+					}
 					else
-				    		{
-				            		$start = mktime(0, 0, 0, 1, 1, date('Y',$invoice['cdate']));
-		            				$end = mktime(0, 0, 0, 1, 1, date('Y',$invoice['cdate'])+1);
-				    		}
+				    	{
+				        	$start = mktime(0, 0, 0, 1, 1, date('Y',$invoice['cdate']));
+		            			$end = mktime(0, 0, 0, 1, 1, date('Y',$invoice['cdate'])+1);
+				    	}
 						
 					$number = $LMS->DB->GetOne('SELECT MAX(number) FROM invoices WHERE cdate >= ? AND cdate < ?', array($start, $end));
-					if (!$number) $number=0;
-					$invoice['number']=++$number;
+					$invoice['number'] = $number ? ++$number : 1;
 					$invoice['month'] = $month;
 					$invoice['year']  = $year;
-				} else {
+				} 
+				else 
+				{
 					$invoice['month'] = $oldmonth;
 					$invoice['year']  = $oldyear;
 				}
@@ -131,31 +133,41 @@ switch($_GET['action'])
 				$invoice['cdatewarning'] = 1;
 			}
 		}
-		
-		$invoice['userid'] = $_POST['userid'];
-		
-		if($this->CONFIG['invoices']['monthly_numbering'])
-			{
-		    		$start = mktime(0, 0, 0, date('n',$invoice['cdate']), 1, date('Y',$invoice['cdate']));
-				$end = mktime(0, 0, 0, date('n',$invoice['cdate'])+1, 1, date('Y',$invoice['cdate']));
-			}
-				else
-			{
-		    		$start = mktime(0, 0, 0, 1, 1, date('Y',$invoice['cdate']));
-				$end = mktime(0, 0, 0, 1, 1, date('Y',$invoice['cdate'])+1);
-			}
-						
-		if ($LMS->DB->GetOne('SELECT number FROM invoices WHERE cdate >= ? AND cdate < ? AND number = ?', array($start, $end, $invoice['number'])))
+
+		if($LMS->CONFIG['invoices']['monthly_numbering'])
 		{
-			$error['number'] = trans('This invoice number is already taken!');
+			$start = mktime(0, 0, 0, date('n',$invoice['cdate']), 1, date('Y',$invoice['cdate']));
+			$end = mktime(0, 0, 0, date('n',$invoice['cdate'])+1, 1, date('Y',$invoice['cdate']));
+		}
+		else
+		{
+			$start = mktime(0, 0, 0, 1, 1, date('Y',$invoice['cdate']));
+			$end = mktime(0, 0, 0, 1, 1, date('Y',$invoice['cdate'])+1);
+		}
+		
+		if(!$invoice['number'])
+		{
 			$number = $LMS->DB->GetOne('SELECT MAX(number) FROM invoices WHERE cdate >= ? AND cdate < ?', array($start, $end));
-			if (!$number) $number=0;
-			$invoice['number']=++$number;			
+			$invoice['number'] = $number ? ++$number : 1;
+		}
+		
+		if(!eregi('^[0-9]+$',$invoice['number']))
+		{
+			$error['number'] = trans('Invoice number must be integer!');
+			break;
+		}
+		
+		if ($LMS->DB->GetOne('SELECT number FROM invoices WHERE cdate >= ? AND cdate < ? AND number = ?', array($start, $end, $invoice['number'] ? $invoice['number'] : 0)))
+		{
+				$error['number'] = trans('Invoice number $0 already exists!', $invoice['number']);
+				$number = $LMS->DB->GetOne('SELECT MAX(number) FROM invoices WHERE cdate >= ? AND cdate < ?', array($start, $end));
+				$invoice['number'] = $number ? ++$number : 1;
 		}
 
 		if(!$error)
 			if($LMS->UserExists(($_GET['userid'] != '' ? $_GET['userid'] : $_POST['user'])))
 				$customer = $LMS->GetUser(($_GET['userid'] != '' ? $_GET['userid'] : $_POST['user']));
+
 	break;
 
 	case 'save':
@@ -179,19 +191,20 @@ switch($_GET['action'])
     		unset($contents);
     		unset($customer);
     		unset($error);
-		if($this->CONFIG['invoices']['monthly_numbering'])
-	    		{
-		                $start = mktime(0, 0, 0, date('n',time()), 1, date('Y',time()));
-			        $end = mktime(0, 0, 0, date('n',time())+1, 1, date('Y',time()));
-			}
+		
+		if($LMS->CONFIG['invoices']['monthly_numbering'])
+	    	{
+		        $start = mktime(0, 0, 0, date('n',time()), 1, date('Y',time()));
+		        $end = mktime(0, 0, 0, date('n',time())+1, 1, date('Y',time()));
+		}
 		else
-		        {
-		                $start = mktime(0, 0, 0, 1, 1, date('Y',time()));
-		                $end = mktime(0, 0, 0, 1, 1, date('Y',time())+1);
-		        }
+		{
+		        $start = mktime(0, 0, 0, 1, 1, date('Y',time()));
+		        $end = mktime(0, 0, 0, 1, 1, date('Y',time())+1);
+		}
 
 		$number = $LMS->DB->GetOne('SELECT MAX(number) FROM invoices WHERE cdate >= ? AND cdate < ?', array($start, $end));
-		$invoice['number']=++$number;
+		$invoice['number'] = $number ? ++$number : 1;
 		$invoice['month'] = date("m");
 		$invoice['year']  = date("Y");
 		$invoice['cdate'] = time();
