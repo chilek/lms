9b561236f054a4b7540075ccb8fb48d16b61dd77
Aleksander Machniak <alec@alec.pl>
2005-02-06 20:40:22 +0000

    - unpaid covenants - zajawka

---
+++ modules/choosecovenants.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * LMS version 1.5-cvs
+ *
+ *  (C) Copyright 2001-2005 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$layout['pagetitle'] = trans('Select Unpaid Covenants');
+
+$userid = $_GET['id'];
+
+if(isset($_POST['marks']))
+{
+	$_SESSION['unpaid'][$userid] = $_POST['marks'];
+	die;
+}
+
+if($covenantlist = $LMS->DB->GetAll('SELECT invoiceid, itemid, MIN(cdate) AS cdate, 
+			SUM(CASE type WHEN 3 THEN value ELSE value*-1 END)*-1 AS value
+			FROM cash LEFT JOIN invoices ON (invoiceid = invoices.id)
+			WHERE userid = ? AND invoiceid > 0 AND itemid > 0
+			GROUP BY invoiceid, itemid
+			HAVING SUM(CASE type WHEN 3 THEN value ELSE value*-1 END)*-1 > 0
+			ORDER BY cdate', array($userid)))
+{
+	foreach($covenantlist as $idx => $row)
+	{
+		$record = $LMS->DB->GetRow('SELECT cash.id AS id, number, taxvalue, comment
+					    FROM cash LEFT JOIN invoices ON (invoiceid = invoices.id)
+					    WHERE invoiceid = ? AND itemid = ? AND type = 4',
+					    array($row['invoiceid'], $row['itemid']));
+		
+		$record['invoice'] = $_CONFIG['invoices']['number_template'];
+		$record['invoice'] = str_replace('%M', date('m', $row['cdate']), $record['invoice']);
+		$record['invoice'] = str_replace('%Y', date('Y', $row['cdate']), $record['invoice']);
+		$record['invoice'] = str_replace('%N', $record['number'], $record['invoice']);
+
+		if(in_array($record['id'], (array) $_SESSION['unpaid'][$userid]))
+			$record['selected'] = TRUE;
+		
+		$covenantlist[$idx] = array_merge($record, $covenantlist[$idx]);
+	}
+}
+
+unset($_SESSION['unpaid'][$userid]);
+
+$SMARTY->assign('covenantlist',$covenantlist);
+$SMARTY->assign('userid', $userid);
+$SMARTY->display('choosecovenants.html');
+
+?>
