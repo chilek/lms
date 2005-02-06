06a01799e16c5ff70057cadeaa0dd57b11f0da14
Aleksander Machniak <alec@alec.pl>
2005-02-06 20:40:22 +0000

    - unpaid covenants - zajawka

---
+++ lib/upgradedb/postgres.2005020600.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * LMS version 1.5-cvs
+ *
+ *  (C) Copyright 2001-2005 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$DB->BeginTrans();
+$DB->Execute("
+	CREATE INDEX cash_invoiceid_idx ON cash (invoiceid);
+	UPDATE dbinfo SET keyvalue = '2005020600' WHERE keytype = 'dbversion'
+");
+$DB->CommitTrans();
+
+?>
