5ae72557a4bd5a9f43c760bef3586a7d6bd7bce9
Aleksander Machniak <alec@alec.pl>
2005-02-09 19:09:51 +0000

    - now works with international libs

--- contrib/customer_otherip/class.php
+++ contrib/customer_otherip/class.php
@@ -18,60 +18,71 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
  *  USA.
  *
  *  $Id$
  */
 
-// REPLACE THIS WITH PATH TO YOURS CONFIG FILE
+// REPLACE THIS WITH PATH TO YOU CONFIG FILE
 
 $CONFIG_FILE = (is_readable('lms.ini')) ? 'lms.ini' : '/etc/lms/lms.ini';
 
 // PLEASE DO NOT MODIFY ANYTHING BELOW THIS LINE UNLESS YOU KNOW
 // *EXACTLY* WHAT ARE YOU DOING!!!
+// *******************************************************************
 
 // Parse configuration file
 
-function lms_parse_ini_file($filename, $process_sections = false)
+function lms_parse_ini_file($filename, $process_sections = false) 
 {
 	$ini_array = array();
-	$sec_name = "";
+	$section = '';
 	$lines = file($filename);
-	foreach($lines as $line)
+	foreach($lines as $line) 
 	{
 		$line = trim($line);
-
-		if($line == "" || $line[0] == ";" || $line[0] == "#")
+		
+		if($line == '' || $line[0] == ';' || $line[0] == '#') 
 			continue;
-
-		if( sscanf($line, "[%[^]]", &$sec_name)==1 )
-			$sec_name = trim($sec_name);
-		else
+		
+		list($sec_name) = sscanf($line, "[%[^]]");
+		
+		if( $sec_name )
+			$section = trim($sec_name);
+		else 
 		{
-			if ( sscanf($line, "%[^=] = '%[^']'", &$property, &$value) != 2 )
-				if ( sscanf($line, "%[^=] = \"%[^\"]\"", &$property, &$value) != 2 )
-					if( sscanf($line, "%[^=] = %[^;#]", &$property, &$value) != 2 )
+			list($property, $value) = sscanf($line, "%[^=] = '%[^']'");
+			if ( !$property || !$value ) 
+			{
+				list($property, $value) = sscanf($line, "%[^=] = \"%[^\"]\"");
+				if ( !$property || !$value ) 
+				{
+					list($property, $value) = sscanf($line, "%[^=] = %[^;#]");
+					if( !$property || !$value ) 
 						continue;
 					else
 						$value = trim($value, "\"'");
-
+				}
+			}
+		
 			$property = trim($property);
 			$value = trim($value);
-
-			if($process_sections)
-				$ini_array[$sec_name][$property] = $value;
-			else
+			
+			if($process_sections) 
+				$ini_array[$section][$property] = $value;
+			else 
 				$ini_array[$property] = $value;
 		}
 	}
+	
 	return $ini_array;
 }
 
-foreach(lms_parse_ini_file($CONFIG_FILE, true) as $key=>$val) $_CONFIG[$key] = $val;
+foreach(lms_parse_ini_file($CONFIG_FILE, true) as $key => $val)
+	$_CONFIG[$key] = $val;
 
-// Define directories and configuration vars
-
+// Check for configuration vars and set default values
 $_CONFIG['directories']['sys_dir'] = (! $_CONFIG['directories']['sys_dir'] ? getcwd() : $_CONFIG['directories']['sys_dir']);
 $_CONFIG['directories']['backup_dir'] = (! $_CONFIG['directories']['backup_dir'] ? $_CONFIG['directories']['sys_dir'].'/backups' : $_CONFIG['directories']['backup_dir']);
 $_CONFIG['directories']['lib_dir'] = (! $_CONFIG['directories']['lib_dir'] ? $_CONFIG['directories']['sys_dir'].'/lib' : $_CONFIG['directories']['lib_dir']);
@@ -93,45 +104,55 @@ $_MODULES_DIR = $_CONFIG['directories']['modules_dir'];
 $_SMARTY_DIR = $_CONFIG['directories']['smarty_dir'];
 $_SMARTY_COMPILE_DIR = $_CONFIG['directories']['smarty_compile_dir'];
 $_SMARTY_TEMPLATES_DIR = $_CONFIG['directories']['smarty_templates_dir'];
-//$_TIMEOUT = $_CONFIG['phpui']['timeout'];
-//$_FORCE_SSL = chkconfig($_CONFIG['phpui']['force_ssl']);
 $_DBTYPE = $_CONFIG['database']['type'];
 $_DBHOST = $_CONFIG['database']['host'];
 $_DBUSER = $_CONFIG['database']['user'];
 $_DBPASS = $_CONFIG['database']['password'];
 $_DBNAME = $_CONFIG['database']['database'];
 
-// Set our sweet polish locales :>
+// Init database 
 
-//setlocale (LC_ALL, 'pl_PL');
-
-// include required files
-
-require_once($_SMARTY_DIR.'/Smarty.class.php');
 require_once($_LIB_DIR.'/LMSDB.php');
-require_once($_LIB_DIR.'/common.php');
-require_once($_LIB_DIR.'/LMS.class.php');
-
-// Initialize LMSDB object
 
 $DB = DBInit($_DBTYPE, $_DBHOST, $_DBUSER, $_DBPASS, $_DBNAME);
 
-// Initialize database and template classes
+// Read configuration of LMS-UI from database
 
-$SESSION = NULL;
+if($cfg = $DB->GetAll('SELECT section, var, value FROM uiconfig WHERE disabled=0'))
+	foreach($cfg as $row)
+		$_CONFIG[$row['section']][$row['var']] = $row['value'];
 
-$LMS = new LMS($DB,$SESSION,$_CONFIG);
+// Initialize templates engine
+
+require_once($_SMARTY_DIR.'/Smarty.class.php');
 
 $SMARTY = new Smarty;
 
-// test for proper version of Smarty
+// Include required files (including sequence is important)
 
+require_once($_LIB_DIR.'/language.php');
+require_once($_LIB_DIR.'/common.php');
+require_once($_LIB_DIR.'/LMS.class.php');
+
+// Initialize LMS class
+
+$SESSION = NULL;
+
+$LMS = new LMS($DB, $SESSION, $_CONFIG);
+$LMS->lang = $_language;
+
+// set some template and layout variables
+
+$SMARTY->assign_by_ref('_LANG', $_LANG);
+$SMARTY->assign_by_ref('LANGDEFS', $LANGDEFS);
+$SMARTY->assign_by_ref('_language', $LMS->lang);
 $SMARTY->template_dir = getcwd();
 $SMARTY->compile_dir = $_SMARTY_COMPILE_DIR;
 
-$layout[lmsv]='1.5-cvs';
+require_once($_LIB_DIR.'/smarty_addons.php');
 
-$SMARTY->assign('menu',$menu);
-$SMARTY->assign('layout',$layout);
+$layout['lmsv'] = '1.5-cvs';
+
+$SMARTY->assign_by_ref('layout', $layout);
 
 ?>
