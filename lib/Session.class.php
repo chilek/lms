c637b755092a31f15a3b9651938e98838ee3242d
Łukasz Jarosław Mozer <baseciq@netx.waw.pl>
2005-03-09 11:42:59 +0000

    - new version

--- lib/Session.class.php
+++ lib/Session.class.php
@@ -3,144 +3,168 @@
 /*
  * LMS version 1.5-cvs
  *
- *  (C) Copyright 2001-2005 LMS Developers
+ *  (C) Copyright 2004 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License Version 2 as
- *  published by the Free Software Foundation.
+ *  This program is licensed under LMS Public License. Please, see
+ *  doc/LICENSE.en file for information about copyright notice.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
- *  USA.
+ *  LMS Public License for more details.
  *
  *  $Id$
  */
 
 class Session {
 
-	var $id;
-	var $login;
-	var $logname;
-	var $passwd;
-	var $islogged = FALSE;
-	var $last;
-	var $lastip;
-	var $error;
-	var $_version = '1.5-cvs';
-	var $_revision = '$Revision$';
-
-	function Session(&$DB,$timeout = 600)
+	var $SID = NULL;			// session unique ID
+	var $_version = '1.5-cvs';		// library version
+	var $_revision = '$Revision$';	// library revision
+	var $_content = array();		// session content array
+	var $_updated = FALSE;			// indicates that content has
+						// been altered
+	var $DB = NULL;				// database library object
+	var $CONFIG = NULL;			// configuration array
+	var $timeout = 600;			// timeout since session will
+						// be destroyed
+	var $autoupdate = FALSE;		// do automatic update on each
+						// save() or save_by_ref() ?
+	var $GCprob = 10;			// probality (in percent) of
+						// garbage collector procedure
+	
+	function Session(&$DB, &$CONFIG)
 	{
-		session_start();
-		$this->DB = &$DB;
-		if($_GET['override'])
-			$loginform = $_GET['loginform'];
-		else
-			$loginform = $_POST['loginform'];
-		if(isset($loginform))
-		{
-			$this->login = $loginform['login'];
-			$this->passwd = $loginform['pwd'];
-			$_SESSION['session_timestamp'] = time();
-			writesyslog('Login attempt by '.$this->login, LOG_INFO);
-		}
-		elseif($this->DB->GetOne('SELECT COUNT(id) FROM admins') == 0)
-		{
-			$this->islogged = TRUE;
-			$this->passwd = 'EMPTY';
-			$this->logname = trans('not logged in');
-			$_GET['m'] = 'adminadd';
-			return TRUE;
-		}
-		else
-		{
-			$this->login = $_SESSION['session_login'];
-			$this->passwd = $_SESSION['session_passwd'];
-		}
+		$this->DB =& $DB;
+		$this->CONFIG =& $CONFIG;
 		
-		if($this->VerifyPassword() && $this->TimeOut($timeout))
-		{
-			$this->islogged = TRUE;
-			$admindata = $this->DB->GetRow('SELECT id, name FROM admins WHERE login=?',array($this->login));
-			$this->id = $admindata['id'];
-			$this->logname = $admindata['name'];
-			$this->last = $_SESSION['session_last'];
-			$this->lastip = $_SESSION['session_lastip'];
-			if(isset($loginform))
-			{
-				$admindata = $this->DB->GetRow('SELECT lastlogindate, lastloginip FROM admins WHERE id=?',array($this->id));
-				$this->last = $admindata['lastlogindate'];
-				$this->lastip = $admindata['lastloginip'];
-				
-				$this->DB->Execute('UPDATE admins SET lastlogindate=?, lastloginip=? WHERE id=?', array(time(),$_SERVER['REMOTE_ADDR'],$this->id));
-				writesyslog('User '.$this->login.' logged in.', LOG_INFO);
-			}
-			$_SESSION['session_login'] = $this->login;
-			$_SESSION['session_passwd'] = $this->passwd;
-			$_SESSION['session_last'] = $this->last;
-			$_SESSION['session_lastip'] = $this->lastip;
-		}else{
-			$this->islogged = FALSE;
-			if(isset($loginform))
-			{
-				writesyslog('Bad password for '.$this->login, LOG_WARNING);
-				$this->DB->Execute('UPDATE admins SET failedlogindate=?, failedloginip=? WHERE login=?',array(time(),$_SERVER['REMOTE_ADDR'],$this->login));
-			}
-			$this->LogOut();
-		}
+		if(! isset($_COOKIE['SID']))
+			$this->_createSession();
+		else
+			$this->_restoreSession();
+
+		if(rand(1,100) <= $this->GCprob)
+			$this->_garbageCollector();
+
+		if(isset($this->CONFIG['phpui']['timeout']))
+			$this->timeout =& $this->CONFIG['phpui']['timeout'];
 	}
 
-	function _postinit()
+	function close()
 	{
+		$this->_saveSession();
+		$this->SID = NULL;
+		$this->_content = array();
+	}
+
+	function finish()
+	{
+		$this->_destroySession();
+	}
+
+	function makeSID()
+	{
+		list($usec, $sec) = split(' ', microtime());
+		return md5(uniqid(rand(), true)).sprintf('%09x', $sec).sprintf('%07x', ($usec * 10000000));
+	}
+
+	function save($variable, $content)
+	{
+		$this->_content[$variable] = $content;
+		if($this->autoupdate)
+			$this->_saveSession();
+		else
+			$this->_updated = TRUE;
+	}
+
+	function save_by_ref($variable, &$content)
+	{
+		$this->_content[$variable] =& $content;
+		if($this->autoupdate)
+			$this->_saveSession();
+		else
+			$this->_updated = TRUE;
+	}
+
+	function restore($variable, &$content)
+	{
+		if(isset($this->_content[$variable]))
+			$content = $this->_content[$variable];
+		else
+			return NULL;
+	}
+
+	function _createSession()
+	{
+		$this->SID = $this->makeSID();
+		$this->_content = array();
+		$this->DB->Execute('INSERT INTO sessions (id, ctime, mtime, atime, vdata, content) VALUES (?, ?NOW?, ?NOW?, ?NOW?, ?, ?)', array($this->SID, serialize($this->makeVData()), serialize($this->_content)));
+		setcookie('SID', $this->SID);
+	}
+
+	function _restoreSession()
+	{
+		$this->SID = $_COOKIE['SID'];
+		if(
+				$this->DB->GetOne('SELECT id FROM sessions WHERE id = ?', array($this->SID)) &&
+				serialize($this->makeVData()) == $this->DB->GetOne('SELECT vdata FROM sessions WHERE id = ?', array($this->SID)) &&
+				$this->_testSessionTimeout()
+		  )
+		{
+			$this->DB->Execute('UPDATE sessions SET atime = ?NOW? WHERE id = ?', array($this->SID));
+			$this->_content = unserialize($this->DB->GetOne('SELECT content FROM sessions WHERE id = ?', array($this->SID)));
+			
+			
+		}
+		else
+			$this->_createSession();
+	}
+
+	function _testSessionTimeout()
+	{
+		if($this->DB->GetOne('SELECT * FROM sessions WHERE id = ? AND mtime < ?NOW? - ? AND atime < ?NOW? - ?', array($this->SID, $this->timeout, $this->timeout)))
+		{
+			$this->_destroySession();
+			return FALSE;
+		}
+		else
+			return TRUE;
+	}
+
+	function _saveSession()
+	{
+		if($this->autoupdate || $this->_updated)
+			$this->DB->Execute('UPDATE sessions SET content = ?, mtime = ?NOW? WHERE id = ?', array(serialize($this->_content), $this->SID));
+	}
+
+	function _destroySession()
+	{
+		$this->DB->Execute('DELETE FROM sessions WHERE id = ?', array($this->SID));
+		$this->_content = array();
+		$this->SID = NULL;
+	}
+
+	function _garbageCollector()
+	{
+		
+		// usuwa sesje które dosiêgn± timeout
+		$this->DB->Execute('DELETE FROM sessions WHERE atime < ?NOW? - ? AND mtime < ?NOW? - ?', array($this->timeout, $this->timeout)); 
 		return TRUE;
 	}
 
-	function LogOut()
+	function makeVData()
 	{
-		if ($this->islogged)
-			writesyslog('User '.$this->login.' logged out.',LOG_INFO);
-		session_destroy();
-		unset($this->login);
-		unset($this->password);
-		unset($_SESSION);
-	}		
-	
-	function TimeOut($timeout = 600)
-	{
-		if( (time() - $_SESSION['session_timestamp']) > $timeout )
-		{
-			$this->error = trans('Exceeded idle time limit ($0 sec.)',$timeout);
-			return FALSE;
-		}
+		foreach(array('REMOTE_ADDR', 'REMOTE_HOST', 'HTTP_USER_AGENT', 'HTTP_VIA', 'HTTP_X_FORWARDED_FOR', 'SERVER_NAME', 'SERVER_PORT') as $vkey)
+			if(isset($_SERVER[$vkey]))
+				$vdata[$vkey] = $_SERVER[$vkey];
+		if(isset($vdata))
+			return $vdata;
 		else
-		{
-			$_SESSION['session_timestamp'] = time();
-			return TRUE;
-		}
+			return NULL;
 	}
-	
-	function VerifyPassword()
-	{
-		$dbpasswd = $this->DB->GetOne('SELECT passwd FROM admins WHERE login=? AND deleted=0',array($this->login));
-		if (crypt($this->passwd,$dbpasswd)==$dbpasswd)
-			return TRUE;
-		else 
-		{
-			if(isset($this->login))
-				$this->error = trans('Wrong password or login.');
-			else
-				$this->error = trans('Login yourself, please.');
-			return FALSE;
-		}
-	}
+
 }
 
 ?>
