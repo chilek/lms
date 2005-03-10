ae7d750922e5b9f1b72bf0d08cc8cbcb518abc52
Aleksander Machniak <alec@alec.pl>
2005-03-10 20:07:40 +0000

    - fixed copyrights

--- lib/Session.class.php
+++ lib/Session.class.php
@@ -3,20 +3,27 @@
 /*
  * LMS version 1.5-cvs
  *
- *  (C) Copyright 2004 LMS Developers
+ *  (C) Copyright 2001-2005 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
- *  This program is licensed under LMS Public License. Please, see
- *  doc/LICENSE.en file for information about copyright notice.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  LMS Public License for more details.
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
  *
  *  $Id$
  */
+
 
 class Session {
 
