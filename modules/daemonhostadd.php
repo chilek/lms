ef7b873800e5cbbca78e9bf2ecf690e90998ab8b
Aleksander Machniak <alec@alec.pl>
2005-03-31 19:29:32 +0000

    - daemon(s) online configuration & management

---
+++ modules/daemonhostadd.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * LMS version 1.5-cvs
+ *
+ *  (C) Copyright 2001-2005 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+function GetHostIdByName($name)
+{
+	global $LMS;
+	return $LMS->DB->GetOne('SELECT id FROM daemonhosts WHERE name = ?', array($name));
+}
+
+if($hostadd = $_POST['hostadd']) 
+{
+	$hostadd['name'] = trim($hostadd['name']);
+	$hostadd['description'] = trim($hostadd['description']);
+	
+	if($hostadd['name']=='' && $hostadd['description']=='')
+	{
+		$SESSION->redirect('?m=daemonhostlist');
+	}
+	
+	if($hostadd['name'] == '')
+		$error['name'] = trans('Host name is required!');
+	elseif(GetHostIdByName($hostadd['name']))
+		$error['name'] = trans('Host with specified name exists!');
+	
+	if(!$error)
+	{
+		$LMS->DB->Execute('INSERT INTO daemonhosts (name, description) VALUES (?,?)',
+				    array($hostadd['name'], $hostadd['description']));
+		$LMS->SetTS('daemonhosts');
+		
+		if(!$hostadd['reuse'])
+		{
+			$SESSION->redirect('?m=daemonhostlist');
+		}
+		
+		unset($hostadd['name']);
+		unset($hostadd['description']);
+	}
+
+}	
+
+$layout['pagetitle'] = trans('New Host');
+
+$SESSION->save('backto', $_SERVER['QUERY_STRING']);
+
+$SMARTY->assign('error', $error);
+$SMARTY->assign('hostadd', $hostadd);
+$SMARTY->assign('layout', $layout);
+$SMARTY->display('daemonhostadd.html');
+
+?>
---
+++ modules/daemonhostdel.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * LMS version 1.5-cvs
+ *
+ *  (C) Copyright 2001-2005 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$id = $_GET['id'];
+
+if($id && $_GET['is_sure']=='1')
+{
+	if($LMS->DB->Execute('DELETE FROM daemonhosts WHERE id = ?', array($id)))
+	{
+		if($instances = $LMS->DB->GetCol('SELECT id FROM daemoninstances WHERE hostid = ?', array($id)))
+		{
+			foreach($instances as $instance)
+			{
+				$LMS->DB->Execute('DELETE FROM daemoninstances WHERE id = ?', array($instance));
+				$LMS->DB->Execute('DELETE FROM daemonconfig WHERE instanceid = ?', array($instance));
+			}
+			$LMS->SetTS('daemoninstances');
+			$LMS->SetTS('daemonconfig');
+		}
+		$LMS->SetTS('daemonhosts');
+	}
+}
+
+header('Location: ?m=daemonhostlist');
+
+?>
