61c0d618dce4f69f9a5b7a3ef6d2a101bd58dc9e
Aleksander Machniak <alec@alec.pl>
2005-04-21 07:25:13 +0000

    - admins.hosts

---
+++ lib/upgradedb/postgres.2005042100.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * LMS version 1.5-cvs
+ *
+ *  (C) Copyright 2001-2005 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$DB->BeginTrans();
+
+$DB->Execute("
+	ALTER TABLE admins ADD hosts varchar(255);
+	UPDATE admins SET hosts='';
+	ALTER TABLE admins ALTER hosts SET NOT NULL;
+	ALTER TABLE admins ALTER hosts SET DEFAULT '';
+");
+
+$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?",array('2005042100', 'dbversion'));
+
+$DB->CommitTrans();
+
+?>
