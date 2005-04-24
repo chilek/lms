db7145254e0fa6167365ccd8e88e8f0e2b0b2a99
Jakub Flis <kflis@kflis.net>
2005-04-24 12:34:48 +0000

    added new db versions with extra ipaddr field. (please check if that works on mysql and sqlite - i just have checked that with postgres).

---
+++ lib/upgradedb/sqlite.2005042400.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * LMS version 1.5-cvs
+ *
+ *  (C) Copyright 2001-2005 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$DB->BeginTrans();
+
+$DB->Execute("CREATE TEMP TABLE admins_t AS SELECT * FROM admins");
+$DB->Execute("DROP TABLE admins");
+$DB->Execute("CREATE TABLE admins (
+	id integer PRIMARY KEY,
+	login varchar(32) 	DEFAULT '' NOT NULL,
+	name varchar(64) 	DEFAULT '' NOT NULL,
+	email varchar(255) 	DEFAULT '' NOT NULL,
+	rights varchar(64) 	DEFAULT '' NOT NULL,
+	hosts varchar(255) 	DEFAULT '' NOT NULL,
+	passwd varchar(255) 	DEFAULT '' NOT NULL,
+	lastlogindate integer 	DEFAULT 0  NOT NULL,
+	lastloginip varchar(16) DEFAULT '' NOT NULL,
+	failedlogindate integer DEFAULT 0  NOT NULL,
+	failedloginip varchar(16) DEFAULT '' NOT NULL,
+	deleted smallint	DEFAULT 0 NOT NULL,
+	UNIQUE (login)
+");
+$DB->Execute("INSERT INTO admins (id, login, name, email, rights, passwd, lastlogindate, lastloginip, failedlogindate, failedloginip, deleted)
+		SELECT id, login, name, email, rights, passwd, lastlogindate, lastloginip, failedlogindate, failedloginip, deleted
+		FROM admins_t");
+$DB->Execute("DROP TABLE admins_t");
+
+$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?",array('2005042100', 'dbversion'));
+$DB->Execute("ALTER TABLE nodes add ipaddr_pub bigint DEFAULT '0' NOT NULL");
+
+$DB->CommitTrans();
+
+?>
