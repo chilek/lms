56653aa92c287568793942b02eab0a5163846b45
Aleksander Machniak <alec@alec.pl>
2005-05-23 06:25:40 +0000

    - all UI colors moved to style.css (done)

--- img/common.js
+++ img/common.js
@@ -31,38 +31,6 @@ function removeClass(theElem, theClass)
 	theElem.className = str.replace(regexp, "");
 }
 
-function setPointer(theRow, thePointerColor)
-{
-	if (thePointerColor == '' || typeof(theRow.style) == 'undefined') {
-		return false;
-	}
-	if (typeof(document.getElementsByTagName) != 'undefined') {
-		var theCells = theRow.getElementsByTagName('td');
-	}
-	else if (typeof(theRow.cells) != 'undefined') {
-		var theCells = theRow.cells;
-	}
-	else {
-		return false;
-	}
-	
-	var rowCellsCnt  = theCells.length;
-	for (var c = 0; c < rowCellsCnt; c++) {
-		theCells[c].style.backgroundColor = thePointerColor;
-	}
-	
-	return true;
-}
-
-function setPointerTD(theCell, thePointerColor)
-{
-	if (thePointerColor == '' || typeof(theCell.style) == 'undefined') {
-		return false;
-	}
-	theCell.style.backgroundColor = thePointerColor;
-	return true;
-}
-
 function openSelectWindow(theURL,winName,myWidth, myHeight, isCenter, formfield)
 {
 	if(window.screen)
