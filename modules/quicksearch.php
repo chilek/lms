1cadbc388178b6cee367a65c0dd767964d9b4caa
Konrad Rzentarzewski <lms@kondi.net>
2005-05-23 13:32:04 +0000

    - quick search made quick (and i mean it!) - tested on MSIE6.0sp1 and FF1.0.4 - based on LGPL AutoSuggest library

--- modules/quicksearch.php
+++ modules/quicksearch.php
@@ -29,6 +29,27 @@ $search = trim($_GET['what']);
 switch($_GET['mode'])
 {
 	case 'user':
+		if($_GET['ajax']==1) // support for AutoSuggest
+		{
+			$candidates = $DB->GetAll('SELECT id, lastname, name, email, phone1, phone2, phone3 FROM users WHERE id ?LIKE? \''.$search.'%\' OR lastname ?LIKE? \''.$search.'%\' OR name ?LIKE? \''.$search.'%\' OR email ?LIKE? \'%'.$search.'%\' OR phone1 ?LIKE? \''.$search.'%\' OR phone2 ?LIKE? \''.$search.'%\' OR phone3 ?LIKE? \''.$search.'%\' ORDER by lastname, name, email, phone1 LIMIT 15');
+			$eglible=array(); $actions=array(); $descriptions=array();
+			foreach($candidates as $idx => $row) {
+				$actions[$row['id']]='?m=userinfo&id='.$row['id'];
+				$eglible[$row['id']]=$row['name'].' '.$row['lastname'];
+				if (preg_match("/^$search/",$row['id'])) $descriptions[$row['id']]=trans('Id: ').$row['id'];
+				if (preg_match("/^$search/",$row['lastname'])) $descriptions[$row['id']]=trans('Lastname: ').$row['lastname'];
+				if (preg_match("/^$search/",$row['name'])) $descriptions[$row['id']]=trans('Firstname: ').$row['name'];
+				if (preg_match("/$search/",$row['email'])) $descriptions[$row['id']]=trans('E-mail: ').$row['email'];
+				if (preg_match("/^$search/",$row['phone1'])) $descriptions[$row['id']]=trans('Phone: ').$row['phone1'];
+				if (preg_match("/^$search/",$row['phone2'])) $descriptions[$row['id']]=trans('Phone: ').$row['phone2'];
+				if (preg_match("/^$search/",$row['phone3'])) $descriptions[$row['id']]=trans('Phone: ').$row['phone3'];
+			}
+			header('Content-type: text/plain');
+			print preg_replace('/$/',"\");\n","this.eligible = new Array(\"".implode('","',$eglible));
+			print preg_replace('/$/',"\");\n","this.descriptions = new Array(\"".implode('","',$descriptions));
+			print preg_replace('/$/',"\");\n","this.actions = new Array(\"".implode('","',$actions));
+		}
+
 		if(is_numeric($search)) // maybe it's customer ID
 		{
 			if($customerid = $DB->GetOne('SELECT id FROM users WHERE id = '.$search))
@@ -55,6 +76,23 @@ switch($_GET['mode'])
 	break;
 
 	case 'node':
+		if($_GET['ajax']==1) // support for AutoSuggest
+		{
+			$candidates = $DB->GetAll('SELECT id, lower(name) as name, inet_ntoa(ipaddr) as ipaddr, lower(mac) as mac FROM nodes WHERE id ?LIKE? \''.$search.'%\' OR lower(name) ?LIKE? lower(\''.$search.'%\') OR ipaddr ?LIKE? \'%'.$search.'%\' OR lower(mac) ?LIKE? lower(\'%'.$search.'%\') ORDER BY name, ipaddr, mac LIMIT 15');
+			$eglible=array(); $actions=array(); $descriptions=array();
+			foreach($candidates as $idx => $row) {
+				$actions[$row['id']]='?m=nodeinfo&id='.$row['id'];
+				$eglible[$row['id']]=$row['name'].' '.$row['lastname'];
+				if (preg_match("/^$search/",$row['name'])) $descriptions[$row['id']]=trans('Name: ').$row['name'];
+				if (preg_match("/$search/",$row['ipaddr'])) $descriptions[$row['id']]=trans('IP: ').$row['ipaddr'];
+				if (preg_match("/$search/",$row['mac'])) $descriptions[$row['id']]=trans('MAC: ').$row['mac'];
+			}
+			header('Content-type: text/plain');
+			print preg_replace('/$/',"\");\n","this.eligible = new Array(\"".implode('","',$eglible));
+			print preg_replace('/$/',"\");\n","this.descriptions = new Array(\"".implode('","',$descriptions));
+			print preg_replace('/$/',"\");\n","this.actions = new Array(\"".implode('","',$actions));
+		}
+
 		if(is_numeric($search) && !strstr($search, '.')) // maybe it's node ID
 		{
 			if($nodeid = $DB->GetOne('SELECT id FROM nodes WHERE id = '.$search))
@@ -77,6 +115,22 @@ switch($_GET['mode'])
 	break;
 	
 	case 'ticket':
+		if($_GET['ajax']==1) // support for AutoSuggest
+		{
+			$candidates = $DB->GetAll('SELECT id, subject FROM rttickets WHERE id ?LIKE? \''.$search.'%\' OR lower(subject) ?LIKE? lower(\'%'.$search.'%\') ORDER BY subject, id LIMIT 15');
+			$eglible=array(); $actions=array(); $descriptions=array();
+			foreach($candidates as $idx => $row) {
+				$actions[$row['id']]='?m=rtticketview&id='.$row['id'];
+				$eglible[$row['id']]=$row['subject'];
+				if (preg_match("/$search/",$row['id'])) $descriptions[$row['id']]=trans('Id: ').$row['id'];
+				if (preg_match("/$search/",$row['subject'])) $descriptions[$row['id']]=trans('Subject: ').$row['subject'];
+			}
+			header('Content-type: text/plain');
+			print preg_replace('/$/',"\");\n","this.eligible = new Array(\"".implode('","',$eglible));
+			print preg_replace('/$/',"\");\n","this.descriptions = new Array(\"".implode('","',$descriptions));
+			print preg_replace('/$/',"\");\n","this.actions = new Array(\"".implode('","',$actions));
+		}
+
 		if(intval($search))
 			$target = '?m=rtticketview&id='.$search;
 		else
