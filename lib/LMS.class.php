2790e21032fc95eb8d49c6e5d9c363eb184a7bec
Aleksander Machniak <alec@alec.pl>
2005-05-25 08:51:46 +0000

    - fixed debtors counting in UserStats() (for MySQL)

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -763,8 +763,11 @@ class LMS
 		if($balances = $this->DB->GetCol('SELECT SUM((type * -2 + 7)*value) FROM cash LEFT JOIN users ON customerid = users.id WHERE deleted = 0 GROUP BY customerid HAVING SUM((type * -2 + 7)*value) < 0'))
 		{
 			foreach($balances as $idx)
-				$result['debtvalue'] -= $idx;
-			$result['debt'] = sizeof($balances);
+				if($idx < 0)
+				{
+					$result['debtvalue'] -= $idx;
+					$result['debt']++;
+				}
 		}	
 		return $result;
 	}
