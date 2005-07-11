0c93b15b662cfb4c37cfd7b1621c3764d98921c8
Aleksander Machniak <alec@alec.pl>
2005-07-11 18:37:40 +0000

    - fix: added quotes

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -1576,7 +1576,7 @@ class LMS
 			// NOTE: don't waste CPU/mem when printing history is not set:
 			if ($this->CONFIG['invoices']['print_balance_history'])
 				$result['customerbalancelist'] = $this->GetCustomerBalanceList($result['customerid']);
-			$result[customerbalancelistlimit]=$this->CONFIG['invoices']['print_balance_history_limit'];
+			$result['customerbalancelistlimit'] = $this->CONFIG['invoices']['print_balance_history_limit'];
 			return $result;
 		}
 		else
