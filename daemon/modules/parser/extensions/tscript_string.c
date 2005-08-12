bfc830b282a5a64fccc1f1c6ca419e7ac658bdd8
Aleksander Machniak <alec@alec.pl>
2005-08-12 12:17:14 +0000

    - updated string and exec extensions (cleanup)

--- daemon/modules/parser/extensions/tscript_string.c
+++ daemon/modules/parser/extensions/tscript_string.c
@@ -1,6 +1,24 @@
+/*
+
+T-Script - STRING EXTENSION
+Copyright (C) 2004, Adrian Smarzewski <adrian@kadu.net>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
 #include "tscript_string.h"
 #include "tscript_extensions.h"
 #include "tscript_values.h"
+
 #include <string.h>
 #include <ctype.h>
 
