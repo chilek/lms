bd356c3f281b687eb60dd3813bb061b7911574e5
Aleksander Machniak <alec@alec.pl>
2005-08-23 13:20:05 +0000

    - restored last revision (describing payment with unpaid invoices)

--- modules/customerbalanceok.php
+++ modules/customerbalanceok.php
@@ -28,7 +28,33 @@ $customerid = $_GET['id'];
 
 if($LMS->CustomerExists($customerid))
 {
+	$covenants = $DB->GetAll('SELECT a.id AS cashid,
+			ROUND(SUM(CASE a.type WHEN 3 THEN a.value*-1 ELSE a.value END)/(CASE COUNT(b.id) WHEN 0 THEN 1 ELSE COUNT(b.id) END),2)
+			+ COALESCE(SUM(CASE b.type WHEN 3 THEN b.value*-1 ELSE b.value END),0) AS value
+			FROM cash a 
+			LEFT JOIN cash b ON (a.id = b.reference)
+			WHERE a.customerid = ? AND a.docid>0
+			GROUP BY a.docid, a.itemid, a.id
+			HAVING ROUND(SUM(CASE a.type WHEN 3 THEN a.value*-1 ELSE a.value END)/(CASE COUNT(b.id) WHEN 0 THEN 1 ELSE COUNT(b.id) END),2)
+			+ COALESCE(SUM(CASE b.type WHEN 3 THEN b.value*-1 ELSE b.value END),0) > 0', array($customerid));
+
 	$balance = $LMS->GetCustomerBalance($customerid);
+
+	if($covenants) foreach($covenants as $row)
+	{
+		if($balance>=0) break;
+	
+		$value = -$balance>=$row['value'] ? $row['value'] : -$balance;
+		$balance += $value;
+		
+		$DB->Execute('INSERT INTO cash (time, userid, type, value, customerid, comment, reference)
+				VALUES (?NOW?, ?, 3, ?, ?, ?, ?)', 
+				array($AUTH->id, 
+					$value,
+					$customerid,
+					trans('Accounted'),
+					$row['cashid']));
+	}
 
 	if($balance<0)
 	{
