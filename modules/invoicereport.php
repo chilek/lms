042793e71b8bd907247c0e9d554b30c494c59b2b
Aleksander Machniak <alec@alec.pl>
2005-08-23 11:49:24 +0000

    - fixed round-off in calculations on invoices report

--- modules/invoicereport.php
+++ modules/invoicereport.php
@@ -48,6 +48,7 @@ $layout['pagetitle'] = trans('Sale Registry for period $0 - $1', $from, $to);
 $listdata = array();
 $invoicelist = array();
 
+// get taxes for calculations and for building report table
 $taxes = $DB->GetAllByKey('SELECT taxid AS id, label, taxes.value AS value
 	    FROM documents 
 	    LEFT JOIN invoicecontents ON (documents.id = docid)
@@ -56,36 +57,52 @@ $taxes = $DB->GetAllByKey('SELECT taxid AS id, label, taxes.value AS value
 	    GROUP BY taxid, label, taxes.value 
 	    ORDER BY value ASC', 'id', array(DOC_INVOICE,$unixfrom, $unixto));
 
-if($result = $DB->GetAll('SELECT documents.id AS id, number, cdate, customerid, name, address, zip, city, ten, ssn, taxid, SUM(value*count) AS value, template 
+// we can't here simply get documents with SUM(value*count)
+// because we need here incoices-like round-off
+
+// get documents items numeric values for calculations
+$items = $DB->GetAll('SELECT docid, taxid, value, count
 	    FROM documents 
-	    LEFT JOIN numberplans ON numberplanid = numberplans.id
 	    LEFT JOIN invoicecontents ON docid = documents.id 
 	    WHERE type = ? AND (cdate BETWEEN ? AND ?) 
-	    GROUP BY documents.id, number, taxid, cdate, customerid, name, address, zip, city, ten, ssn, template 
-	    ORDER BY cdate ASC', array(DOC_INVOICE, $unixfrom, $unixto)))
-{
-	foreach($result as $idx => $row)
-	{
-		$id = $row['id'];
-		$taxid = $row['taxid'];
-		$value = round($row['value'], 2);
-		
-		$invoicelist[$id]['custname'] = $row['name'];
-		$invoicelist[$id]['custaddress'] = $row['zip'].' '.$row['city'].', '.$row['address'];
-		$invoicelist[$id]['ten'] = ($row['ten'] ? trans('TEN').' '.$row['ten'] : ($row['ssn'] ? trans('SSN').' '.$row['ssn'] : ''));
-		$invoicelist[$id]['number'] = docnumber($row['number'], $row['template'], $row['cdate']);
-		$invoicelist[$id]['cdate'] = $row['cdate'];
-		$invoicelist[$id]['customerid'] = $row['customerid'];
+	    ORDER BY cdate ASC, docid', array(DOC_INVOICE, $unixfrom, $unixto));
 
-		$invoicelist[$id][$taxid]['val'] += round($value / ($taxes[$taxid]['value']+100) * 100, 2);
-		$invoicelist[$id][$taxid]['tax'] += $value - $invoicelist[$id][$taxid]['val'];
-		$invoicelist[$id]['tax'] += $invoicelist[$id][$taxid]['tax'];
-		$invoicelist[$id]['brutto'] += $value;
+// get documents data
+$docs = $DB->GetAllByKey('SELECT documents.id AS id, number, cdate, customerid, name, address, zip, city, ten, ssn, template 
+	    FROM documents 
+	    LEFT JOIN numberplans ON numberplanid = numberplans.id
+	    WHERE type = ? AND (cdate BETWEEN ? AND ?) 
+	    GROUP BY documents.id, number, cdate, customerid, name, address, zip, city, ten, ssn, template 
+	    ORDER BY cdate ASC', 'id', array(DOC_INVOICE, $unixfrom, $unixto));
+
+if($items)
+{
+	foreach($items as $row)
+	{
+		$idx = $row['docid'];
+		$doc = $docs[$idx];
+		$taxid = $row['taxid'];
 		
-		$listdata[$taxid]['tax'] += $invoicelist[$id][$taxid]['tax'];
-		$listdata[$taxid]['val'] += $invoicelist[$id][$taxid]['val'];
-		$listdata['tax'] += $invoicelist[$id][$taxid]['tax'];
-		$listdata['brutto'] += $value;
+		$invoicelist[$idx]['custname'] = $doc['name'];
+		$invoicelist[$idx]['custaddress'] = $doc['zip'].' '.$doc['city'].', '.$doc['address'];
+		$invoicelist[$idx]['ten'] = ($doc['ten'] ? trans('TEN').' '.$doc['ten'] : ($doc['ssn'] ? trans('SSN').' '.$doc['ssn'] : ''));
+		$invoicelist[$idx]['number'] = docnumber($doc['number'], $doc['template'], $doc['cdate']);
+		$invoicelist[$idx]['cdate'] = $doc['cdate'];
+		$invoicelist[$idx]['customerid'] = $doc['customerid'];
+
+		$sum = $row['value'] * $row['count'];
+		$val = round($row['value'] / ($taxes[$taxid]['value']+100) * 100, 2) * $row['count'];
+		$tax = $sum - $val;
+		
+		$invoicelist[$idx][$taxid]['tax'] += $tax;
+		$invoicelist[$idx][$taxid]['val'] += $val;
+		$invoicelist[$idx]['tax'] += $tax;
+		$invoicelist[$idx]['brutto'] += $sum;
+		
+		$listdata[$taxid]['tax'] += $tax;
+		$listdata[$taxid]['val'] += $val;
+		$listdata['tax'] += $tax;
+		$listdata['brutto'] += $sum;
 	}
 }
 
