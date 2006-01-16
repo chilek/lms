857697d6e959227984610b0aa3dc40a24d6c7a4f
Aleksander Machniak <alec@alec.pl>
2006-01-16 09:35:17 +0000

    - changed year in copyrights

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -3,7 +3,7 @@
 /*
  * LMS version 1.8-cvs
  *
- *  (C) Copyright 2001-2005 LMS Developers
+ *  (C) Copyright 2001-2006 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
