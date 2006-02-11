9d60ffa71da0a2fd67e71807f59f9e12e5e89a1b
Aleksander Machniak <alec@alec.pl>
2006-02-11 11:16:36 +0000

    - lmsd/host fixed networks option parsing

--- daemon/modules/hostfile/hostfile.c
+++ daemon/modules/hostfile/hostfile.c
@@ -49,7 +49,7 @@ void reload(GLOBAL *g, struct hostfile_module *hm)
 
 	while( n>1 )
 	{
-		n = sscanf(netnames, "%s %[.a-zA-Z0-9-_ ]", netname, netnames);
+		n = sscanf(netnames, "%s %[._a-zA-Z0-9- ]", netname, netnames);
 
 		if( strlen(netname) )
 		{
@@ -96,7 +96,7 @@ void reload(GLOBAL *g, struct hostfile_module *hm)
 
 	while( k>1 )
 	{
-		k = sscanf(groupnames, "%s %[.a-zA-Z0-9-_ ]", groupname, groupnames);
+		k = sscanf(groupnames, "%s %[._a-zA-Z0-9- ]", groupname, groupnames);
 
 		if( strlen(groupname) )
 		{
