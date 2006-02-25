a2e0ef1ef78f97602fee34b531b53d297397c35a
Aleksander Machniak <alec@alec.pl>
2006-02-25 19:59:27 +0000

    - fixed dir times

--- devel/buildpack.sh
+++ devel/buildpack.sh
@@ -47,6 +47,7 @@ umask 022
 cd $TEMPDIR/$X/
 tar -xzf lms.tar.gz
 chmod 777 lms/{templates_c,backups,documents}
+touch `find . -type d`
 cd lms
 rm -Rf $NOTDISTRIB
 grep -air '1\.8-cvs' .|cut -d: -f1|sort|uniq|xargs perl -pi -e "s/1\.8-cvs/$LMSVER $CODENAME/g"
