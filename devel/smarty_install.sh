29a66e67ec37e1462da8da4628504e20d61da018
Aleksander Machniak <alec@alec.pl>
2006-09-04 13:26:47 +0000

    - simple script for Smarty installation

---
+++ devel/smarty_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Smarty templates library quick installation (with sources download)
+#
+
+SMARTYVER="2.6.14"
+
+cd ../lib
+# download
+echo -n "Downloading Smarty sources... "
+wget http://smarty.php.net/distributions/Smarty-$SMARTYVER.tar.gz
+echo "done."
+
+# extracting package
+echo -n "Extracting... "
+tar -xzf Smarty-$SMARTYVER.tar.gz
+echo "done."
+
+# merging
+echo -n "Merging... "
+mv Smarty-$SMARTYVER/libs/* Smarty/
+mv Smarty-$SMARTYVER/libs/plugins/* Smarty/plugins/
+echo "done."
+
+# cleanup
+echo -n "Cleaning up... " 
+rm -Rf Smarty-$SMARTYVER Smarty-$SMARTYVER.tar.gz
+echo "done."
+cd ../devel
