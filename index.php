bc2545aca627ee9de4f0f5739af58df7e237571a
Aleksander Machniak <alec@alec.pl>
2007-09-12 08:48:55 +0000

    - removed Smarty version from "old Smarty version" error string

--- index.php
+++ index.php
@@ -146,7 +146,7 @@ $SMARTY = new Smarty;
 // test for proper version of Smarty
 
 if(version_compare('2.6.0', $SMARTY->_version) > 0)
-	die('<B>Old version of Smarty engine! You must get newest from <A HREF="http://smarty.php.net/distributions/Smarty-2.6.8.tar.gz">http://smarty.php.net/distributions/Smarty-2.6.8.tar.gz</A></B>');
+	die('<B>Old version of Smarty engine! You must get newest from <A HREF="http://smarty.php.net/distributions">http://smarty.php.net</A></B>');
 
 // Read configuration of LMS-UI from database
 
