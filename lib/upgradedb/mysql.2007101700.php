fb9ddd9b7dd9abca0f1cfe182ea2d39905ec0874
Aleksander Machniak <alec@alec.pl>
2007-10-31 09:41:43 +0000

    - fixed problem with ajax plugins in Opera

--- lib/upgradedb/mysql.2007101700.php
+++ lib/upgradedb/mysql.2007101700.php
@@ -29,7 +29,7 @@ $DB->BeginTrans();
 $DB->Execute("ALTER TABLE nodes CHANGE name name varchar(32) NOT NULL DEFAULT ''");
 $DB->Execute("ALTER TABLE users ADD position varchar(255) NOT NULL DEFAULT ''");
 
-$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?",array('2007101700', 'dbversion'));
+$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2007101700', 'dbversion'));
 
 $DB->CommitTrans();
 
