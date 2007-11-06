bae47d611d52b30e5faf1c20f4882558054e5dea
Aleksander Machniak <alec@alec.pl>
2007-11-06 08:31:22 +0000

    - cleanup

--- lib/LMS.class.php
+++ lib/LMS.class.php
@@ -1816,7 +1816,7 @@ class LMS
 			$result['customerpin'] = $this->DB->GetOne('SELECT pin FROM customers WHERE id=?', array($result['customerid']));
 			
 			// NOTE: don't waste CPU/mem when printing history is not set:
-			if(isset($this->CONFIG['invoices']['print_balance_history']) && chkconfig($this->CONFIG['invoices']['print_balance_history']))
+			if(chkconfig($this->CONFIG['invoices']['print_balance_history']))
 			{
 				$result['customerbalancelist'] = $this->GetCustomerBalanceList($result['customerid']);
 				$result['customerbalancelistlimit'] = $this->CONFIG['invoices']['print_balance_history_limit'];
