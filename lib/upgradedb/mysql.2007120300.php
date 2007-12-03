e3e45e662a993eff333e3b43d157c1be3ea79023
Aleksander Machniak <alec@alec.pl>
2007-12-03 13:09:19 +0000

    - fix: added customersview refresh

--- lib/upgradedb/mysql.2007120300.php
+++ lib/upgradedb/mysql.2007120300.php
@@ -26,8 +26,16 @@
 
 $DB->BeginTrans();
 
-$DB->Execute("ALTER TABLE customers ADD cutoffstop int(11) NOT NULL DEFAULT '0'");
 
+$DB->Execute("ALTER TABLE customers ADD cutoffstop int(11) NOT NULL DEFAULT '0'");
+$DB->Execute("DROP VIEW customersview");
+$DB->Execute("CREATE VIEW customersview AS
+        SELECT c.* FROM customers c
+	WHERE NOT EXISTS (
+	        SELECT 1 FROM customerassignments a
+	        JOIN excludedgroups e ON (a.customergroupid = e.customergroupid)
+	        WHERE e.userid = lms_current_user() AND a.customerid = c.id)");
+								
 $DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2007120300', 'dbversion'));
 
 $DB->CommitTrans();
