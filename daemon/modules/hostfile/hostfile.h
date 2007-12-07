730b6a8642b217024f1ddf9c081d7a1dcebe5f1d
Aleksander Machniak <alec@alec.pl>
2007-12-07 13:49:25 +0000

    - lmsd: when used with -q option crontab instances are skipped - lmsd/hostfile: better customergroups option handling - lmsd/hostfile: added options 'warn', 'warn_pub', 'warn_replace' for   warning flag support, 'public_replace' for better public IPs   handling and 'host_begin', 'host_end' for usability

--- daemon/modules/hostfile/hostfile.h
+++ daemon/modules/hostfile/hostfile.h
@@ -17,20 +17,44 @@ struct group
 	int id;
 };
 
+struct host
+{
+	char *name;
+	char *passwd;
+	char *warning;
+	char *access;
+	char *info;
+	char *mac;
+	char *id;
+	char *ip;
+	char *ip_pub;
+	char *i16;
+	char *i16_pub;
+	unsigned long address;
+	unsigned long mask;
+	struct net net;
+};
+
 struct hostfile_module
 {
 	MODULE base;
 
 	char *prefix;
 	char *append;
+	char *host_prefix;
+	char *host_append;
 	char *grant;
 	char *deny;
 	char *grant_pub;
 	char *deny_pub;
+	char *warn;
+	char *warn_pub;
 	char *file;
 	char *command;
 	char *networks;
 	char *customergroups;
 
 	int skip_dev_ips;
+	int pub_replace;
+	int warn_replace;
 };
