5789f83c09c4d235825ea58583a0c02f6451fcaa
Aleksander Machniak <alec@alec.pl>
2008-01-04 07:53:11 +0000

    - s/2001-2007/2001-2008/ in copyrights

--- daemon/modules/ethers/ethers.c
+++ daemon/modules/ethers/ethers.c
@@ -1,7 +1,7 @@
 /*
  * LMS version 1.11-cvs
  *
- *  (C) Copyright 2001-2007 LMS Developers
+ *  (C) Copyright 2001-2008 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
