248c6e05230f86780d4d3dd40546dfc15b238128
Aleksander Machniak <alec@alec.pl>
2008-02-18 09:05:07 +0000

    - Hosting: multi-account aliases

---
+++ lib/upgradedb/postgres.2008021500.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * LMS version 1.11-cvs
+ *
+ *  (C) Copyright 2001-2008 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$DB->BeginTrans();
+
+$DB->Execute("
+	CREATE SEQUENCE aliasassignments_id_seq;
+	CREATE TABLE aliasassignments (
+		id		integer		DEFAULT nextval('passwd_id_seq'::text) NOT NULL,
+		aliasid		integer		DEFAULT 0 NOT NULL,
+		accountid	integer		DEFAULT 0 NOT NULL,
+		PRIMARY KEY (id),
+		UNIQUE (aliasid, accountid)
+	);
+
+	ALTER TABLE aliases ADD domainid integer NOT NULL DEFAULT 0;
+	
+	UPDATE aliases SET domainid = (SELECT domainid FROM passwd WHERE id = accountid);
+
+	INSERT INTO aliasassignments (aliasid, accountid) 
+		SELECT a.id, accountid FROM aliases;
+	
+	ALTER TABLE aliases DROP accountid;
+	ALTER TABLE aliases ADD UNIQUE INDEX (login, domainid);
+");
+
+$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2008021500', 'dbversion'));
+
+$DB->CommitTrans();
+
+?>
