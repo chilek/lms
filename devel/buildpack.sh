32983cdfe4e3a8485de276c4676c9fd4cc886418
Aleksander Machniak <alec@alec.pl>
2008-02-27 18:06:56 +0000

    - Smarty-2.6.19

--- devel/buildpack.sh
+++ devel/buildpack.sh
@@ -7,7 +7,7 @@
 
 WORKDIR=`pwd`
 NOTDISTRIB="devel .project modules/core modules/mailing modules/auth modules/traffic modules/users lib/ExecStack.class.php"
-SMARTYVER="2.6.18"
+SMARTYVER="2.6.19"
 
 echo -ne "Katalog tmp? [$TMPDIR]: "
 read TEMPDIR
