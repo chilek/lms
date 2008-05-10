a8b1477e4d12f13586fc9de23a07831d22a590f1
Grzegorz Chwesewicz <ceho@retis.net.pl>
2008-05-10 23:55:38 +0000

    - code cleanup

--- devel/docgen-pl.sh
+++ devel/docgen-pl.sh
@@ -2,67 +2,60 @@
 #
 # Skrypt s³u¿y do tworzenia dokumentacji w ró¿nych formatach
 # (html, txt) na podstawie ¼ród³owych plików sgml
-# Przed u¿yciem nale¿y zainstalowaæ w systemie sgml-tools
-# z openjade (lynx'a pewnie ka¿dy ma)
 #
+# Wymagane programy:
+# -sgml-tools
+# -openjade
+# -lynx
+# -iconv
 
 cd ../doc/sgml/pl
+
+generate_html()
+{
+	jade -t sgml -d lms.dsl index.sgml
+	if [ $? -ne 0 ]
+		then exit 1
+	fi
+
+	mv ./*.html ../../html/pl/
+
+	for i in ../../html/pl/*.html
+	do
+		iconv --from latin2 --to utf-8 < $i > $i.tmp
+		mv -f $i.tmp $i
+	done
+}
+
+generate_txt()
+{
+	jade -t sgml -V nochunks -d lms.dsl index.sgml > ../../README_pl.html
+	if [ $? -ne 0 ]
+		then exit 1
+	fi
+
+	lynx -dump ../../README_pl.html -display_charset=ISO-8859-2 -raw -nolist -dont_wrap_pre > ../../README_pl
+	if [ $? -ne 0 ]
+		then exit 2
+	fi
+
+	iconv --from latin2 --to utf-8 < ../../README_pl > ../../README_pl.tmp
+	mv -f ../../README_pl.tmp ../../README_pl
+}
 
 case "$1" in
 
     'html')	####### sgml -> html #######################
-        jade -t sgml -d lms.dsl index.sgml
-	if [ $? -ne 0 ]
-	    then exit 1
-	fi
-	mv ./*.html ../../html/pl/
-	for i in ../../html/pl/*.html
-	do
-	    iconv --from latin2 --to utf-8 < $i > $i.tmp
-	    mv -f $i.tmp $i
-	done
-	exit 0
+	generate_html
     ;;
 
     'txt')	####### sgml -> text #######################
-	jade -t sgml -V nochunks -d lms.dsl index.sgml > ../../README_pl.html
-	if [ $? -ne 0 ]
-	    then exit 1
-	fi
-#	elinks -dump ../../README.html > ../../README
-	lynx -dump ../../README_pl.html -display_charset=ISO-8859-2 -raw -nolist -dont_wrap_pre > ../../README_pl
-        if [ $? -ne 0 ]
-	    then
-	    exit 2
-	fi
-	iconv --from latin2 --to utf-8 < ../../README_pl > ../../README_pl.tmp
-	mv -f ../../README_pl.tmp ../../README_pl
-	exit 0
+	generate_txt
     ;;
 
     'all')	####### sgml -> html & txt #################
-        jade -t sgml -d lms.dsl index.sgml
-	if [ $? -ne 0 ]
-	    then exit 1
-	fi
-	mv ./*.html ../../html/pl/
-	for i in ../../html/pl/*.html
-	do
-	    iconv --from latin2 --to utf-8 < $i > $i.tmp
-	    mv -f $i.tmp $i
-	done
-
-	jade -t sgml -V nochunks -d lms.dsl index.sgml > ../../README_pl.html
-	if [ $? -ne 0 ]
-	    then exit 1
-	fi
-	lynx -dump ../../README_pl.html -display_charset=ISO-8859-2 -raw -nolist -dont_wrap_pre > ../../README_pl
-        if [ $? -ne 0 ]
-	    then
-	    exit 2
-	fi
-	iconv --from latin2 --to utf-8 < ../../README_pl > ../../README_pl.tmp
-	mv -f ../../README_pl.tmp ../../README_pl
+	generate_html
+	generate_txt
     ;;
         
     *)
--- devel/docgen.sh
+++ devel/docgen.sh
@@ -1,54 +1,51 @@
 #!/bin/bash
 #
-# Script generate documentation in different formats
+# Script generates documentation in different formats
 # (html, txt) from source sgml files
-# Required is instalation of sgml-tools with openjade
-# (lynx probably have anybody)
 #
+# Programs required:
+# -sgml-tools
+# -openjade
+# -lynx
 
 cd ../doc/sgml
+
+generate_html()
+{
+	jade -t sgml -d lms.dsl index.sgml
+	if [ $? -ne 0 ]
+		then exit 1
+	fi
+
+	mv ./*.html ../html/en/
+}
+
+generate_txt()
+{
+	jade -t sgml -V nochunks -d lms.dsl index.sgml > ../README.html
+	if [ $? -ne 0 ]
+		then exit 1
+	fi
+	
+	lynx -dump ../README.html -display_charset=ISO-8859-1 -raw -nolist -dont_wrap_pre > ../README
+	if [ $? -ne 0 ]
+		then exit 2
+	fi
+}
 
 case "$1" in
 
     'html')	####### sgml -> html #######################
-        jade -t sgml -d lms.dsl index.sgml
-	if [ $? -ne 0 ]
-	    then exit 1
-	fi
-	mv ./*.html ../html/en/
-	exit 0
+	generate_html
     ;;
 
     'txt')	####### sgml -> text #######################
-	jade -t sgml -V nochunks -d lms.dsl index.sgml > ../README.html
-	if [ $? -ne 0 ]
-	    then exit 1
-	fi
-#	elinks -dump ../README.html > ../README
-	lynx -dump ../README.html -display_charset=ISO-8859-1 -raw -nolist -dont_wrap_pre > ../README
-        if [ $? -ne 0 ]
-	    then
-	    exit 2
-	fi
-	exit 0
+	generate_txt
     ;;
 
     'all')	####### sgml -> html & txt #################
-        jade -t sgml -d lms.dsl index.sgml
-	if [ $? -ne 0 ]
-	    then exit 1
-	fi
-	mv ./*.html ../html/en/
-
-	jade -t sgml -V nochunks -d lms.dsl index.sgml > ../README.html
-	if [ $? -ne 0 ]
-	    then exit 1
-	fi
-	lynx -dump ../README.html -display_charset=ISO-8859-1 -raw -nolist -dont_wrap_pre > ../README
-        if [ $? -ne 0 ]
-	    then
-	    exit 2
-	fi
+	generate_html
+	generate_txt
     ;;
         
     *)
