f74fa16b57973f361740de2bb371a84a6df16cb4
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2009-05-08 10:45:24 +0000

    - lithuanian userpanel localisation started

---
+++ userpanel/lib/locale/lt/strings.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ *  LMS Userpanel version 1.11-cvs
+ *
+ *  (C) Copyright 2001-2009 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ *  LT VERSION BY ETANETAS ver.0.1
+ */
+
+$_LANG['Access denied!'] = 'Neturite prieigos!';
+$_LANG['and'] = 'ir';
+$_LANG['Cancel'] = 'Panaikink';
+$_LANG['classic'] = 'klasikinis';
+$_LANG['Configure Module: $0'] = 'Modulio konfigūracija: $0';
+$_LANG['Customer ID:'] = 'Kliento ID:';
+$_LANG['Customers\' rights'] = 'Klientų įgaliojimai';
+$_LANG['Database errors occurred!'] = 'Atsirado duomenų bazės aptarnavimo klaidų!';
+$_LANG['Database version'] = 'Duomenų bazės versija';
+$_LANG['Default Userpanel Rights:'] = 'Numatyti įgaliojimai Vartotojo panelėje:';
+$_LANG['Description'] = 'Aprašymas';
+$_LANG['Display name'] = 'Pasirodantis pavadinimas';
+$_LANG['Error'] = 'Klaida';
+$_LANG['[ Error! ]'] = '[ Klaida! ]';
+$_LANG['Error:'] = 'Klaida:';
+$_LANG['Error!'] = 'Klaida!';
+$_LANG['For more information see'] = 'Kad sužinotumėte daugiau informacijos pasižiūrėkite';
+$_LANG['Function <b>$0</b> in module <b>$1</b> not found!'] = 'Funkcija <b>$0</b> modulyje <b>$1</b> nerasta!';
+$_LANG['Hint style:'] = 'Užuominos stilius:';
+$_LANG['Idle time limit exceeded ($0 sec.)'] = 'Viršytas neveiklumo laiko limitas ($0 sek.)';
+$_LANG['Login'] = 'Prisijunk';
+$_LANG['Basic Configuration:'] = 'Pagrindinė konfigūracija:';
+$_LANG['modern'] = 'šiuolaikinis';
+$_LANG['Module'] = 'Modulis';
+$_LANG['Module <b>$0</b> not found!'] = 'Modulis <b>$0</b> nerastas!';
+$_LANG['Module Configuration:'] = 'Modulio konfigūracija:';
+$_LANG['monthly'] = 'per mėnesį';
+$_LANG['No modules found!'] = 'Nerstas nei vienas modulis!';
+$_LANG['none'] = 'joks';
+$_LANG['PIN:'] = 'PIN:';
+$_LANG['Please login.'] = 'Prašau prisijungti.';
+$_LANG['Prio'] = 'Prio (ankstesnis)';
+$_LANG['quarterly'] = 'ketvirčiais';
+$_LANG['Query:'] = 'Klausimas:';
+$_LANG['Submit'] = 'Įrašyti';
+$_LANG['There are any userpanel rights.'] = 'Neturite jokių įgaliojimų Vartotojo panelei.';
+$_LANG['This module does not have any configuration settings'] = 'Šiam modulyje nėra jokių konfigūracijos pasirinkčių';
+$_LANG['This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2.'] = 'Ši programa yra laisvai pasiekiama, gali ją platinti toliau ir/arba tobulinti remdamasis Visuotinės Viešos Licencijos GNU sąlygomis – pagal 2-ąją šios licencijos versiją';
+$_LANG['Tip'] = 'Užuomina';
+$_LANG['Total:'] = 'Iš viso:';
+$_LANG['Userpanel Configuration'] = 'Vartotojo panelės konfigūracija';
+$_LANG['Userpanel design style'] = 'Vartotojo panelės dizainas';
+$_LANG['Userpanel hints style. Chose modern for animated assistant, classic for tips in LMS style, or none to disable hints displaying'] = 'Užuominos stilius Vartotojo panelėje. Pasirink šiuolaikinį, kad rodytų animuotą asistentą, klasikinį – užuomina LMS stiliumi arba jokio, kad išjungtum užuominų rodymą';
+$_LANG['Userpanel module <b>$0</b> not found!'] = 'Nerastas modulis <b>$0</b>!';
+$_LANG['Userpanel Rights:'] = 'Įgaliojimai Vartotojo panelėje:';
+$_LANG['Userpanel style:'] = 'Vartotojo panelės stilius:';
+$_LANG['VCS'] = 'wBOK';
+$_LANG['Virtual Customer Service'] = 'Virtualus kliento aptarnavimo biuras';
+$_LANG['weekly'] = 'per savaitę';
+$_LANG['yearly'] = 'per metus';
+$_LANG['Rights'] = 'Įgaliojimai';
+$_LANG['Hiding some modules useless when customer hasn\'t got any nodes'] = 'Kai kurių modulių slėpimas, kai klientas neturi kompiuterių';
+$_LANG['Disabling modules:'] = 'Modulių išjungimas:';
