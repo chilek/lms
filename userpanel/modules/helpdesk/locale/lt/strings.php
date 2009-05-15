6be288a2fe7d7b49244b2ce7331ac560d6bc04dd
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2009-05-15 06:00:36 +0000

    - lithuanian userpanel localisation finished

---
+++ userpanel/modules/helpdesk/locale/lt/strings.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ *  LMS Userpanel version 1.11-cvs
+ *
+ *  (C) Copyright 2001-2009 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$_LANG['Allows you to contact with network administrator'] = 'Leidþia Jums susisiekti su administratoriumi';
+$_LANG['Body:'] = 'Turinys:';
+$_LANG['Cancel'] = 'Panaikink';
+$_LANG['Click icon on the left to see request details'] = 'Paspauskite þenklà kairëje, kad pamatytumëte praðymo turiná';
+$_LANG['Date:'] = 'Data:';
+$_LANG['dead'] = 'negyvas';
+$_LANG['Debug email:'] = 'Patikrinimo þinutë:';
+$_LANG['Debug email for new tickets notifications.'] = 'Tikrinimo el. paðto adresas, kuriuo bus siunèiamos þinutës apie naujus praðymus.';
+$_LANG['Default queue:'] = 'Numanoma eilë:';
+$_LANG['Default user:'] = 'Numanomas vartotojas:';
+$_LANG['Details'] = 'Smulkmenos';
+$_LANG['Enter request contents'] = 'Ávesk praðymo turiná';
+$_LANG['Enter request subject'] = 'Ávesk praðymo temà';
+$_LANG['From:'] = 'Nuo:';
+$_LANG['Helpdesk'] = 'Pagalbos centras';
+$_LANG['Helpdesk queue for tickets requested in Userpanel'] = 'Pagalbos centro eilë praðymams ið Vratotojo panelës';
+$_LANG['Last modified:'] = 'Paskutinis modifikuotas:';
+$_LANG['LMS address needed to build link to new ticket in notification message'] = 'LMS adreso reikia praneðime sukurti saità á naujà praðymà';
+$_LANG['LMS url:'] = 'LMS URL:';
+$_LANG['Module Configuration:'] = 'Modulio konfiguracija:';
+$_LANG['new'] = 'naujas';
+$_LANG['New Request'] = 'Nauja praðymas';
+$_LANG['No such requests in database.'] = 'Duomenø bazëje nëra praðymø.';
+$_LANG['Number:'] = 'Numeris:';
+$_LANG['open'] = 'atviras';
+$_LANG['Request history'] = 'Praðymø istorija';
+$_LANG['Request No. $0'] = 'Praðymas Nr. $0';
+$_LANG['resolved'] = 'iðspræstas';
+$_LANG['- select queue -'] = '- pasirink eilæ -';
+$_LANG['- select user -'] = '- pasirink vartotojà -';
+$_LANG['Sender of new request notifications and his email. If not specified notifications goes from queue address.'] = 'Praneðimø apie naujus praðymus siuntëjas(ir jo el. paðto adresas). Jei nenurodytas bus panaudotas eilës numeris.';
+$_LANG['Status:'] = 'Statusas:';
+$_LANG['Subject:'] = 'Tema:';
+$_LANG['Submit'] = 'Tæsk';
+$_LANG['[ Submit ]'] = '[ Áraðyk ]';
+$_LANG['This module allows your customers to contact with you'] = 'Ðis modulis leidþia Jûsø klientams susisiekti su Jumis';
+$_LANG['Ticket must have its body!'] = 'Turi nurodyti praðymo turiná!';
+$_LANG['Ticket must have its title!'] = 'Praðymas turi bûti pavadintas!';
+$_LANG['Message'] = 'Þinutë';
+
+?>
---
+++ userpanel/modules/modules/locale/lt/strings.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ *  LMS Userpanel version 1.11-cvs
+ *
+ *  (C) Copyright 2001-2009 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$_LANG['Modules'] = 'Moduliai';
+$_LANG['Loaded modules'] = 'Uþkrauti moduliai';
+$_LANG['Show informations about modules'] = 'Rodo informacijà apie modulius';
+$_LANG['This module is for showing informations about modules'] = 'Ðis modulis rodo informacijà apie uþkrautus modulius';
+$_LANG['Module'] = 'Modulis‚';
+$_LANG['Display name'] = 'Rodomas pavadinimas';
+$_LANG['Prio'] = 'Prioritetas';
+$_LANG['Tip'] = 'Uþuomina';
+$_LANG['Description'] = 'Apraðas';
+
+?>
