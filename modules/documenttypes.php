81b9292e2102ca3d346b6e2e94c6f474d7d6fb65
Aleksander Machniak <alec@alec.pl>
2009-05-15 12:35:18 +0000

    - documenttypes

---
+++ modules/documenttypes.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * LMS version 1.11-cvs
+ *
+ *  (C) Copyright 2001-2009 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$typelist = $DOCTYPES;
+
+foreach($typelist as $idx => $name)
+	if($idx >= 0)
+		unset($typelist[$idx]);
+
+if ($SESSION->is_set('dtlp') && !isset($_GET['page']))
+	$SESSION->restore('dtlp', $_GET['page']);
+
+$listdata['total'] = sizeof($typelist);
+	    
+$page = (!isset($_GET['page']) ? 1 : $_GET['page']); 
+$pagelimit = (!isset($CONFIG['phpui']['documenttypes_pagelimit']) ? $listdata['total'] : $CONFIG['phpui']['documenttypes_pagelimit']);
+$start = ($page - 1) * $pagelimit;
+
+$SESSION->save('dtlp', $page);
+
+$layout['pagetitle'] = trans('Document Types List');
+
+$SESSION->save('backto', $_SERVER['QUERY_STRING']);
+
+$SMARTY->assign('pagelimit', $pagelimit);
+$SMARTY->assign('page', $page);
+$SMARTY->assign('start', $start);
+$SMARTY->assign('listdata', $listdata);
+$SMARTY->assign('typelist', $typelist);
+$SMARTY->display('documenttypes.html');
+
+?>
