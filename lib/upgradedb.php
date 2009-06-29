c909e59ab6788f6621e1adafff4fec9369f6d2ec
Aleksander Machniak <alec@alec.pl>
2009-06-29 16:42:10 +0000

    - reverted unwanted (to early) changes

--- lib/upgradedb.php
+++ lib/upgradedb.php
@@ -24,7 +24,7 @@
  *  $Id$
  */
 
-define('DBVERSION', '2009062300'); // here should be always the newest version of database!
+define('DBVERSION', '2009060200'); // here should be always the newest version of database!
 				 // it placed here to avoid read disk every time when we call this file.
 
 /*
