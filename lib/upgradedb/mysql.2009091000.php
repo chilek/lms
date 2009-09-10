50be1008aac4d5d3b5c637cf30defe4a50faa0cd
Grzegorz Chwesewicz <ceho@retis.net.pl>
2009-09-10 09:13:40 +0000

    - extended description size of netdevices

---
+++ lib/upgradedb/mysql.2009091000.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * LMS version 1.11-cvs
+ *
+ *  (C) Copyright 2001-2009 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$DB->Execute("
+DROP VIEW nas;
+
+ALTER TABLE netdevices ALTER description TYPE text;
+
+CREATE VIEW nas AS 
+        SELECT n.id, inet_ntoa(n.ipaddr) AS nasname, d.shortname, d.nastype AS type,
+	d.clients AS ports, d.secret, d.community, d.description 
+        FROM nodes n 
+        JOIN netdevices d ON (n.netdev = d.id) 
+        WHERE n.nas = 1;
+");
+
+$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2009091000', 'dbversion'));
+
+?>
