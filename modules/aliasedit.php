06a30449b7bc3eeaca1e61cf37a144a41e5bdc07
Aleksander Machniak <alec@alec.pl>
2009-09-17 16:53:35 +0000

    - fix endless redirect when removing account assignment from alias

--- modules/aliasedit.php
+++ modules/aliasedit.php
@@ -45,30 +45,6 @@ if(!$aliasold)
 	$SESSION->redirect('?'.$SESSION->get('backto'));
 }
 
-if(!empty($_GET['delaccount']) || !empty($_GET['delmailforward']))
-{
-	$cnt = $DB->GetOne('SELECT COUNT(*) FROM aliasassignments WHERE aliasid = ?', array($aliasold['id']));
-
-	if($cnt < 2)
-	{
-		$DB->BeginTrans();
-	        $DB->Execute('DELETE FROM aliases WHERE id = ?', array($aliasold['id']));
-		$DB->Execute('DELETE FROM aliasassignments WHERE aliasid = ?', array($aliasold['id']));
-		$DB->CommitTrans();		
-	}
-	else
-	{
-		if (!empty($_GET['delaccount']))
-			$DB->Execute('DELETE FROM aliasassignments WHERE aliasid = ? AND accountid = ?',
-				array($aliasold['id'], intval($_GET['delaccount'])));
-		else
-			$DB->Execute('DELETE FROM aliasassignments WHERE aliasid = ? AND mail_forward = ?',
-				array($aliasold['id'], $_GET['delmailforward']));
-	}
-	
-	$SESSION->redirect('?'.$SESSION->get('backto'));
-}
-
 $layout['pagetitle'] = trans('Alias Edit: $0', $aliasold['login'].'@'.$aliasold['domain']);
 
 if(isset($_POST['alias']))
