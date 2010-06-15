bf9d568bd1510b941b80b76b65e1faf5b5270216
Aleksander Machniak <alec@alec.pl>
2010-06-15 10:22:37 +0000

    - re-designed customer assignments forms

--- modules/customerassignmentedit.php
+++ modules/customerassignmentedit.php
@@ -24,23 +24,34 @@
  *  $Id$
  */
 
-$customerid = $DB->GetOne('SELECT customerid FROM assignments WHERE id=?', array($_GET['id']));
+// get customer name and check privileges using customersview
+$customer = $DB->GetRow('SELECT a.customerid AS id, '
+    .$DB->Concat('c.lastname',"' '",'c.name').' AS name
+    FROM assignments a
+    JOIN customersview c ON (c.id = a.customerid)
+    WHERE a.id = ?', array($_GET['id']));
 
-if(!$customerid)
+if(!$customer)
 {
 	$SESSION->redirect('?'.$SESSION->get('backto'));
 }
 
-if(isset($_POST['assignmentedit']))
+if($_GET['action'] == 'suspend')
 {
-	$a = $_POST['assignmentedit'];
-	
+    $LMS->SuspendAssignment($_GET['id'], $_GET['suspend']);
+    $SESSION->redirect('?'.$SESSION->get('backto'));
+}
+
+if(isset($_POST['assignment']))
+{
+	$a = $_POST['assignment'];
+
 	foreach($a as $key => $val)
 		if(!is_array($val))
 			$a[$key] = trim($val);
-	
+
 	$a['id'] = $_GET['id'];
-	$a['customerid'] = $customerid;
+	$a['customerid'] = $customer['id'];
 	$a['liabilityid'] = isset($_GET['lid']) ? $_GET['lid'] : 0;
 
 	$period = sprintf('%d',$a['period']);
@@ -50,22 +61,22 @@ if(isset($_POST['assignmentedit']))
 		case DAILY:
 			$at = 0;
 		break;
-		
+
 		case WEEKLY:
 			$at = sprintf('%d',$a['at']);
-			
+
 			if(chkconfig($CONFIG['phpui']['use_current_payday']) && $at==0)
 			{
 				$at = strftime('%u', time());
 			}
-			
+
 			if($at < 1 || $at > 7)
-				$error['editat'] = trans('Incorrect day of week (1-7)!');
+				$error['at'] = trans('Incorrect day of week (1-7)!');
 		break;
 
 		case MONTHLY:
 			$at = sprintf('%d',$a['at']);
-			
+
 			if(isset($CONFIG['phpui']['use_current_payday']) && chkconfig($CONFIG['phpui']['use_current_payday']) && $at==0)
 			{
 				$at = date('j', time());
@@ -75,11 +86,11 @@ if(isset($_POST['assignmentedit']))
 			{
 				$at = $CONFIG['phpui']['default_monthly_payday'];
 			}
-			
+
 			$a['at'] = $at;
-			
+
 			if($at > 28 || $at < 1)
-				$error['editat'] = trans('Incorrect day of month (1-28)!');
+				$error['at'] = trans('Incorrect day of month (1-28)!');
 		break;
 
 		case QUARTERLY:
@@ -91,19 +102,19 @@ if(isset($_POST['assignmentedit']))
 			}
 			elseif(!preg_match('/^[0-9]{2}\/[0-9]{2}$/', $a['at']))
 			{
-				$error['editat'] = trans('Incorrect date format! Enter date in DD/MM format!');
+				$error['at'] = trans('Incorrect date format! Enter date in DD/MM format!');
 			}
 			else
 			{
 				list($d,$m) = explode('/',$a['at']);
 			}
-			
+
 			if(!$error)
 			{
 				if($d>30 || $d<1 || ($d>28 && $m==2))
-					$error['editat'] = trans('This month doesn\'t contain specified number of days');
+					$error['at'] = trans('This month doesn\'t contain specified number of days');
 				if($m>3 || $m<1)
-					$error['editat'] = trans('Incorrect month number (max.3)!');
+					$error['at'] = trans('Incorrect month number (max.3)!');
 
 				$at = ($m-1) * 100 + $d;
 			}
@@ -112,7 +123,7 @@ if(isset($_POST['assignmentedit']))
 		case HALFYEARLY:
 			if(!preg_match('/^[0-9]{2}\/[0-9]{2}$/', $a['at']) && $a['at'])
 			{
-				$error['editat'] = trans('Incorrect date format! Enter date in DD/MM format!');
+				$error['at'] = trans('Incorrect date format! Enter date in DD/MM format!');
 			}
 			elseif(chkconfig($CONFIG['phpui']['use_current_payday']) && !$a['at'])
 			{
@@ -124,13 +135,13 @@ if(isset($_POST['assignmentedit']))
 			{
 				list($d,$m) = explode('/',$a['at']);
 			}
-			
+
 			if(!$error)
 			{
 				if($d>30 || $d<1 || ($d>28 && $m==2))
-					$error['editat'] = trans('This month doesn\'t contain specified number of days');
+					$error['at'] = trans('This month doesn\'t contain specified number of days');
 				if($m>6 || $m<1)
-					$error['editat'] = trans('Incorrect month number (max.6)!');
+					$error['at'] = trans('Incorrect month number (max.6)!');
 
 				$at = ($m-1) * 100 + $d;
 			}
@@ -145,48 +156,48 @@ if(isset($_POST['assignmentedit']))
 			}
 			elseif(!preg_match('/^[0-9]{2}\/[0-9]{2}$/', $a['at']))
 			{
-				$error['editat'] = trans('Incorrect date format! Enter date in DD/MM format!');
+				$error['at'] = trans('Incorrect date format! Enter date in DD/MM format!');
 			}
 			else
 			{
 				list($d,$m) = explode('/',$a['at']);
 			}
-			
+
 			if(!$error)
 			{
 				if($d>30 || $d<1 || ($d>28 && $m==2))
-					$error['editat'] = trans('This month doesn\'t contain specified number of days');
+					$error['at'] = trans('This month doesn\'t contain specified number of days');
 				if($m>12 || $m<1)
-					$error['editat'] = trans('Incorrect month number');
-			
+					$error['at'] = trans('Incorrect month number');
+
 				$ttime = mktime(12, 0, 0, $m, $d, 1990);
 				$at = date('z',$ttime) + 1;
 			}
 		break;
 
 		default: // DISPOSABLE
-                        $period = DISPOSABLE;
-			
-			if(isset($a['tariffid']) && $a['tariffid'] != '0')
+            $period = DISPOSABLE;
+
+			if($a['tariffid'] != -1)
 			{
-    				$a['dateto'] = 0;
-	            		$a['datefrom'] = 0;
+    			$a['dateto'] = '';
+	            $a['datefrom'] = '';
 			}
-		     
-		        if(preg_match('/^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/', $a['at']))
+
+	        if(preg_match('/^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/', $a['at']))
 			{
 				list($y, $m, $d) = explode('/', $a['at']);
 				if(checkdate($m, $d, $y))
 				{
 					$at = mktime(0, 0, 0, $m, $d, $y);
 					if($at < mktime(0,0,0))
-						$error['editat'] = trans('Incorrect date!');
+						$error['at'] = trans('Incorrect date!');
 				}
 				else
-					$error['editat'] = trans('Incorrect date format! Enter date in YYYY/MM/DD format!');
+					$error['at'] = trans('Incorrect date format! Enter date in YYYY/MM/DD format!');
 			}
 			else
-				$error['editat'] = trans('Incorrect date format! Enter date in YYYY/MM/DD format!');
+				$error['at'] = trans('Incorrect date format! Enter date in YYYY/MM/DD format!');
 		break;
 	}
 
@@ -198,10 +209,10 @@ if(isset($_POST['assignmentedit']))
 		if(checkdate($m, $d, $y))
 			$from = mktime(0, 0, 0, $m, $d, $y);
 		else
-			$error['editdatefrom'] = trans('Incorrect charging start time!');
+			$error['datefrom'] = trans('Incorrect charging start time!');
 	}
 	else
-		$error['editdatefrom'] = trans('Incorrect charging start time!');
+		$error['datefrom'] = trans('Incorrect charging start time!');
 
 	if($a['dateto'] == '')
 		$to = 0;
@@ -211,64 +222,89 @@ if(isset($_POST['assignmentedit']))
 		if(checkdate($m, $d, $y))
 			$to = mktime(23, 59, 59, $m, $d, $y);
 		else
-			$error['editdateto'] = trans('Incorrect charging end time!');
+			$error['dateto'] = trans('Incorrect charging end time!');
 	}
 	else
-		$error['editdateto'] = trans('Incorrect charging end time!');
+		$error['dateto'] = trans('Incorrect charging end time!');
 
 	if($to < $from && $to != 0 && $from != 0)
-		$error['editdateto'] = trans('Incorrect date range!');
-
-	if(!$a['liabilityid'] && (!isset($a['tariffid']) || $a['tariffid'] == '0'))
-	{
-		unset($error['editat']);
-		$at = 0;
-	}
+		$error['dateto'] = trans('Incorrect date range!');
 
 	$a['discount'] = str_replace(',','.',$a['discount']);
 	if($a['discount'] == '')
 		$a['discount'] = 0;
 	elseif($a['discount']<0 || $a['discount']>99.99 || !is_numeric($a['discount']))
-		$error['editdiscount'] = trans('Wrong discount value!');
+		$error['discount'] = trans('Wrong discount value!');
 
-        if(isset($a['tariffid']) && $a['tariffid'] != '')
-    		$a['value'] = 0;
-
-	if($a['liabilityid'])
+	if($a['tariffid'] == -1)
+	{
+		unset($error['at']);
+		$at = 0;
+	}
+    else if(!$a['tariffid'])
 	{
 		if($a['name'] == '')
-			$error['editname'] = trans('Liability name/description is required!');
-		if(!preg_match('/^[-]?[0-9.,]+$/', $a['value']))
-			$error['editvalue'] = trans('Incorrect value!');
+			$error['name'] = trans('Liability name is required!');
+		if (!$a['value'])
+		    $error['value'] = trans('Liability value is required!');
+		else if(!preg_match('/^[-]?[0-9.,]+$/', $a['value']))
+			$error['value'] = trans('Incorrect value!');
 	}
 
 	if(!$error) 
 	{
-	        if($a['liabilityid'])
+	    $DB->BeginTrans();
+
+        if($a['liabilityid'])
 		{
-			$a['tariffid'] = 0;
-			$DB->Execute('UPDATE liabilities SET value=?, name=?, taxid=?, prodid=? WHERE id=?',
-			    array(str_replace(',','.',$a['value']),
-				    $a['name'],
-				    $a['taxid'],
-				    $a['prodid'],
-				    $a['liabilityid']
-				    ));
+			if ($a['tariffid']) {
+			    $DB->Execute('DELETE FROM liabilities WHERE id=?', array($a['liabilityid']));
+			    $a['liabilityid'] = 0;
+			}
+            else
+			    $DB->Execute('UPDATE liabilities SET value=?, name=?, taxid=?, prodid=? WHERE id=?',
+			        array(str_replace(',','.',$a['value']),
+				        $a['name'],
+				        intval($a['taxid']),
+				        $a['prodid'],
+				        $a['liabilityid']
+				));
 		}
-										
-		$DB->Execute('UPDATE assignments SET tariffid=?, customerid=?, period=?, at=?, invoice=?, settlement=?, datefrom=?, dateto=?, discount=?  WHERE id=?',
-			    array(  $a['tariffid'], 
-				    $customerid, 
-				    $period, 
-				    $at, 
-				    isset($a['invoice']) ? 1 : 0, 
-				    isset($a['settlement']) ? 1 : 0, 
-				    $from, 
+		else if (!$a['tariffid']) {
+		    $DB->Execute('INSERT INTO liabilities (name, value, taxid, prodid)
+		            VALUES (?, ?, ?, ?)',
+                    array($a['name'],
+                        $a['value'],
+		                intval($a['taxid']),
+		                $a['prodid']
+		            ));
+
+            $a['liabilityid'] = $DB->GetLastInsertID('liabilities'); 
+		}
+
+		if ($a['tariffid'] == -1) {
+		    $a['tariffid'] = 0;
+		    $a['discount'] = 0;
+		    unset($a['invoice']);
+		    unset($a['settlement']);
+		}
+
+		$DB->Execute('UPDATE assignments SET tariffid=?, customerid=?, period=?, at=?,
+		        invoice=?, settlement=?, datefrom=?, dateto=?, discount=?, liabilityid=?
+		        WHERE id=?',
+			    array(intval($a['tariffid']),
+				    $customer['id'],
+				    $period,
+				    $at,
+				    isset($a['invoice']) ? 1 : 0,
+				    isset($a['settlement']) ? 1 : 0,
+				    $from,
 				    $to,
 				    $a['discount'],
+				    $a['liabilityid'],
 				    $a['id'],
-				    ));
-		
+				));
+
 		$DB->Execute('DELETE FROM nodeassignments WHERE assignmentid=?', array($a['id']));
 
 		if(isset($a['nodes']) && sizeof($a['nodes']))
@@ -278,8 +314,12 @@ if(isset($_POST['assignmentedit']))
 					array($nodeid, $a['id']));
 		}
 
+        $DB->CommitTrans();
+
 		$SESSION->redirect('?'.$SESSION->get('backto'));
 	}
+
+    $SMARTY->assign('error', $error);
 }
 else
 {
@@ -296,7 +336,7 @@ else
 		$a['dateto'] = date('Y/m/d', $a['dateto']);
 	if($a['datefrom'])
 		$a['datefrom'] = date('Y/m/d', $a['datefrom']);
-	
+
 	switch($a['period'])
 	{
 		case QUARTERLY:
@@ -310,29 +350,33 @@ else
 			break;
 		case DISPOSABLE:
 			if($a['at'])
-				$a['at'] = date('Y/m/d', $a['at']);
+    			$a['at'] = date('Y/m/d', $a['at']);
 			break;
 	}
-	
+
+    if (!$a['tariffid'] && !$a['liabilityid'])
+        $a['tariffid'] = -1;
+
 	// nodes assignments
-	$a['nodes'] = $DB->GetAllByKey('SELECT nodeid FROM nodeassignments WHERE assignmentid=?', 'nodeid', array($a['id']));
+	$a['nodes'] = $DB->GetCol('SELECT nodeid FROM nodeassignments WHERE assignmentid=?', array($a['id']));
 }
 
 $expired = isset($_GET['expired']) ? $_GET['expired'] : false;
 
-$layout['pagetitle'] = trans('Customer Charging Edit: $0', '<A href="?m=customerinfo&id='.$customerid.'">'.$LMS->GetCustomerName($customerid).'</A>');
+$layout['pagetitle'] = trans('Liability Edit: $0', '<A href="?m=customerinfo&id='.$customer['id'].'">'.$customer['name'].'</A>');
 
 $SESSION->save('backto', $_SERVER['QUERY_STRING']);
 
-$SMARTY->assign('customernodes', $LMS->GetCustomerNodes($customerid));
+$customernodes = $LMS->GetCustomerNodes($customer['id']);
+unset($customernodes['total']);
+
+$SMARTY->assign('customernodes', $customernodes);
 $SMARTY->assign('tariffs', $LMS->GetTariffs());
 $SMARTY->assign('taxeslist', $LMS->GetTaxes());
 $SMARTY->assign('expired', $expired);
-$SMARTY->assign('error', $error);
-$SMARTY->assign('assignmentedit', $a);
-$SMARTY->assign('assignments', $LMS->GetCustomerAssignments($customerid, $expired));
-$balancelist['customerid'] = $customerid;
-$SMARTY->assign('balancelist', $balancelist);
+$SMARTY->assign('assignment', $a);
+$SMARTY->assign('assignments', $LMS->GetCustomerAssignments($customer['id'], $expired));
+$SMARTY->assign('customerinfo', $customer);
 $SMARTY->display('customerassignmentsedit.html');
 
 ?>
