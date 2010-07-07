b72e8679e0fc3feda855d4b88cb2351d16b5746e
Aleksander Machniak <alec@alec.pl>
2010-07-07 11:50:29 +0000

    - fix units setting on graphs

--- modules/trafficgraph.php
+++ modules/trafficgraph.php
@@ -260,15 +260,15 @@ function TrafficGraph ($nodeid, $net=NULL, $customer=NULL, $bar=NULL, $fromdate=
 //	imageline($img,$movx-2,$movy+1,$movx+$graph_width+3,$movy+1,$textcolor);
 //	imageline($img,$movx-2,10,$movx+$graph_width+3,10,$textcolor);
 
-	if ($stats_max/1024>1)
-	{
-		$vdiv = 1024;
-		$suffix = 'kbit/s';
-	}
-	elseif ($stats_max/(1024*1024)>1)
+	if ($stats_max/(1024*1024) > 10)
 	{
 		$vdiv = 1024*1024;
 		$suffix = 'Mbit/s';
+	}
+	else if ($stats_max/1024 > 1)
+	{
+		$vdiv = 1024;
+		$suffix = 'kbit/s';
 	}
 	else
 	{
