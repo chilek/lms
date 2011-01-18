c52a06ba6ac6bb05ac01dd50d8f47368286a4b44
Aleksander Machniak <alec@alec.pl>
2011-01-18 08:11:57 +0000

    - Updated copyright year

--- daemon/lmsd.c
+++ daemon/lmsd.c
@@ -1,7 +1,7 @@
 /*
  * LMS version 1.11-cvs
  *
- *  (C) Copyright 2001-2010 LMS Developers
+ *  (C) Copyright 2001-2011 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
@@ -446,7 +446,7 @@ static void parse_command_line(int argc, char **argv)
 		switch(opt) 
 		{
     		case 'v':
-            		printf("LMS Daemon version 1.11-cvs (%s)\nCopyright (c) 2001-2010 LMS Developers\n", revision);
+            		printf("LMS Daemon version 1.11-cvs (%s)\nCopyright (c) 2001-2011 LMS Developers\n", revision);
             		exit(0);
     		case 's':
             		ssl = 1;
