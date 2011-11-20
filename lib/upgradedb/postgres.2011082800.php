61b91f8d69d11c78d2c010212409b58c77bbc86a
Aleksander Machniak <alec@alec.pl>
2011-11-20 09:37:24 +0000

    - Added lost CREATE VIEW vmacs

--- lib/upgradedb/postgres.2011082800.php
+++ lib/upgradedb/postgres.2011082800.php
@@ -178,6 +178,11 @@ $DB->Execute("
     FROM nodes n
     LEFT JOIN (SELECT nodeid, array_to_string(array_agg(mac), ',') AS mac
         FROM macs GROUP BY nodeid) m ON (n.id = m.nodeid);
+
+    CREATE VIEW vmacs AS
+    SELECT n.*, m.mac, m.id AS macid
+        FROM nodes n
+        JOIN macs m ON (n.id = m.nodeid);
 ");
 
 $DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2011082800', 'dbversion'));
