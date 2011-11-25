3323888e4935ebd481aaf9ba928972df744b7455
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2011-11-25 20:22:55 +0000

    - added permalink to map

--- img/map.js
+++ img/map.js
@@ -145,7 +145,8 @@ function createMap(deviceArray, devlinkArray, nodeArray, nodelinkArray, selectio
 	var map = new OpenLayers.Map("map", {
 		controls: [new OpenLayers.Control.KeyboardDefaults(),
 			new OpenLayers.Control.PanZoomBar(),
-			new OpenLayers.Control.Navigation()] });
+			new OpenLayers.Control.Navigation(),
+			new OpenLayers.Control.Permalink("Permalink")] });
 	var gsat = new OpenLayers.Layer.Google("Google Satellite",
 		{type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 20, visibility: false});
 	var gphy = new OpenLayers.Layer.Google("Google Physical",
