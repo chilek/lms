0a97c25114f61d344195b1104a77bba4b3a22473
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2011-12-07 12:22:46 +0000

    - fixed support for smarty 3.x

--- devel/smarty_install.sh
+++ devel/smarty_install.sh
@@ -3,7 +3,7 @@
 # Smarty templates library quick installation (with sources download)
 #
 
-SMARTYVER="3.1.4"
+SMARTYVER="3.1.6"
 
 cd ../lib
 # download
@@ -19,6 +19,7 @@ echo "done."
 # merging
 echo -n "Merging... "
 mv Smarty-$SMARTYVER/libs/* Smarty/
+mv Smarty-$SMARTYVER/libs/sysplugins/* Smarty/sysplugins/
 mv Smarty-$SMARTYVER/libs/plugins/* Smarty/plugins/
 echo "done."
 
