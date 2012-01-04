03098a1b4fa1735d001e1245df265f85c3da06b2
Aleksander Machniak <alec@alec.pl>
2012-01-04 08:24:30 +0000

    - More <label>'s, added global CheckAll() function

--- img/common.js
+++ img/common.js
@@ -219,6 +219,31 @@ function checkElement(id)
 	}
 }
 
+function CheckAll(form, elem, excl)
+{
+    var i, len, n, e, f,
+        form = document.forms[form] ? document.forms[form] : document.getElementById(form),
+        inputs = form.getElementsByTagName('INPUT');
+
+    for (i=0, len=inputs.length; i<len; i++) {
+        e = inputs[i];
+
+        if (e.type != 'checkbox' || e == elem)
+            continue;
+
+        if (excl && excl.length) {
+            f = 0;
+            for (n=0; n<excl.length; n++)
+                if (e.name == excl[n])
+                    f = 1;
+            if (f)
+                continue;
+        }
+
+        e.checked = elem.checked;
+    }
+}
+
 function get_object_pos(obj)
 {
 	// get old element size/position
