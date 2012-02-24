9d0d0700c373b399095100127ab6685835cc9ee3
Krzysztof Kulesza <prezes@cmza.pl>
2012-02-24 08:18:57 +0100

    localize OpenLayer netmap & update theme for LMS

---
+++ img/OpenLayers/lang/pl.js
@@ -0,0 +1,30 @@
+/**
+ * @requires OpenLayers/Lang.js
+ */
+
+/**
+ * Namespace: OpenLayers.Lang["pl"]
+ * Dictionary for Polish.
+ */
+
+OpenLayers.Lang.pl = {
+	'Permalink': "Permalink",
+	'Overlays': "Nakładki",
+	'Base Layer': "Warstwy",
+
+	//labels for the graticule control
+	'W': 'Z',
+	'E': 'W',
+	'N': 'Pn',
+	'S': 'Pd',
+	'Graticule': 'Siatka',
+
+	// LMS labels
+	'Devices': "Urządzenia",
+	'Device Links': "Połączenia urządzeń",
+	'Nodes': "Komputery",
+	'Node Links': "Połączenia komputerów",
+	'Check a host ...': "Sprawdź hosta ...",
+	'Center map around network elements ...': "Centruj mapę względem urządzeń sieciowych...",
+	'Refesh network state ...': "Odśwież stan sieci ..."
+};
