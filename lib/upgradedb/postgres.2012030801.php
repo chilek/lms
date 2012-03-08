c93ee3a7dfe6057faaf8c54a2e0c5af835fb2b7c
Tomasz Chilinski <tomasz.chilinski@chilan.com>
2012-03-08 10:29:14 +0100

    netlink speed support (stage 1)

--- lib/upgradedb/postgres.2012030801.php
+++ lib/upgradedb/postgres.2012030801.php
@@ -5,6 +5,7 @@
  *
  *  (C) Copyright 2001-2012 LMS Developers
  *
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License Version 2 as
  *  published by the Free Software Foundation.
@@ -23,9 +24,10 @@
 
 $DB->BeginTrans();
 
-$DB->Execute("ALTER TABLE location_streets ADD name2 varchar(128) DEFAULT NULL");
-$DB->Execute("DROP VIEW teryt_ulic");
+// name2 support for teryt location_street
 $DB->Execute("
+	ALTER TABLE location_streets ADD name2 varchar(128) DEFAULT NULL;
+	DROP VIEW teryt_ulic;
 	CREATE VIEW teryt_ulic AS
 		SELECT st.ident AS woj, d.ident AS pow, b.ident AS gmi, b.type AS rodz_gmi,
 			c.ident AS sym, s.ident AS sym_ul, s.name AS nazwa_1, s.name2 AS nazwa_2, t.name AS cecha, s.id
@@ -34,10 +36,13 @@ $DB->Execute("
 		JOIN location_cities c ON (s.cityid = c.id)
 		JOIN location_boroughs b ON (c.boroughid = b.id)
 		JOIN location_districts d ON (b.districtid = d.id)
-		JOIN location_states st ON (d.stateid = st.id)
+		JOIN location_states st ON (d.stateid = st.id);
 ");
 
-$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2012030800', 'dbversion'));
+// netlink speed support
+$DB->Execute("ALTER TABLE netlinks ADD speed integer DEFAULT 100000 NOT NULL");
+
+$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2012030801', 'dbversion'));
 
 $DB->CommitTrans();
 
