6c1e999f830aeb8550597123ccd33550a7e93686
Krzysztof Kulesza <prezes@cmza.pl>
2012-04-14 11:41:02 +0200

    add support for printing PDF documents

--- lib/common.php
+++ lib/common.php
@@ -766,36 +766,84 @@ function register_plugin($handle, $plugin)
         $PLUGINS[$handle][] = $plugin;
 }
 
-function html2pdf($content, $orientation='P', $margins=array(5, 10, 5, 10), $save=false)
+function html2pdf($content, $subject=NULL, $title=NULL, $type=NULL, $id=NULL, $orientation='P', $margins=array(5, 10, 5, 10), $save=false, $copy=false)
 {
-	global $layout;
+	global $layout, $DB;
 	require_once(LIB_DIR.'/html2pdf/html2pdf.class.php');
 
 	if (isset($margins))
 		if (!is_array($margins))
-			$margins = array(5, 10, 5, 10);
+			$margins = array(5, 10, 5, 10); /* default */
 
 	$html2pdf = new HTML2PDF($orientation, 'A4', 'en', true, 'UTF-8', $margins);
 
 	$html2pdf->pdf->SetProducer('LMS Developers');
 	$html2pdf->pdf->SetCreator('LMS '.$layout['lmsv']);
-	$html2pdf->pdf->SetSubject(trans('Reports'));
-	$html2pdf->pdf->SetTitle($layout['pagetitle']);
+	if ($subject)
+		$html2pdf->pdf->SetSubject($subject);
+	if ($title)
+		$html2pdf->pdf->SetTitle($title);
 
 	$html2pdf->pdf->SetDisplayMode('fullpage', 'SinglePage', 'UseNone');
 	$html2pdf->AddFont('arial', '', 'arial.php');
 	$html2pdf->AddFont('arial', 'B', 'arialb.php');
 	$html2pdf->AddFont('arial', 'I', 'ariali.php');
 	$html2pdf->AddFont('arial', 'BI', 'arialbi.php');
+	$html2pdf->AddFont('times', '', 'times.php');
 
 	$html2pdf->WriteHTML($content);
+
+	if ($copy) {
+		/* add watermark only for contract & annex */
+		if(($type == DOC_CONTRACT) || ($type == DOC_ANNEX)) {
+			$PageWidth = $html2pdf->pdf->getPageWidth();
+			$PageHeight = $html2pdf->pdf->getPageHeight();
+			$x = ($PageWidth / 2) - 100;
+			$y = ($PageHeight / 2) + 50;
+			$html2pdf->pdf->SetAlpha(0.2);
+			$html2pdf->pdf->StartTransform();
+			$html2pdf->pdf->Rotate(45, $x, $y);
+			$html2pdf->AddFont('courier', '', 'courier.php');
+			$html2pdf->AddFont('courier', 'B', 'courierb.php');
+			$html2pdf->pdf->SetFont("courier", "B", 120);
+			$html2pdf->pdf->SetTextColor(255, 0, 0);
+			$html2pdf->pdf->Text($x, $y, trim(preg_replace("/(.)/i", "\${1} ", trans('COPY'))));
+			$html2pdf->pdf->StopTransform();
+			$html2pdf->pdf->SetAlpha(1);
+		}
+	}
+
+	if ($type && $id) {
+		$info = $DB->GetRow('SELECT di.name, di.description FROM divisions di
+			LEFT JOIN documents d ON (d.divisionid = di.id)
+			WHERE d.id = ?', array($id));
+	}
+
+	if(($type == DOC_CONTRACT) || ($type == DOC_ANNEX)) {
+		/* set signature additional information */
+		$info = array(
+			'Name' => $info['name'],
+			'Location' => $subject,
+			'Reason' => $title,
+			'ContactInfo' => $info['description'],
+		);
+
+		/* setup your cert & key file */
+		$cert = 'file://'.LIB_DIR.'/tcpdf/config/lms.cert';
+		$key = 'file://'.LIB_DIR.'/tcpdf/config/lms.key';
+
+		/* set document digital signature & protection */
+		if (file_exists($cert) && file_exists($key)) {
+			$html2pdf->pdf->setSignature($cert, $key, 'lms-documents', '', 1, $info);
+		}
+	}
 	$html2pdf->pdf->SetProtection(array('modify', 'annot-forms', 'fill-forms', 'extract', 'assemble'), '', PASSWORD_CHANGEME, '1');
 
 	if ($save) {
 		if (function_exists('mb_convert_encoding'))
-			$filename = mb_convert_encoding($layout['pagetitle'], "ISO-8859-2", "UTF-8");
+			$filename = mb_convert_encoding($title, "ISO-8859-2", "UTF-8");
 		else
-			$filename = iconv("UTF-8", "ISO-8859-2//TRANSLIT", $layout['pagetitle']);
+			$filename = iconv("UTF-8", "ISO-8859-2//TRANSLIT", $title);
 		$html2pdf->Output($filename.'.pdf', 'D');
 	} else {
 		$html2pdf->Output();
