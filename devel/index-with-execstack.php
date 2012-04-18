26965273544458e676c916f9318cb9a9886f2ff3
Tomasz Chiliński <Tomasz.chilinski@chilan.com>
2012-04-18 12:18:31 +0200

    ported to Smarty 3

--- devel/index-with-execstack.php
+++ devel/index-with-execstack.php
@@ -3,7 +3,7 @@
 /*
  * LMS version 1.11-git
  *
- *  (C) Copyright 2001-2005 LMS Developers
+ *  (C) Copyright 2001-2012 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
@@ -25,86 +25,38 @@
  */
 
 // REPLACE THIS WITH PATH TO YOUR CONFIG FILE
+// find alternative config files:
+if (is_readable('lms.ini'))
+	$CONFIG_FILE = 'lms.ini';
+elseif (is_readable('/etc/lms/lms-' . $_SERVER['HTTP_HOST'] . '.ini'))
+	$CONFIG_FILE = '/etc/lms/lms-' . $_SERVER['HTTP_HOST'] . '.ini';
+elseif (!is_readable($CONFIG_FILE))
+	die('Unable to read configuration file [' . $CONFIG_FILE . ']!');
 
-$CONFIG_FILE = (is_readable('lms.ini')) ? 'lms.ini' : '/etc/lms/lms.ini';
-
-// PLEASE DO NOT MODIFY ANYTHING BELOW THIS LINE UNLESS YOU KNOW
-// *EXACTLY* WHAT ARE YOU DOING!!!
-// *******************************************************************
-
-// Parse configuration file
-function lms_parse_ini_file($filename, $process_sections = false) 
-{
-	$ini_array = array();
-	$section = '';
-	$lines = file($filename);
-	foreach($lines as $line) 
-	{
-		$line = trim($line);
-		
-		if($line == '' || $line[0] == ';' || $line[0] == '#') 
-			continue;
-		
-		list($sec_name) = sscanf($line, "[%[^]]");
-		
-		if( $sec_name )
-			$section = trim($sec_name);
-		else 
-		{
-			list($property, $value) = sscanf($line, "%[^=] = '%[^']'");
-			if ( !$property || !$value ) 
-			{
-				list($property, $value) = sscanf($line, "%[^=] = \"%[^\"]\"");
-				if ( !$property || !$value ) 
-				{
-					list($property, $value) = sscanf($line, "%[^=] = %[^;#]");
-					if( !$property || !$value ) 
-						continue;
-					else
-						$value = trim($value, "\"'");
-				}
-			}
-		
-			$property = trim($property);
-			$value = trim($value);
-			
-			if($process_sections) 
-				$ini_array[$section][$property] = $value;
-			else 
-				$ini_array[$property] = $value;
-		}
-	}
-	
-	return $ini_array;
-}
-
-$CONFIG = array();
-
-foreach(lms_parse_ini_file($CONFIG_FILE, true) as $key => $val)
-	$CONFIG[$key] = $val;
+$CONFIG = (array) parse_ini_file($CONFIG_FILE, true);
 
 // Check for configuration vars and set default values
 $CONFIG['directories']['sys_dir'] = (!isset($CONFIG['directories']['sys_dir']) ? getcwd() : $CONFIG['directories']['sys_dir']);
-$CONFIG['directories']['backup_dir'] = (!isset($CONFIG['directories']['backup_dir']) ? $CONFIG['directories']['sys_dir'].'/backups' : $CONFIG['directories']['backup_dir']);
-$CONFIG['directories']['doc_dir'] = (!isset($CONFIG['directories']['doc_dir']) ? $CONFIG['directories']['sys_dir'].'/documents' : $CONFIG['directories']['doc_dir']);
-$CONFIG['directories']['lib_dir'] = (!isset($CONFIG['directories']['lib_dir']) ? $CONFIG['directories']['sys_dir'].'/lib' : $CONFIG['directories']['lib_dir']);
-$CONFIG['directories']['modules_dir'] = (!isset($CONFIG['directories']['modules_dir']) ? $CONFIG['directories']['sys_dir'].'/modules' : $CONFIG['directories']['modules_dir']);
-$CONFIG['directories']['config_templates_dir'] = (!isset($CONFIG['directories']['config_templates_dir']) ? $CONFIG['directories']['sys_dir'].'/config_templates' : $CONFIG['directories']['config_templates_dir']);
-$CONFIG['directories']['smarty_compile_dir'] = (!isset($CONFIG['directories']['smarty_compile_dir']) ? $CONFIG['directories']['sys_dir'].'/templates_c' : $CONFIG['directories']['smarty_compile_dir']);
-$CONFIG['directories']['smarty_templates_dir'] = (!isset($CONFIG['directories']['smarty_templates_dir']) ? $CONFIG['directories']['sys_dir'].'/templates' : $CONFIG['directories']['smarty_templates_dir']);
+$CONFIG['directories']['backup_dir'] = (!isset($CONFIG['directories']['backup_dir']) ? $CONFIG['directories']['sys_dir'] . '/backups' : $CONFIG['directories']['backup_dir']);
+$CONFIG['directories']['doc_dir'] = (!isset($CONFIG['directories']['doc_dir']) ? $CONFIG['directories']['sys_dir'] . '/documents' : $CONFIG['directories']['doc_dir']);
+$CONFIG['directories']['lib_dir'] = (!isset($CONFIG['directories']['lib_dir']) ? $CONFIG['directories']['sys_dir'] . '/lib' : $CONFIG['directories']['lib_dir']);
+$CONFIG['directories']['modules_dir'] = (!isset($CONFIG['directories']['modules_dir']) ? $CONFIG['directories']['sys_dir'] . '/modules' : $CONFIG['directories']['modules_dir']);
+$CONFIG['directories']['config_templates_dir'] = (!isset($CONFIG['directories']['config_templates_dir']) ? $CONFIG['directories']['sys_dir'] . '/config_templates' : $CONFIG['directories']['config_templates_dir']);
+$CONFIG['directories']['smarty_compile_dir'] = (!isset($CONFIG['directories']['smarty_compile_dir']) ? $CONFIG['directories']['sys_dir'] . '/templates_c' : $CONFIG['directories']['smarty_compile_dir']);
+$CONFIG['directories']['smarty_templates_dir'] = (!isset($CONFIG['directories']['smarty_templates_dir']) ? $CONFIG['directories']['sys_dir'] . '/templates' : $CONFIG['directories']['smarty_templates_dir']);
 
 define('SYS_DIR', $CONFIG['directories']['sys_dir']);
 define('LIB_DIR', $CONFIG['directories']['lib_dir']);
 define('DOC_DIR', $CONFIG['directories']['doc_dir']);
 define('BACKUP_DIR', $CONFIG['directories']['backup_dir']);
-define('MODULES_DIR', = $CONFIG['directories']['modules_dir']);
-define('SMARTY_COMPILE_DIR', = $CONFIG['directories']['smarty_compile_dir']);
-define('SMARTY_TEMPLATES_DIR', = $CONFIG['directories']['smarty_templates_dir']);
+define('MODULES_DIR', $CONFIG['directories']['modules_dir']);
+define('SMARTY_COMPILE_DIR', $CONFIG['directories']['smarty_compile_dir']);
+define('SMARTY_TEMPLATES_DIR', $CONFIG['directories']['smarty_templates_dir']);
 
 // Do some checks and load config defaults
 
-require_once(LIB_DIR.'/checkdirs.php');
-require_once(LIB_DIR.'/config.php');
+require_once(LIB_DIR . '/checkdirs.php');
+require_once(LIB_DIR . '/config.php');
 
 // Init database 
 
@@ -114,42 +66,41 @@ $_DBUSER = $CONFIG['database']['user'];
 $_DBPASS = $CONFIG['database']['password'];
 $_DBNAME = $CONFIG['database']['database'];
 
-require_once(LIB_DIR.'/LMSDB.php');
+require_once(LIB_DIR . '/LMSDB.php');
 
 $DB = DBInit($_DBTYPE, $_DBHOST, $_DBUSER, $_DBPASS, $_DBNAME);
 
 // Call any of upgrade process before anything else
 
-require_once(LIB_DIR.'/upgradedb.php');
-
-// Initialize templates engine
-
-require_once(LIB_DIR.'/Smarty/Smarty.class.php');
-
-$SMARTY = new Smarty;
+require_once(LIB_DIR . '/upgradedb.php');
 
 // test for proper version of Smarty
 
-if(version_compare('2.6.0', $SMARTY->_version) > 0)
-	die('<B>Old version of Smarty engine! You must get newest from <A HREF="http://www.smarty.net/distributions/Smarty-2.6.12.tar.gz">http://www.smarty.net/distributions/Smarty-2.6.12.tar.gz</A></B>');
+if (defined('Smarty::SMARTY_VERSION'))
+	$ver_chunks = preg_split('/[- ]/', Smarty::SMARTY_VERSION);
+else
+	$ver_chunks = NULL;
+if (count($ver_chunks) != 2 || version_compare('3.0', $ver_chunks[1]) > 0)
+	die('<B>Wrong version of Smarty engine! We support only Smarty-3.x greater than 3.0.</B>');
+
+define('SMARTY_VERSION', $ver_chunks[1]);
 
 // system localization
 
-require_once(LIB_DIR.'/language.php');
+require_once(LIB_DIR . '/language.php');
 
 // Read configuration of LMS-UI from database
 
-if($cfg = $DB->GetAll('SELECT section, var, value FROM uiconfig WHERE disabled=0'))
-	foreach($cfg as $row)
+if ($cfg = $DB->GetAll('SELECT section, var, value FROM uiconfig WHERE disabled=0'))
+	foreach ($cfg as $row)
 		$CONFIG[$row['section']][$row['var']] = $row['value'];
 
 // Redirect to SSL
 
 $_FORCE_SSL = (isset($CONFIG['phpui']['force_ssl']) ? chkconfig($CONFIG['phpui']['force_ssl']) : FALSE);
 
-if($_FORCE_SSL && $_SERVER['HTTPS'] != 'on')
-{
-	header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+if ($_FORCE_SSL && $_SERVER['HTTPS'] != 'on') {
+	header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 	exit(0);
 }
 
@@ -157,35 +108,33 @@ if($_FORCE_SSL && $_SERVER['HTTPS'] != 'on')
 
 $_LMSDIR = dirname(__FILE__);
 
-require_once(LIB_DIR.'/ExecStack.class.php');
+require_once(LIB_DIR . '/ExecStack.class.php');
 
-$ExecStack = new ExecStack($_LMSDIR.'/modules/', (isset($_GET['m']) ? $_GET['m'] : NULL), (isset($_GET['a']) ? $_GET['a'] : NULL));
+$ExecStack = new ExecStack($_LMSDIR . '/modules/', (isset($_GET['m']) ? $_GET['m'] : NULL), (isset($_GET['a']) ? $_GET['a'] : NULL));
 
 // don't use foreach() below, because privileges checking action
 // will probably need to remove some actions from stack
 // Note: that action should be executed first
 
-while(list(, $execute) = each($ExecStack->_EXECSTACK['actions']))
-{
+while (list(, $execute) = each($ExecStack->_EXECSTACK['actions'])) {
 	// do include once, because testing that language for executed module has been already loaded
 	// will take some time, so let's PHP decide if we already loaded it or what...
 
-	@include_once($_LMSDIR.'/modules/'.$execute['module'].'/lang/'.$_language.'.php');
-	@include_once($_LMSDIR.'/modules/'.$execute['module'].'/modinit.php');
+	@include_once($_LMSDIR . '/modules/' . $execute['module'] . '/lang/' . $_language . '.php');
+	@include_once($_LMSDIR . '/modules/' . $execute['module'] . '/modinit.php');
 
 	// execute action
 
-	if($ExecStack->needExec($execute['module'], $execute['action']))
-		include($_LMSDIR.'/modules/'.$execute['module'].'/actions/'.$execute['action'].'.php');
+	if ($ExecStack->needExec($execute['module'], $execute['action']))
+		include($_LMSDIR . '/modules/' . $execute['module'] . '/actions/' . $execute['action'] . '.php');
 }
 
-foreach($ExecStack->_EXECSTACK['templates'] as $step => $execute)
-	$SMARTY->display($_LMSDIR.'/modules/'.$execute['module'].'/templates/'.$execute['template'].'.html');
+foreach ($ExecStack->_EXECSTACK['templates'] as $step => $execute)
+	$SMARTY->display($_LMSDIR . '/modules/' . $execute['module'] . '/templates/' . $execute['template'] . '.html');
 
 $DB->Destroy();
 
 
 echo '<PRE>';
 print_r($ExecStack);
-
 ?>
