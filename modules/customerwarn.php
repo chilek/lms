819b677bdda7078b53a80775f487377fea7bee26
Tomasz Chilinski <tomasz.chilinski@chilan.com>
2012-04-28 13:38:32 +0200

    is_int doesn't work as we'd like to, so replaced by is_numeric as workaround

--- modules/customerwarn.php
+++ modules/customerwarn.php
@@ -32,7 +32,7 @@ if (isset($setwarnings['mcustomerid']))
 	$warnoff = isset($setwarnings['warnoff']) ? $setwarnings['warnoff'] : FALSE;
 	$message = isset($setwarnings['message']) ? $setwarnings['message'] : NULL;
 
-	$cids = implode(',', array_values(array_filter($setwarnings['mcustomerid'], "is_int")));
+	$cids = implode(',', array_values(array_filter($setwarnings['mcustomerid'], 'is_numeric')));
 	if (!empty($cids)) {
 		$DB->Execute('UPDATE nodes SET warning = ? WHERE ownerid IN (' . $cids . ')',
 			array($warnon ? 1 : 0));
