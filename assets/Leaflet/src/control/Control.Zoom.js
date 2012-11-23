8861aa9f5cc6a9c8a09b21c23a1f16bab63043bd
Łukasz Kopiszka <lukasz@alfa-system.pl>
2012-11-23 10:24:05 +0100

    add leaflet

---
+++ assets/Leaflet/src/control/Control.Zoom.js
@@ -0,0 +1,56 @@
+L.Control.Zoom = L.Control.extend({
+	options: {
+		position: 'topleft'
+	},
+
+	onAdd: function (map) {
+		var className = 'leaflet-control-zoom',
+		    container = L.DomUtil.create('div', className);
+
+		this._map = map;
+
+		this._createButton('Zoom in', className + '-in', container, this._zoomIn, this);
+		this._createButton('Zoom out', className + '-out', container, this._zoomOut, this);
+
+		return container;
+	},
+
+	_zoomIn: function (e) {
+		this._map.zoomIn(e.shiftKey ? 3 : 1);
+	},
+
+	_zoomOut: function (e) {
+		this._map.zoomOut(e.shiftKey ? 3 : 1);
+	},
+
+	_createButton: function (title, className, container, fn, context) {
+		var link = L.DomUtil.create('a', className, container);
+		link.href = '#';
+		link.title = title;
+
+		L.DomEvent
+		    .on(link, 'click', L.DomEvent.stopPropagation)
+		    .on(link, 'mousedown', L.DomEvent.stopPropagation)
+		    .on(link, 'dblclick', L.DomEvent.stopPropagation)
+		    .on(link, 'click', L.DomEvent.preventDefault)
+		    .on(link, 'click', fn, context);
+
+		return link;
+	}
+});
+
+L.Map.mergeOptions({
+	zoomControl: true
+});
+
+L.Map.addInitHook(function () {
+	if (this.options.zoomControl) {
+		this.zoomControl = new L.Control.Zoom();
+		this.addControl(this.zoomControl);
+	}
+});
+
+L.control.zoom = function (options) {
+	return new L.Control.Zoom(options);
+};
+
