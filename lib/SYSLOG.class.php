0aae3685a4d173c8e51828fa062b28e0cfe7d095
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2013-04-29 00:06:02 +0200

    definitions required even if logging is disabled

--- lib/SYSLOG.class.php
+++ lib/SYSLOG.class.php
@@ -1,3 +1,103 @@
 <?php
 
+define('SYSLOG_RES_USER', 1);
+define('SYSLOG_RES_ASSIGN', 2);
+define('SYSLOG_RES_LIAB', 3);
+define('SYSLOG_RES_NODEASSIGN', 4);
+define('SYSLOG_RES_NODE', 5);
+define('SYSLOG_RES_MAC', 6);
+define('SYSLOG_RES_CUST', 7);
+define('SYSLOG_RES_CUSTCONTACT', 8);
+define('SYSLOG_RES_IMCONTACT', 9);
+define('SYSLOG_RES_CUSTGROUP', 10);
+define('SYSLOG_RES_CUSTASSIGN', 11);
+define('SYSLOG_RES_TARIFF', 12);
+define('SYSLOG_RES_NODEGROUP', 13);
+define('SYSLOG_RES_NODEGROUPASSIGN', 14);
+define('SYSLOG_RES_TAX', 15);
+define('SYSLOG_RES_NUMPLAN', 16);
+define('SYSLOG_RES_NUMPLANASSIGN', 17);
+define('SYSLOG_RES_DIV', 18);
+define('SYSLOG_RES_COUNTRY', 19);
+define('SYSLOG_RES_STATE', 20);
+define('SYSLOG_RES_ZIP', 21);
+define('SYSLOG_RES_HOST', 22);
+define('SYSLOG_RES_DAEMONINST', 23);
+define('SYSLOG_RES_DAEMONCONF', 24);
+define('SYSLOG_RES_CASHSOURCE', 25);
+define('SYSLOG_RES_UICONF', 26);
+define('SYSLOG_RES_PROMO', 27);
+define('SYSLOG_RES_PROMOSCHEMA', 28);
+define('SYSLOG_RES_PROMOASSIGN', 29);
+define('SYSLOG_RES_EXCLGROUP', 30);
+define('SYSLOG_RES_DBBACKUP', 31);
+
+$SYSLOG_RESOURCES = array(
+	SYSLOG_RES_USER => trans('user<!syslog>'),
+	SYSLOG_RES_ASSIGN => trans('assignment<!syslog>'),
+	SYSLOG_RES_LIAB => trans('liability<!syslog>'),
+	SYSLOG_RES_NODEASSIGN => trans('node assignment<!syslog>'),
+	SYSLOG_RES_NODE => trans('node<!syslog>'),
+	SYSLOG_RES_MAC => trans('mac<!syslog>'),
+	SYSLOG_RES_CUST => trans('customer<!syslog>'),
+	SYSLOG_RES_CUSTCONTACT => trans('customer contact<!syslog>'),
+	SYSLOG_RES_IMCONTACT => trans('IM contact<!syslog>'),
+	SYSLOG_RES_CUSTGROUP => trans('customer group<!syslog>'),
+	SYSLOG_RES_CUSTASSIGN => trans('customer assignment<!syslog>'),
+	SYSLOG_RES_TARIFF => trans('tariff<!syslog>'),
+	SYSLOG_RES_NODEGROUP => trans('node group<!syslog>'),
+	SYSLOG_RES_NODEGROUPASSIGN => trans('node group assignment<!syslog>'),
+	SYSLOG_RES_TAX => trans('tax rate<!syslog>'),
+	SYSLOG_RES_NUMPLAN => trans('number plan<!syslog>'),
+	SYSLOG_RES_NUMPLANASSIGN => trans('number plan assignment<!syslog>'),
+	SYSLOG_RES_DIV => trans('division<!syslog>'),
+	SYSLOG_RES_COUNTRY => trans('country<!syslog>'),
+	SYSLOG_RES_STATE => trans('state<!syslog>'),
+	SYSLOG_RES_ZIP => trans('zip code<!syslog>'),
+	SYSLOG_RES_HOST => trans('host<!syslog>'),
+	SYSLOG_RES_DAEMONINST => trans('daemon instance<!syslog>'),
+	SYSLOG_RES_DAEMONCONF => trans('daemon instance setting<!syslog>'),
+	SYSLOG_RES_CASHSOURCE => trans('cash import source<!syslog>'),
+	SYSLOG_RES_UICONF => trans('configuration setting<!syslog>'),
+	SYSLOG_RES_PROMO => trans('promotion<!syslog>'),
+	SYSLOG_RES_PROMOSCHEMA => trans('promotion schema<!syslog>'),
+	SYSLOG_RES_PROMOASSIGN => trans('promotion schema assignment<!syslog>'),
+	SYSLOG_RES_EXCLGROUP => trans('customer group exclusion<!syslog>'),
+	SYSLOG_RES_DBBACKUP => trans('database backup<!syslog>'),
+);
+
+$SYSLOG_RESOURCE_KEYS = array(
+	SYSLOG_RES_USER => 'userid',
+	SYSLOG_RES_ASSIGN => 'assignmentid',
+	SYSLOG_RES_LIAB => 'liabilityid',
+	SYSLOG_RES_NODEASSIGN => 'nodeassignmentid',
+	SYSLOG_RES_NODE => 'nodeid',
+	SYSLOG_RES_MAC => 'macid',
+	SYSLOG_RES_CUST => 'customerid',
+	SYSLOG_RES_CUSTCONTACT => 'customercontactid',
+	SYSLOG_RES_IMCONTACT => 'imessengerid',
+	SYSLOG_RES_CUSTGROUP => 'customergroupid',
+	SYSLOG_RES_CUSTASSIGN => 'customerassignmentid',
+	SYSLOG_RES_TARIFF => 'tariffid',
+	SYSLOG_RES_NODEGROUP => 'nodegroupid',
+	SYSLOG_RES_NODEGROUPASSIGN => 'nodegroupassignmentid',
+	SYSLOG_RES_TAX => 'taxrateid',
+	SYSLOG_RES_NUMPLAN => 'numberplanid',
+	SYSLOG_RES_NUMPLANASSIGN => 'numberplanassignmentid',
+	SYSLOG_RES_DIV => 'divisionid',
+	SYSLOG_RES_COUNTRY => 'countryid',
+	SYSLOG_RES_STATE => 'stateid',
+	SYSLOG_RES_ZIP => 'zipcodeid',
+	SYSLOG_RES_HOST => 'hostid',
+	SYSLOG_RES_DAEMONINST => 'daemoninstanceid',
+	SYSLOG_RES_DAEMONCONF => 'daemonconfigid',
+	SYSLOG_RES_CASHSOURCE => 'cashsourceid',
+	SYSLOG_RES_UICONF => 'uiconfigid',
+	SYSLOG_RES_PROMO => 'promotionid',
+	SYSLOG_RES_PROMOSCHEMA => 'promotionschemaid',
+	SYSLOG_RES_PROMOASSIGN => 'promotionassignmentid',
+	SYSLOG_RES_EXCLGROUP => 'excludedgroupid',
+	SYSLOG_RES_DBBACKUP => null,
+);
+
 ?>
