b98117a9686140e4c97e065eaf7f998f370e7b94
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2014-01-16 16:39:56 +0100

    transfer form converted to utf-8

--- userpanel/modules/finances/transferform.php
+++ userpanel/modules/finances/transferform.php
@@ -44,7 +44,7 @@ if ($division) {
 		$ISP2_DO = $CONFIG['finances']['line_2'];
 }
 
-$USER_T1 = (!isset($CONFIG['finances']['pay_title']) ? 'Abonament - ID:%CID% %LongCID%' : iconv('UTF-8','ISO-8859-2',$CONFIG['finances']['pay_title']));
+$USER_T1 = (!isset($CONFIG['finances']['pay_title']) ? 'Abonament - ID:%CID% %LongCID%' : $CONFIG['finances']['pay_title']);
 $CURR = 'PLN';
 $SHORT_TO_WORDS = check_conf('phpui.to_words_short_version');
 
@@ -52,24 +52,21 @@ $Before = array ("%CID%", "%LongCID%");
 $After = array ($customer['id'], sprintf('%04d',$customer['id']));
 $USER_TY = str_replace($Before,$After,$USER_T1);
 $KWOTA = trim($customer['balance']*-1);
-$ISP1_DO = iconv('UTF-8','ISO-8859-2',$ISP1_DO);
-$ISP2_DO = iconv('UTF-8','ISO-8859-2',$ISP2_DO);
-$USER_OD = trim(iconv('UTF-8','ISO-8859-2', $customer['customername']));
-$USER_ADDR = trim(iconv('UTF-8','ISO-8859-2', $customer['zip'].' '.$customer['city'].' '.$customer['address']));
+$USER_OD = trim($customer['customername']);
+$USER_ADDR = trim($customer['zip'].' '.$customer['city'].' '.$customer['address']);
 $KWOTA_NR = str_replace(',','.',$KWOTA);  // na wszelki wypadek
 $KWOTA_GR = sprintf('%02d',round(($KWOTA_NR - floor($KWOTA_NR))*100));
 
 if($SHORT_TO_WORDS)
 {
 	$KWOTA_ZL = to_words(floor($KWOTA_NR), 0, '', 1);
-	$KWOTA_ZL = iconv('UTF-8','ISO-8859-2',$KWOTA_ZL);
 	$KWOTA_X = $KWOTA_ZL .' '. $KWOTA_GR. '/100';
 }
 else
 {
 	$KWOTA_ZL = to_words(floor($KWOTA_NR));
 	$KWOTA_GR = to_words($KWOTA_GR);
-	$KWOTA_X = iconv('UTF-8', 'ISO-8859-2', trans('$a dollars $b cents', $KWOTA_ZL, $KWOTA_GR));
+	$KWOTA_X = trans('$a dollars $b cents', $KWOTA_ZL, $KWOTA_GR);
 }
 
 ?>
@@ -77,21 +74,21 @@ else
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 //EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl">
 <head>
-  <meta content="text/html; charset=ISO-8859-2" http-equiv="content-type" />
-  <title>Wirtualne Biuro Obs≥ugi Klienta</title>
+  <meta content="text/html; charset=utf-8" http-equiv="content-type" />
+  <title>Wirtualne Biuro Obs≈Çugi Klienta</title>
 </head>
 <body>
 
 <?php
 
-$SHIFT=394; // drugi druczek przesuniÍcie o 394
+$SHIFT=394; // drugi druczek przesuniƒôcie o 394
 
-for ( $j=0; $j<2; $j++ ) // pÍtla g≥Ûwna
+for ( $j=0; $j<2; $j++ ) // pƒôtla g≈Ç√≥wna
 {
 // teksty na druczku:
 
      $posx=60+$j*$SHIFT; 
-     echo('<div style="position: absolute; top: '. $posx .'px; left: 10px"><img src="modules/finances/style/default/przelew.png" border="0" alt="wp≥ata gotÛwkowa" /></div>');
+     echo('<div style="position: absolute; top: '. $posx .'px; left: 10px"><img src="modules/finances/style/default/przelew.png" border="0" alt="wp≈Çata got√≥wkowa" /></div>');
      $posx=63+$j*$SHIFT;
      echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Arial, Helvetica;color: #FF0000;font-size: 6pt;">nazwa odbiorcy</span>');
      $posx=96+$j*$SHIFT;
@@ -103,24 +100,24 @@ for ( $j=0; $j<2; $j++ ) // pÍtla g≥Ûwna
      $posx=163+$j*$SHIFT;
      echo('<span style="position: absolute; top: '. $posx .'px; left: 352px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">kwota</span>');
      $posx=194+$j*$SHIFT;
-     echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">kwota s≥ownie</span>');
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">kwota s≈Çownie</span>');
      $posx=222+$j*$SHIFT;
      echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">nazwa zleceniodawcy</span>');
      $posx=253+$j*$SHIFT;
      echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">nazwa zleceniodawcy cd.</span>');
      $posx=284+$j*$SHIFT;
-     echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">tytu≥em</span>');
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">tytu≈Çem</span>');
      $posx=317+$j*$SHIFT;
-     echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">tytu≥em cd.</span>');
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 72px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">tytu≈Çem cd.</span>');
      $posx=395+$j*$SHIFT;
-     echo('<span style="position: absolute; top: '. $posx .'px; left: 337px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">Op≥ata</span>');
+     echo('<span style="position: absolute; top: '. $posx .'px; left: 337px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">Op≈Çata</span>');
      $posx=425+$j*$SHIFT;
      echo('<span style="position: absolute; top: '. $posx .'px; left: 337px; font-family: Arial, Helvetica; color: #FF0000;font-size: 6pt;">Podpis</span>');
 
 // waluta:
 
      $posx=174+$j*$SHIFT;
-     for ( $i=0, $len=min(strlen($CURR), 27); $i<$len; $i++ ) 
+     for ( $i=0, $len=min(mb_strlen($CURR), 27); $i<$len; $i++ ) 
      {
           $posy=272+$i*19;
           echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'.$CURR[$i].'</span>');
@@ -128,10 +125,10 @@ for ( $j=0; $j<2; $j++ ) // pÍtla g≥Ûwna
 
 // nazwa beneficjenta:
      
-     if (strlen($ISP1_DO)>27)  // jeøeli nazwa 27 znaki _nie_ wpisujemy w kratki
+     if (mb_strlen($ISP1_DO)>27)  // je≈ºeli nazwa 27 znaki _nie_ wpisujemy w kratki
      {
           $posx=75+$j*$SHIFT;
-          echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. substr($ISP1_DO,0,50) .'</span>');
+          echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. mb_substr($ISP1_DO,0,50) .'</span>');
      }
      else
      {
@@ -139,14 +136,14 @@ for ( $j=0; $j<2; $j++ ) // pÍtla g≥Ûwna
           for ( $i=0; $i<27; $i++ ) 
           {
                $posy=62+$i*19;
-               echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'.$ISP1_DO[$i].'</span>');
+               echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">' . mb_substr($ISP1_DO, $i, 1) .'</span>');
           }
      }
 
-     if (strlen($ISP2_DO)>27)  // jeøeli nazwa 27 znaki _nie_ wpisujemy w kratki
+     if (mb_strlen($ISP2_DO)>27)  // je≈ºeli nazwa 27 znaki _nie_ wpisujemy w kratki
      {
           $posx=109+$j*$SHIFT;
-          echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. substr($ISP2_DO,0,50) .'</span>');
+          echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. mb_substr($ISP2_DO,0,50) .'</span>');
      }
      else
      {
@@ -154,14 +151,14 @@ for ( $j=0; $j<2; $j++ ) // pÍtla g≥Ûwna
           for ( $i=0; $i<27; $i++ ) 
           {
                $posy=62+$i*19;
-               echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'.$ISP2_DO[$i].'</span>');
+               echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">' . mb_substr($ISP2_DO, $i, 1) . '</span>');
           }
      }
 
 // numer konta beneficjenta:
 
      $posx=141+$j*$SHIFT;
-     for ( $i=0, $len=min(strlen($KONTO_DO), 26); $i<$len; $i++ )
+     for ( $i=0, $len=min(mb_strlen($KONTO_DO), 26); $i<$len; $i++ )
      {
           $posy=62+$i*19;
           echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'.$KONTO_DO[$i].'</span>');
@@ -172,64 +169,64 @@ for ( $j=0; $j<2; $j++ ) // pÍtla g≥Ûwna
      $posx=174+$j*$SHIFT;
      $KWOTA_SL = sprintf("%0'12.2f", $KWOTA_NR);
      $KWOTA_SL = str_replace('.',',',$KWOTA_SL);
-     for ( $i=0, $len=min(strlen($KWOTA_SL), 12); $i<$len; $i++ ) 
+     for ( $i=0, $len=min(mb_strlen($KWOTA_SL), 12); $i<$len; $i++ ) 
      {
           $posy=347+$i*19;
-          echo('<span style="position: absolute; top: '. $posx .'px; left: ' . $posy . 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. $KWOTA_SL[$i] .'</span>');
+          echo('<span style="position: absolute; top: '. $posx .'px; left: ' . $posy . 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">' .  mb_substr($KWOTA_SL, $i, 1) . '</span>');
      }
 
-// kwota s≥ownie:
+// kwota s≈Çownie:
 
      $posx=205+$j*$SHIFT;
      echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 8pt; font-weight: bold;">'.$KWOTA_X.'</span>');
 
-// dane p≥atnika:
+// dane p≈Çatnika:
 
 
-     if (strlen($USER_OD)>27)  // jeøeli nazwa 27 znaki _nie_ wpisujemy w kratki
+     if (mb_strlen($USER_OD)>27)  // je≈ºeli nazwa 27 znaki _nie_ wpisujemy w kratki
      {
           $posx=235+$j*$SHIFT;
-          echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. substr($USER_OD,0,50) .'</span>');
+          echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. mb_substr($USER_OD,0,50) .'</span>');
      }
-     else                // jeøeli nazwa+adres zmieszcz± siÍ w kratkach to wpisujemy w kratkach
+     else                // je≈ºeli nazwa+adres zmieszczƒÖ siƒô w kratkach to wpisujemy w kratkach
      {                    
           $posx=235+$j*$SHIFT;
-          for ( $i=0, $len=min(strlen($USER_OD), 27); $i<$len; $i++ ) 
+          for ( $i=0, $len=min(mb_strlen($USER_OD), 27); $i<$len; $i++ ) 
           {
                $posy=62+$i*19;
-               echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. $USER_OD[$i].'</span>');
+               echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. mb_substr($USER_OD, $i, 1) .'</span>');
           }
      }
 
-     if (strlen($USER_ADDR)>27)  // jeøeli adres jest d≥uøszy niz 27 znakÛw _nie_ wpisujemy w kratki
+     if (mb_strlen($USER_ADDR)>27)  // je≈ºeli adres jest d≈Çu≈ºszy niz 27 znak√≥w _nie_ wpisujemy w kratki
      {
           $posx=265+$j*$SHIFT;
-          echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. substr($USER_ADDR,0,50) .'</span>');
+          echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. mb_substr($USER_ADDR,0,50) .'</span>');
      }
-     else                // jeøeli nazwa+adres zmieszcz± siÍ w kratkach to wpisujemy w kratkach
+     else                // je≈ºeli nazwa+adres zmieszczƒÖ siƒô w kratkach to wpisujemy w kratkach
      {                    
           $posx=265+$j*$SHIFT;
-          for ( $i=0, $len=min(strlen($USER_ADDR), 27); $i<$len; $i++ ) 
+          for ( $i=0, $len=min(mb_strlen($USER_ADDR), 27); $i<$len; $i++ ) 
           {
                $posy=62+$i*19;
-               echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. $USER_ADDR[$i].'</span>');
+               echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. mb_substr($USER_ADDR, $i, 1) .'</span>');
           }
      }
 
-// tytu≥em:
+// tytu≈Çem:
 
      $posx=298+$j*$SHIFT;
-     for ( $i=0, $len=min(strlen($USER_TY), 27); $i<$len; $i++ ) 
+     for ( $i=0, $len=min(mb_strlen($USER_TY), 27); $i<$len; $i++ ) 
      {
           $posy=62+$i*19;
-          echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">'. $USER_TY[$i].'</span>');
+          echo('<span style="position: absolute; top: '. $posx .'px; left: '. $posy. 'px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">' . mb_substr($USER_TY, $i, 1) . '</span>');
      }
 
 
      $posx=327+$j*$SHIFT;   // wolna linijka
      echo('<span style="position: absolute; top: '. $posx .'px; left: 62px; font-family: Courier, Arial, Helvetica; font-size: 12pt; font-weight: bold;">-</span>');
 
-} //  koniec pÍtli g≥Ûwnej
+} //  koniec pƒôtli g≈Ç√≥wnej
 
 ?>
 </body>
