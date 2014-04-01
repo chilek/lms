ed13b3922ce6bd81bef44b1f007e8fc4400da5cf
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2014-04-01 15:16:36 +0200

    copy shortname fromdivisions to documents

---
+++ lib/upgradedb/postgres.2014040100.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * LMS version 1.11-git
+ *
+ *  (C) Copyright 2001-2013 LMS Developers
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ */
+
+$DB->BeginTrans();
+
+$DB->Execute("ALTER TABLE documents ADD div_shortname TEXT NOT NULL DEFAULT ''");
+
+$dl = $DB->GetAll("SELECT id, shortname FROM divisions");
+
+if (!empty($dl))
+	foreach ($dl as $division)
+		$DB->Execute("UPDATE documents SET div_shortname = ?
+				WHERE divisionid = ?", array(
+				($division['shortname'] ? $division['shortname'] : ''),
+				$division['id']));
+
+$DB->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2014040100', 'dbversion'));
+$DB->CommitTrans();
+
+?>
