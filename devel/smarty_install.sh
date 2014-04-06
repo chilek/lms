c1bcfc9cbfed90563b9720780529f85f06a134d0
knapdruk <knapdruk@knapdruk.nazwa.pl>
2014-04-06 10:47:49 +0200

    fixed script for latest smarty download

--- devel/smarty_install.sh
+++ devel/smarty_install.sh
@@ -3,7 +3,7 @@
 # Smarty templates library quick installation (with sources download)
 #
 
-SMARTYVER="stable"
+URL="http://www.smarty.net/files/Smarty-stable.tar.gz"
 
 set -e
 
@@ -12,26 +12,26 @@ TMP=`mktemp -d`
 
 # download
 echo -n "Downloading Smarty sources... "
-wget -q -O $TMP/Smarty-$SMARTYVER.tar.gz  http://www.smarty.net/files/Smarty-$SMARTYVER.tar.gz
+wget -q -O $TMP/Smarty.tar.gz "$URL"
 echo "done."
 
 # extracting package
 echo -n "Extracting... "
-tar -C $TMP -xzf $TMP/Smarty-$SMARTYVER.tar.gz
+tar -C $TMP -xzf $TMP/Smarty.tar.gz
 echo "done."
 
 # merging
 echo -n "Merging... "
-cp -r $TMP/Smarty-$SMARTYVER/libs/*         $LIB_DIR/Smarty/
-cp -r $TMP/Smarty-$SMARTYVER/libs/plugins/* $LIB_DIR/Smarty/plugins/
+VER=`find $TMP -maxdepth 1 -mindepth 1 -type d |perl -p -i -e 's/^.*(\d\.\d\.\d+)$/$1/g'`
+cp -r $TMP/Smarty-$VER/libs/*         $LIB_DIR/Smarty/
+cp -r $TMP/Smarty-$VER/libs/plugins/* $LIB_DIR/Smarty/plugins/
 echo "done."
 
 
 # cleanup
 echo -n "Cleaning up... " 
-rm -Rf $TMP/Smarty-$SMARTYVER $TMP/Smarty-$SMARTYVER.tar.gz
+rm -Rf $TMP/Smarty-$VER $TMP/Smarty.tar.gz
 rmdir $TMP
 echo "done."
 
 exit
-
