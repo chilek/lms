3bfb2ee7d7c2b6f71610807cd8bd20ea51f96cf1
Krzysztof Labus <krzysztof.labus+dev.github@limer.org>
2014-04-07 20:15:24 +0200

    Update eventedit.php
    Return to last seen form after event close action by @maciejlew

--- modules/eventedit.php
+++ modules/eventedit.php
@@ -27,12 +27,12 @@
 if(isset($_GET['action']) && $_GET['action'] == 'open')
 {
 	$DB->Execute('UPDATE events SET closed = 0 WHERE id = ?',array($_GET['id']));
-	$SESSION->redirect('?m=eventlist');
+	$SESSION->redirect('?'.$SESSION->get('backto'));
 }
 elseif(isset($_GET['action']) && $_GET['action'] == 'close')
 {
 	$DB->Execute('UPDATE events SET closed = 1 WHERE id = ?',array($_GET['id']));
-	$SESSION->redirect('?m=eventlist');
+	$SESSION->redirect('?'.$SESSION->get('backto'));
 }
 elseif(isset($_GET['action']) && $_GET['action'] == 'dropuser')
 {
