a0322a5693743bba12b325064848f88a843b5499
Maciej Lew <maciej.lew.1987@gmail.com>
2014-05-14 21:49:18 +0200

    LMSDB_driver_mysqli class new constructor, properties and methods visibility

--- lib/LMSDB_driver_mysqli.class.php
+++ lib/LMSDB_driver_mysqli.class.php
@@ -30,10 +30,10 @@
 
 class LMSDB_driver_mysqli extends LMSDB_common
 {
-	var $_loaded = TRUE;
-	var $_dbtype = 'mysqli';
+	public $_loaded = TRUE;
+	public $_dbtype = 'mysqli';
 
-	function LMSDB_driver_mysqli($dbhost, $dbuser, $dbpasswd, $dbname)
+	public function __construct($dbhost, $dbuser, $dbpasswd, $dbname)
 	{
 		if(!extension_loaded('mysqli'))
 		{
@@ -47,12 +47,12 @@ class LMSDB_driver_mysqli extends LMSDB_common
 		$this->Connect($dbhost, $dbuser, $dbpasswd, $dbname);
 	}
 
-	function _driver_dbversion()
+	public function _driver_dbversion()
 	{
 		return @mysqli_get_server_info($this->_dblink);
 	}
 
-	function _driver_connect($dbhost, $dbuser, $dbpasswd, $dbname)
+	public function _driver_connect($dbhost, $dbuser, $dbpasswd, $dbname)
 	{
 		if($this->_dblink = @mysqli_connect($dbhost,$dbuser,$dbpasswd,$dbname))
 		{
@@ -67,13 +67,13 @@ class LMSDB_driver_mysqli extends LMSDB_common
 		return $this->_dblink;
 	}
 
-	function _driver_shutdown()
+	public function _driver_shutdown()
 	{
 		$this->_loaded = FALSE;
 		@mysqli_close($this->_dblink); // apparently, mysqli_close() is automagicly called after end of the script...
 	}
 
-	function _driver_geterror()
+	public function _driver_geterror()
 	{
 		if($this->_dblink)
 			return mysqli_error($this->_dblink);
@@ -83,12 +83,12 @@ class LMSDB_driver_mysqli extends LMSDB_common
 			return mysqli_connect_error();
 	}
 
-	function _driver_disconnect()
+	public function _driver_disconnect()
 	{
 		return @mysqli_close($this->_dblink);
 	}
 	
-	function _driver_execute($query)
+	public function _driver_execute($query)
 	{
 		$this->_query = $query;
 
@@ -99,7 +99,7 @@ class LMSDB_driver_mysqli extends LMSDB_common
 		return $this->_result;
 	}
 
-        function _driver_multi_execute($query)
+        public function _driver_multi_execute($query)
         {
                 $this->_query = $query;
                 $total_result = FALSE;
@@ -119,7 +119,7 @@ class LMSDB_driver_mysqli extends LMSDB_common
                 return $total_result;
         }
 
-	function _driver_fetchrow_assoc($result = NULL)
+	public function _driver_fetchrow_assoc($result = NULL)
 	{
 		if(! $this->_error)
 			return mysqli_fetch_array($result ? $result : $this->_result, MYSQLI_ASSOC);
@@ -127,7 +127,7 @@ class LMSDB_driver_mysqli extends LMSDB_common
 			return FALSE;
 	}
 
-	function _driver_fetchrow_num()
+	public function _driver_fetchrow_num()
 	{
 		if(! $this->_error)
 			return mysqli_fetch_array($this->_result, MYSQLI_NUM);
@@ -135,7 +135,7 @@ class LMSDB_driver_mysqli extends LMSDB_common
 			return FALSE;
 	}
 
-	function _driver_affected_rows()
+	public function _driver_affected_rows()
 	{
 		if(! $this->_error)
 			return mysqli_affected_rows($this->_dblink);
@@ -143,7 +143,7 @@ class LMSDB_driver_mysqli extends LMSDB_common
 			return FALSE;
 	}
 
-	function _driver_num_rows()
+	public function _driver_num_rows()
 	{
 		if(! $this->_error)
 			return mysqli_num_rows($this->_result);
@@ -151,45 +151,45 @@ class LMSDB_driver_mysqli extends LMSDB_common
 			return FALSE;
 	}
 	
-	function _driver_now()
+	public function _driver_now()
 	{
 		return 'UNIX_TIMESTAMP()';
 	}
 
-	function _driver_like()
+	public function _driver_like()
 	{
 		return 'LIKE';
 	}
 
-	function _driver_concat($input)
+	public function _driver_concat($input)
 	{
 		$return = implode(', ', $input);
 		return 'CONCAT('.$return.')';
 	}
 
-	function _driver_listtables()
+	public function _driver_listtables()
 	{
 		return $this->GetCol('SELECT table_name FROM information_schema.tables 
 				WHERE table_type = ? AND table_schema = ?',
 				array('BASE TABLE', $this->_dbname));
 	}
 
-	function _driver_begintrans()
+	public function _driver_begintrans()
 	{
 		return $this->Execute('BEGIN');
 	}
 
-	function _driver_committrans()
+	public function _driver_committrans()
 	{
 		return $this->Execute('COMMIT');
 	}
 
-	function _driver_rollbacktrans()
+	public function _driver_rollbacktrans()
         {
 	        return $this->Execute('ROLLBACK');
 	}
 
-	function _driver_locktables($table, $locktype=null)
+	public function _driver_locktables($table, $locktype=null)
 	{
 		$locktype = $locktype ? strtoupper($locktype) : 'WRITE';
 
@@ -199,17 +199,17 @@ class LMSDB_driver_mysqli extends LMSDB_common
 			$this->Execute('LOCK TABLES '.$table.' '.$locktype);
 	}
 
-	function _driver_unlocktables()
+	public function _driver_unlocktables()
 	{
 		$this->Execute('UNLOCK TABLES');
 	}
 
-	function _driver_lastinsertid($table = NULL)
+	public function _driver_lastinsertid($table = NULL)
         {
 	        return $this->GetOne('SELECT LAST_INSERT_ID()');
 	}
 
-	function _driver_groupconcat($field, $separator = ',')
+	public function _driver_groupconcat($field, $separator = ',')
 	{
 		return 'GROUP_CONCAT('.$field.' SEPARATOR \''.$separator.'\')';
 	}
