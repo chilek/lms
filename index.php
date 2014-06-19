0b6106ae522d2ae3a3b2b8cee37167deef9c05c0
Maciej Lew <maciej.lew.1987@gmail.com>
2014-06-19 15:22:25 +0200

    Hides database initialization to LMSDB class

--- index.php
+++ index.php
@@ -64,18 +64,11 @@ require_once(LIB_DIR.'/config.php');
 
 // Init database
 
-$_DBTYPE = $CONFIG['database']['type'];
-$_DBHOST = $CONFIG['database']['host'];
-$_DBUSER = $CONFIG['database']['user'];
-$_DBPASS = $CONFIG['database']['password'];
-$_DBNAME = $CONFIG['database']['database'];
-$_DBDEBUG = (isset($CONFIG['database']['debug']) ? chkconfig($CONFIG['database']['debug']) : FALSE);
-
 $DB = null;
 
 try {
 
-    $DB = LMSDB::getDB($_DBTYPE, $_DBHOST, $_DBUSER, $_DBPASS, $_DBNAME, $_DBDEBUG);
+    $DB = LMSDB::getInstance();
 
 } catch (Exception $ex) {
     
