ea293bea3057d75c445f9c9f51aca055dba92f9e
Maciej Lew <maciej.lew.1987@gmail.com>
2014-06-19 15:08:15 +0200

    LMSConfig library for managing LMS config variables

---
+++ lib/LMSConfig/IniConfigProvider.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ *  LMS version 1.11-git
+ *
+ *  Copyright (C) 2001-2013 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+/**
+ * IniConfigProvider
+ *
+ * @author Maciej Lew <maciej.lew.1987@gmail.com>
+ */
+class IniConfigProvider implements ConfigProviderInterface
+{
+    const NAME = 'INI_CONFIG_PROVIDER';
+    
+    /**
+     * Loads ini file and returns it as raw config
+     * 
+     * Avaliable options are:
+     * ini_file_path - path to ini file
+     * 
+     * By default searches for ini file in defined CONFIG_FILE path.
+     * 
+     * @param array $options Associative array of options
+     * @return array Raw config
+     * @throws Exception Throws exception when unable to read ini file
+     */
+    public function load(array $options = array())
+    {
+        $ini_file_path = CONFIG_FILE;
+        
+        if (isset($options['ini_file_path'])) {
+            $ini_file_path = $options['ini_file_path'];
+        }
+        
+        if (!is_readable($ini_file_path)) {
+            throw new Exception('Unable to read ini file!');
+        }
+        
+        return parse_ini_file($ini_file_path, true);
+    }
+}
---
+++ lib/LMSConfig/UiConfigProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ *  LMS version 1.11-git
+ *
+ *  Copyright (C) 2001-2013 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+/**
+ * UiConfigProvider
+ *
+ * @author Maciej Lew <maciej.lew.1987@gmail.com>
+ */
+class UiConfigProvider implements ConfigProviderInterface
+{
+    const NAME = 'UI_CONFIG_PROVIDER';
+    
+    /**
+     * Return uiconfig database table
+     * 
+     * @param array $options Associative array of options
+     * @return array
+     */
+    public function load(array $options = array())
+    {
+        $db = LMSDB::getInstance();
+        return $db->GetAll('SELECT section, var, value, description FROM uiconfig WHERE disabled = 0');
+    }
+}
