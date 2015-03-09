93c7aac6d2f62e90d2908ecacc151255dd403d86
Krzysztof Kulesza <prezes@cmza.pl>
2015-03-09 08:34:55 +0100

    Fix accesstable.php include custom_accesstable.php

--- lib/accesstable.php
+++ lib/accesstable.php
@@ -139,5 +139,7 @@ $access['table'][255]['deny_reg']	= '^.*$';
 // read user-defined access rights table
 $custom_access_table = ConfigHelper::getConfig('phpui.custom_accesstable');
 if (!is_null($custom_access_table))
-	if (is_readable(LIB_DIR . '/' . $custom_access_table))
+	if (is_readable($custom_access_table) && ($custom_access_table{0} == '/'))
+		@include_once($custom_access_table);
+	else if (is_readable(LIB_DIR . '/' . $custom_access_table))
 		@include_once(LIB_DIR . '/' . $custom_access_table);
