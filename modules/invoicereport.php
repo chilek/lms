a67c4114f91304cae5d6039cc47de867a6512cb8
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2015-03-17 16:25:17 +0100

    Revert "Update invoicereport.php"
    This reverts commit 2f3aa5d5fc553118ec935b622077c66f2a569cc2.
    
    sprintf floating point number formatting doesn't round values properly

--- modules/invoicereport.php
+++ modules/invoicereport.php
@@ -180,7 +180,7 @@ if($items)
 			set_taxes($item['taxid']);
 
 			$refitemsum = $item['value'] * $item['count'];
-			$refitemval = $refitemsum / ($taxes[$item['taxid']]['value']+100) * 100;
+			$refitemval = round($refitemsum / ($taxes[$item['taxid']]['value']+100) * 100, 2);
 			$refitemtax = $refitemsum - $refitemval;
 
 			$invoicelist[$idx][$item['taxid']]['tax'] -= $refitemtax;
@@ -195,7 +195,7 @@ if($items)
 		}
 
 		$sum = $row['value'] * $row['count'];
-		$val = $sum / ($taxes[$taxid]['value']+100) * 100;
+		$val = round($sum / ($taxes[$taxid]['value']+100) * 100, 2);
 		$tax = $sum - $val;
 
 		$invoicelist[$idx][$taxid]['tax'] += $tax;
