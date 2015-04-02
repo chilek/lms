fd6936d8cc73e74a1cfb3eca5b89e46ff3949da1
Maciej Lew <maciej.lew.1987@gmail.com>
2015-04-02 10:43:04 +0200

    Revert constant PLUGINS_DIR
    Some plugins may use old name...

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ define('BACKUP_DIR', $CONFIG['directories']['backup_dir']);
 define('MODULES_DIR', $CONFIG['directories']['modules_dir']);
 define('SMARTY_COMPILE_DIR', $CONFIG['directories']['smarty_compile_dir']);
 define('SMARTY_TEMPLATES_DIR', $CONFIG['directories']['smarty_templates_dir']);
-define('PLUGIN_DIR', $CONFIG['directories']['plugin_dir']);
+define('PLUGINS_DIR', $CONFIG['directories']['plugin_dir']);
 define('VENDOR_DIR', $CONFIG['directories']['vendor_dir']);
 
 // Load autloader
