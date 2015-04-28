207a11d89de79141670858797b58c23499985f9b
maniac <rafal.ramocki@gmail.com>
2015-04-28 02:12:05 +0200

    don't process whole module if we're just hadling ajax request

--- modules/netdevinfo.php
+++ modules/netdevinfo.php
@@ -28,72 +28,73 @@ if (!$LMS->NetDevExists($_GET['id'])) {
 	$SESSION->redirect('?m=netdevlist');
 }
 
-$netdevinfo = $LMS->GetNetDev($_GET['id']);
-$netdevconnected = $LMS->GetNetDevConnectedNames($_GET['id']);
-$netcomplist = $LMS->GetNetdevLinkedNodes($_GET['id']);
-$netdevlist = $LMS->GetNotConnectedDevices($_GET['id']);
-
-$nodelist = $LMS->GetUnlinkedNodes();
-$netdevips = $LMS->GetNetDevIPs($_GET['id']);
-
-$SESSION->save('backto', $_SERVER['QUERY_STRING']);
-
-$layout['pagetitle'] = trans('Device Info: $a $b $c', $netdevinfo['name'], $netdevinfo['producer'], $netdevinfo['model']);
-
-$netdevinfo['id'] = $_GET['id'];
-
-if ($netdevinfo['netnodeid']) {
-	$netnode = $DB->GetRow("SELECT * FROM netnodes WHERE id=".$netdevinfo['netnodeid']);
-	if ($netnode) {
-		$netdevinfo['nodename'] = $netnode['name'];
-	}
-}
-
-$netdevinfo['projectname'] = trans('none');
-if ($netdevinfo['invprojectid']) {
-	$prj = $DB->GetRow("SELECT * FROM invprojects WHERE id = ?", array($netdevinfo['invprojectid']));
-	if ($prj) {
-		if ($prj['type'] == INV_PROJECT_SYSTEM && intval($prj['id'])==1) {
-			/* inherited */
-			if ($netnode) {
-				$prj = $DB->GetRow("SELECT * FROM invprojects WHERE id=?",
-					array($netnode['invprojectid']));
-				if ($prj)
-					$netdevinfo['projectname'] = trans('$a (from network node $b)', $prj['name'], $netnode['name']);
-			}
-		} else
-			$netdevinfo['projectname'] = $prj['name'];
-	}
-} 
-
-$SMARTY->assign('netdevinfo', $netdevinfo);
-$SMARTY->assign('objectid', $netdevinfo['id']);
-$SMARTY->assign('netdevlist', $netdevconnected);
-$SMARTY->assign('netcomplist', $netcomplist);
-$SMARTY->assign('restnetdevlist', $netdevlist);
-$SMARTY->assign('netdevips', $netdevips);
-$SMARTY->assign('nodelist', $nodelist);
-$SMARTY->assign('devlinktype', $SESSION->get('devlinktype'));
-$SMARTY->assign('devlinktechnology', $SESSION->get('devlinktechnology'));
-$SMARTY->assign('devlinkspeed', $SESSION->get('devlinkspeed'));
-$SMARTY->assign('nodelinktype', $SESSION->get('nodelinktype'));
-$SMARTY->assign('nodelinktechnology', $SESSION->get('nodelinktechnology'));
-$SMARTY->assign('nodelinkspeed', $SESSION->get('nodelinkspeed'));
-
 include(MODULES_DIR . '/netdevxajax.inc.php');
 
-if (isset($_GET['ip'])) {
-	$nodeipdata = $LMS->GetNodeConnType($_GET['ip']);
-	$netdevconntype = array();
-	$conntype = $nodeipdata['conntype'];
-	if ($conntype != 0) {
-		$netdevconntype['dhcp'] = ($conntype & 2);
-		$netdevconntype['eap'] = ($conntype & 4);
+if (! array_key_exists('xjxfun', $_POST)) {                  // xajax was called and handled by netdevxajax.inc.php
+	$netdevinfo = $LMS->GetNetDev($_GET['id']);
+	$netdevconnected = $LMS->GetNetDevConnectedNames($_GET['id']);
+	$netcomplist = $LMS->GetNetdevLinkedNodes($_GET['id']);
+	$netdevlist = $LMS->GetNotConnectedDevices($_GET['id']);
+
+	$nodelist = $LMS->GetUnlinkedNodes();
+	$netdevips = $LMS->GetNetDevIPs($_GET['id']);
+
+	$SESSION->save('backto', $_SERVER['QUERY_STRING']);
+
+	$layout['pagetitle'] = trans('Device Info: $a $b $c', $netdevinfo['name'], $netdevinfo['producer'], $netdevinfo['model']);
+
+	$netdevinfo['id'] = $_GET['id'];
+
+	if ($netdevinfo['netnodeid']) {
+		$netnode = $DB->GetRow("SELECT * FROM netnodes WHERE id=".$netdevinfo['netnodeid']);
+		if ($netnode) {
+			$netdevinfo['nodename'] = $netnode['name'];
+		}
 	}
-	$SMARTY->assign('nodeipdata', $LMS->GetNode($_GET['ip']));
-	$SMARTY->assign('netdevconntype', $netdevconntype);
-	$SMARTY->display('netdev/netdevipinfo.html');
-} else {
-	$SMARTY->display('netdev/netdevinfo.html');
+
+	$netdevinfo['projectname'] = trans('none');
+	if ($netdevinfo['invprojectid']) {
+		$prj = $DB->GetRow("SELECT * FROM invprojects WHERE id = ?", array($netdevinfo['invprojectid']));
+		if ($prj) {
+			if ($prj['type'] == INV_PROJECT_SYSTEM && intval($prj['id'])==1) {
+				/* inherited */
+				if ($netnode) {
+					$prj = $DB->GetRow("SELECT * FROM invprojects WHERE id=?",
+						array($netnode['invprojectid']));
+					if ($prj)
+						$netdevinfo['projectname'] = trans('$a (from network node $b)', $prj['name'], $netnode['name']);
+				}
+			} else
+				$netdevinfo['projectname'] = $prj['name'];
+		}
+	}
+	$SMARTY->assign('netdevinfo', $netdevinfo);
+	$SMARTY->assign('objectid', $netdevinfo['id']);
+	$SMARTY->assign('netdevlist', $netdevconnected);
+	$SMARTY->assign('netcomplist', $netcomplist);
+	$SMARTY->assign('restnetdevlist', $netdevlist);
+	$SMARTY->assign('netdevips', $netdevips);
+	$SMARTY->assign('nodelist', $nodelist);
+	$SMARTY->assign('devlinktype', $SESSION->get('devlinktype'));
+	$SMARTY->assign('devlinktechnology', $SESSION->get('devlinktechnology'));
+	$SMARTY->assign('devlinkspeed', $SESSION->get('devlinkspeed'));
+	$SMARTY->assign('nodelinktype', $SESSION->get('nodelinktype'));
+	$SMARTY->assign('nodelinktechnology', $SESSION->get('nodelinktechnology'));
+	$SMARTY->assign('nodelinkspeed', $SESSION->get('nodelinkspeed'));
+
+	if (isset($_GET['ip'])) {
+		$nodeipdata = $LMS->GetNodeConnType($_GET['ip']);
+		$netdevconntype = array();
+		$conntype = $nodeipdata['conntype'];
+		if ($conntype != 0) {
+			$netdevconntype['dhcp'] = ($conntype & 2);
+			$netdevconntype['eap'] = ($conntype & 4);
+		}
+		$SMARTY->assign('nodeipdata', $LMS->GetNode($_GET['ip']));
+		$SMARTY->assign('netdevconntype', $netdevconntype);
+		$SMARTY->display('netdev/netdevipinfo.html');
+	} else {
+		$SMARTY->display('netdev/netdevinfo.html');
+	}
 }
 ?>
