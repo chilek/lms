867567d42dabbab17fc01672f8203c4905b95d93
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2015-08-12 13:06:30 +0200

    fixed customer email selection

--- bin/lms-sendinvoices.php
+++ bin/lms-sendinvoices.php
@@ -230,13 +230,13 @@ $ch = curl_init();
 if (!$ch)
 	die("Fatal error: Can't init curl library!" . PHP_EOL);
 
-$query = "SELECT d.id, d.number, d.cdate, d.name, d.customerid, n.template,
-			(SELECT " . $DB->GroupConcat('contact') . " FROM customercontacts
-			WHERE customerid = customers.id AND type = ?) AS email
+$query = "SELECT d.id, d.number, d.cdate, d.name, d.customerid, n.template, m.email
 		FROM documents d 
 		LEFT JOIN customers c ON c.id = d.customerid 
+		JOIN (SELECT customerid, " . $DB->GroupConcat('contact') . " AS email
+			FROM customercontacts WHERE type = ? GROUP BY customerid) m ON m.customerid = c.id
 		LEFT JOIN numberplans n ON n.id = d.numberplanid 
-		WHERE c.deleted = 0 AND d.type IN (1,3) AND c.invoicenotice = 1 AND email IS NOT NULL"
+		WHERE c.deleted = 0 AND d.type IN (1,3) AND c.invoicenotice = 1"
 			. (!empty($invoiceid) ? "AND d.id = " . $invoiceid : "AND d.cdate >= $daystart AND d.cdate <= $dayend")
 			. (!empty($groupnames) ? $customergroups : "")
 		. " ORDER BY d.number";
