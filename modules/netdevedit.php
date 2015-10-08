9a256cbcd0327193ae5fe522281f2b8574b82044
ernesttar <ernest@poczta.tarman.pl>
2015-10-08 16:14:24 +0200

    Update netdevedit.php
    Hook after update

--- modules/netdevedit.php
+++ modules/netdevedit.php
@@ -736,6 +736,7 @@ if (isset($_POST['netdev'])) {
 
 		$LMS->NetDevUpdate($netdevdata);
 		$LMS->CleanupInvprojects();
+		$hook_data=$LMS->executeHook('netdevedit_after_update',array('smarty'=>$SMARTY);
 		$SESSION->redirect('?m=netdevinfo&id=' . $_GET['id']);
 	}
 } else {
