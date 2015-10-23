1dcf3119b763b17e27875e6db99ae06243db362c
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2015-10-23 17:54:08 +0200

    fixed environment variable name which determines database type for lmsd

--- daemon/lmsd.c
+++ daemon/lmsd.c
@@ -83,7 +83,7 @@ int main(int argc, char *argv[], char **envp)
         configfile = ( getenv("LMSINI") ? getenv("LMSINI") : "/etc/lms/lms.ini" );
 
     	// read environment and command line
-	driver = ( getenv("LMSDBTYPE") ? getenv("DBTYPE") : strdup("mysql") );
+	driver = ( getenv("LMSDBTYPE") ? getenv("LMSDBTYPE") : strdup("mysql") );
 	passwd = ( getenv("LMSDBPASS") ? getenv("LMSDBPASS") : "" );
 	dbname = ( getenv("LMSDBNAME") ? getenv("LMSDBNAME") : "lms" );
 	user = ( getenv("LMSDBUSER") ? getenv("LMSDBUSER") : "lms" );
