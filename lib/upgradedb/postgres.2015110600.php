f0709d889771e02eaacbf51748bba155853fc43c
Maciej Wawryk <wawryk2@gmail.com>
2015-11-13 14:34:29 +0100

    ChangeLog and sql sript

--- lib/upgradedb/postgres.2015110600.php
+++ lib/upgradedb/postgres.2015110600.php
@@ -6,7 +6,7 @@ define('EMAIL_INVOICE', 16);
 $this->BeginTrans();
     
 $this->Execute("UPDATE customercontacts SET type = ? WHERE customerid IN (SELECT id FROM customers WHERE einvoice = 1 AND invoicenotice = 1) AND type & ? = ?",
-            array(EMAIL_INVOICE, EMAIL, EMAIL));
+            array((EMAIL + EMAIL_INVOICE), EMAIL, EMAIL));
 
 $this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2015110600', 'dbversion'));
 
