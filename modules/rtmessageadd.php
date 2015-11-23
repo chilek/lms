e813099cd648bb636812b962a4cad26d3056e02c
Maciej Hiller <maciej.hiller@net2b.pl>
2015-11-23 12:01:47 +0100

    messageadd: fix init data

--- modules/rtmessageadd.php
+++ modules/rtmessageadd.php
@@ -401,7 +401,7 @@ else
 	if($_GET['ticketid'])
 	{
 		$queue = $LMS->GetQueueByTicketId($_GET['ticketid']);
-		$message = $DB->GetRow('SELECT id AS ticketid, state, cause FROM rttickets WHERE id = ?', array($_GET['ticketid']));
+		$message = $DB->GetRow('SELECT id AS ticketid, state, cause, queueid, owner FROM rttickets WHERE id = ?', array($_GET['ticketid']));
 	}
 
 	$user = $LMS->GetUserInfo($AUTH->id);
