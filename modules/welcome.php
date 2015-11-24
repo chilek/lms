e44bd60638e5c62cb2a4d01ca959c94a0e3d7053
Łukasz Kopiszka <lukasz@alfa-system.pl>
2015-11-24 15:13:06 +0100

    fix: check if userpanel_dir is set and exists

--- modules/welcome.php
+++ modules/welcome.php
@@ -56,8 +56,10 @@ if (ConfigHelper::checkConfig('privileges.superuser') || !ConfigHelper::checkCon
 	$SMARTY->assign('customerstats', $LMS->CustomerStats());
 	$SMARTY->assign('nodestats', $LMS->NodeStats());
 
-	$customerschanges=$DB->GetOne('SELECT COUNT(id) FROM up_info_changes');
-	$SMARTY->assign('customerschanges', ( $customerschanges ? $customerschanges : 0));
+	 if (file_exists(ConfigHelper::getConfig('directories.userpanel_dir') . DIRECTORY_SEPARATOR . 'index.php')) {
+		$customerschanges=$DB->GetOne('SELECT COUNT(id) FROM up_info_changes');
+		$SMARTY->assign('customerschanges', ( $customerschanges ? $customerschanges : 0));
+	}
 }
 
 $layout['plugins'] = $plugin_manager->getAllPluginInfo();
