deefd5abfe431e686da108e9442f471c8fbd7e15
Marek Skiba <skibamarek@gmail.com>
2015-12-03 17:36:46 +0100

    fix db schema

---
+++ lib/upgradedb/mysql.2015120201.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * LMS version 1.11-git
+ *
+ *  (C) Copyright 2001-2015 LMS Developers
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ */
+
+$this->BeginTrans();
+
+$this->Execute("DROP VIEW vnodes; DROP VIEW vmacs;");
+$this->Execute("
+	CREATE VIEW vnodes AS
+		SELECT n.*, m.mac
+		FROM nodes n
+		LEFT JOIN vnodes_mac m ON (n.id = m.nodeid)
+		WHERE n.ipaddr <> 0 OR n.ipaddr_pub <> 0;
+
+	CREATE VIEW vmacs AS
+		SELECT n.*, m.mac, m.id AS macid
+		FROM nodes n
+		JOIN macs m ON (n.id = m.nodeid)
+		WHERE n.ipaddr <> 0 OR n.ipaddr_pub <> 0;
+");
+
+$this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2015120201', 'dbversion'));
+
+$this->CommitTrans();
+
+?>
