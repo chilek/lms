c76305807a4e530c50f5e151fb6ff2cdfe7920a3
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2015-12-28 00:28:10 +0100

    fixed customercontacts db schema update when landline contacts where marked as 0 type contacts

--- lib/upgradedb/mysql.2015110600.php
+++ lib/upgradedb/mysql.2015110600.php
@@ -25,9 +25,9 @@ define('EMAIL', 8);
 define('EMAIL_INVOICE', 16);
 
 $this->BeginTrans();
-    
-$this->Execute("UPDATE customercontacts SET type = ? WHERE customerid IN (SELECT id FROM customers WHERE einvoice = 1 AND invoicenotice = 1) AND type & ? = ?",
-            array((EMAIL + EMAIL_INVOICE), EMAIL, EMAIL));
+
+$this->Execute("UPDATE customercontacts SET type = ? WHERE customerid IN (SELECT id FROM customers WHERE einvoice = 1 AND invoicenotice = 1) AND (type & ?) > 0",
+            array(EMAIL | EMAIL_INVOICE, EMAIL));
 
 $this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2015110600', 'dbversion'));
 
