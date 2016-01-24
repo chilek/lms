15ad8ece894123006fd10277552f6aaa4a0bfea5
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2016-01-24 23:33:39 +0100

    show new contact fields after customer addition with reuse add form

--- modules/customeradd.php
+++ modules/customeradd.php
@@ -285,6 +285,7 @@ if (isset($_POST['customeradd']))
 
 		$reuse['status'] = $customeradd['status'];
 		$reuse['contacts'][] = array();
+		$reuse['emails'][] = array();
 		unset($customeradd);
 		$customeradd = $reuse;
 		$customeradd['reuse'] = '1';
