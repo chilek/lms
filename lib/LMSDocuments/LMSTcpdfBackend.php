4aa961a37fe70809a5aa7c41390b5711d7645eb3
Maciej Lew <maciej.lew.1987@gmail.com>
2016-02-07 12:46:28 +0100

    Fixes path to LIB_DIR

--- lib/LMSDocuments/LMSTcpdfBackend.php
+++ lib/LMSDocuments/LMSTcpdfBackend.php
@@ -31,7 +31,7 @@
  */
 
 if (!defined('LIB_DIR')) {
-    define('LIB_DIR', '../');
+    define('LIB_DIR', __DIR__ . DIRECTORY_SEPARATOR . '..');
 }
 require_once(LIB_DIR . DIRECTORY_SEPARATOR . 'tcpdf' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'lang' . DIRECTORY_SEPARATOR . 'pol.php');
 
