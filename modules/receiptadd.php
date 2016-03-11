3e4d9dcaf4496654a9bfb96ecd347afc86f3f33d
Maciej Wawryk <wawryk2@gmail.com>
2016-03-11 12:21:48 +0100

    Add hook after commit trans in case save

--- modules/receiptadd.php
+++ modules/receiptadd.php
@@ -685,7 +685,12 @@ switch($action)
 			}
 
 			$DB->CommitTrans();
-
+			$hook_data = $LMS->executeHook(
+                            'receiptadd_after_submit', 
+                            array(
+                                'customer' => $customer,
+                            )
+                        );
 			$print = TRUE;
 		}
 		elseif($contents && ($receipt['o_type'] == 'other' || $receipt['o_type'] == 'advance'))
