46ed9b22602afbb9de4897149afd79ed8ecf2598
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2016-08-26 14:21:28 +0200

    use new smarty function documentview for all document viewers which support preview/prelisten

---
+++ lib/SmartyPlugins/function.documentview.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * LMS version 1.11-git
+ *
+ *  (C) Copyright 2001-2016 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+function smarty_function_documentview($params, $template) {
+	static $vars = array('type', 'name', 'url', 'id', 'text');
+
+	$result = '';
+	foreach ($vars as $var)
+		if (isset($params[$var]))
+			$$var = $params[$var];
+		else
+			return $result;
+	$external = isset($params['external']) && $params['external'] == 'true';
+
+	if (preg_match('/image\/(jpeg|png|gif)/', $type))
+		$type = 'image';
+	elseif (preg_match('/audio\/(mp3|ogg|oga|wav)/', $type))
+		$type = 'audio';
+	elseif (preg_match('/video\/(mp4|ogg|webm)/', $type))
+		$type = 'video';
+	else
+		$type = '';
+
+	$result .= '<div class="documentviewdialog" id="documentviewdialog-' . $id . '" title="' . $name . '" style="display: none;">';
+	switch ($type) {
+		case 'image':
+			$result .= '<img src="' . $url . '" style="width: 100%;">';
+			break;
+		case 'audio':
+			$result .= '<audio src="' . $url . '" style="width: 100%;" controls preload="none">'
+				. trans("Your browser does not support the audio element.")
+				. '</audio>';
+			break;
+		case 'video':
+			$result .= '<video src="' . $url . '" style="width: 100%;" controls preload="none">'
+				. trans("Your browser does not support the video element.")
+				. '</video>';
+	}
+	$result .= '</div>';
+
+	$result .= '<A href="' . $url . '"';
+	if (empty($type))
+		$result .=  ($external ? 'rel="external"' : '');
+	else
+		$result .= ' id="documentview-' . $id . '" data-dialog-id="documentviewdialog-' . $id . '" '
+			. 'class="documentview documentview-' . $type . '"';
+	$result .= '>' . $text . '</A>';
+
+	return $result;
+}
+
+?>
