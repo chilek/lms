b1577e80d1678139f7dcf7d31ab0c93c7c541e96
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2016-08-27 02:03:34 +0200

    fixed document deletion when it hasn't got any document attachments bound

--- modules/documentdel.php
+++ modules/documentdel.php
@@ -27,15 +27,15 @@
 if ($_GET['is_sure'] == '1') {
 	if (isset($_POST['marks'])) {
 		foreach($_POST['marks'] as $id => $mark) {
-			$md5sums = $DB->GetCol('SELECT a.md5sum FROM documentattachments a
-				JOIN documents d ON (d.id = a.docid)
+			$docid = $DB->GetCol('SELECT d.id FROM documents d
 				JOIN docrights r ON (r.doctype = d.type)
-				WHERE a.docid = ? AND r.userid = ? AND (r.rights & 16) = 16',
+				WHERE d.id = ? AND r.userid = ? AND (r.rights & 16) = 16',
 				array($id, $AUTH->id));
-
-			if (!$md5sums)
+			if (!$docid)
 				continue;
 
+			$md5sums = $DB->GetCol('SELECT md5sums FROM documentattachments
+				WHERE docid = ?', array($id));
 			foreach ($md5sums as $md5sum)
 				if ($DB->GetOne('SELECT COUNT(*) FROM documentattachments WHERE md5sum = ?', array((string)$md5sum)) == 1) {
 					$filename_pdf = DOC_DIR . DIRECTORY_SEPARATOR . substr($md5sum,0,2) . DIRECTORY_SEPARATOR . $md5sum.'.pdf';
@@ -44,35 +44,25 @@ if ($_GET['is_sure'] == '1') {
 					@unlink(DOC_DIR . DIRECTORY_SEPARATOR . substr($md5sum,0,2) . DIRECTORY_SEPARATOR . $md5sum);
 				}
 
-			$DB->BeginTrans();
-
-			$DB->Execute('DELETE FROM documentcontents WHERE docid = ?',array($id));
-			$DB->Execute('DELETE FROM documents WHERE id = ?',array($id));
-
-			$DB->CommitTrans();
+			$DB->Execute('DELETE FROM documents WHERE id = ?', array($id));
 		}
 	} elseif(isset($_GET['id'])) {
-		$md5sums = $DB->GetCol('SELECT a.md5sum FROM documentattachments a
-			JOIN documents d ON (d.id = a.docid)
+		$docid = $DB->GetOne('SELECT d.id FROM documents d
 			JOIN docrights r ON (r.doctype = d.type)
-			WHERE a.docid = ? AND r.userid = ? AND (r.rights & 16) = 16',
+			WHERE d.id = ? AND r.userid = ? AND (r.rights & 16) = 16',
 			array($_GET['id'], $AUTH->id));
-
-		if (!$md5sums) {
+		if (!$docid) {
 			$SMARTY->display('noaccess.html');
 			die;
 		}
 
+		$md5sums = $DB->GetCol('SELECT md5sums FROM documentattachments
+			WHERE docid = ?', array($_GET['id']));
 		foreach ($md5sums as $md5sum)
 			if ($DB->GetOne('SELECT COUNT(*) FROM documentattachments WHERE md5sum = ?', array((string)$md5sum)) == 1)
 				@unlink(DOC_DIR.'/'.substr($md5sum,0,2).'/'.$md5sum);
 
-		$DB->BeginTrans();
-
-		$DB->Execute('DELETE FROM documentcontents WHERE docid = ?',array($_GET['id']));
-		$DB->Execute('DELETE FROM documents WHERE id = ?',array($_GET['id']));
-
-		$DB->CommitTrans();
+		$DB->Execute('DELETE FROM documents WHERE id = ?', array($_GET['id']));
 	}
 }
 
