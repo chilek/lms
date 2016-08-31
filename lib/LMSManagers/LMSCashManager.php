50d011af16fbef2c8d65b85bab3b848e7003130e
Maciej Wawryk <wawryk2@gmail.com>
2016-08-31 13:26:29 +0200

    fix spaces in value
    duże kwoty na wyciągach z mbanku przychodzą z dodatkowymi spacjami jak np 1 111,11 co powoduje pominięcie tej kwoty przy imporcie

--- lib/LMSManagers/LMSCashManager.php
+++ lib/LMSManagers/LMSCashManager.php
@@ -104,6 +104,7 @@ class LMSCashManager extends LMSManager implements LMSCashManagerInterface
 			$comment = isset($matches[$pattern['pcomment']]) ? trim($matches[$pattern['pcomment']]) : '';
 			$time = isset($matches[$pattern['pdate']]) ? trim($matches[$pattern['pdate']]) : '';
 			$value = str_replace(',','.', isset($matches[$pattern['pvalue']]) ? preg_replace('/[\s]/', '', $matches[$pattern['pvalue']]) : '');
+			$value = str_replace(' ','', $value);
 			$srcaccount = isset($matches[$pattern['srcaccount']]) ? preg_replace('/[\s]/', '', $matches[$pattern['srcaccount']]) : '';
 			$dstaccount = isset($matches[$pattern['dstaccount']]) ? preg_replace('/[\s]/', '', $matches[$pattern['dstaccount']]) : '';
 
