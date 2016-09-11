300341e0630f34fbe5b419f12e0ce7cb8cf8d4fd
Sławomir Paszkiewicz <paszczus@uni.opole.pl>
2016-09-11 14:47:41 +0200

    - merged from upstream

---
+++ daemon/modules/cutoff/cutoff.h
@@ -0,0 +1,24 @@
+
+struct cutoff_module
+{
+	MODULE base;
+
+	int warn_only;
+	int nodegroup_only;
+	int nodeassignments;
+	int customerassignments;
+	int checkinvoices;
+	int deadline;
+	int disable_suspended;
+	int connect;
+
+	char *limit;
+	char *command;
+	char *warning;
+	char *expwarning;
+
+	char *customergroups;
+	char *excluded_customergroups;
+	char *networks;
+	char *excluded_networks;
+};
