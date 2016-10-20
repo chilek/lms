38f2a6e5768bd97cd4dab72aaee4cf694feef200
Łukasz Kopiszka <lukasz@alfa-system.pl>
2016-10-21 00:16:23 +0200

    add missing and/or improve existing translations
    Conflicts:
    	userpanel/modules/info/style/bclean/templates/userinfobox.html

--- userpanel/modules/info/locale/pl/strings.php
+++ userpanel/modules/info/locale/pl/strings.php
@@ -83,5 +83,8 @@ $_LANG['Postal address:'] = 'Adres korespondencyjny:';
 
 $_LANG['Show confirmed documents only:'] = 'Pokaż wyłącznie dokumenty zatwierdzone:';
 $_LANG['Show only documents which are confirmed'] = 'Pokaż wyłącznie dokumenty, które zostały zatwierdzone';
+$_LANG['Company data'] = 'Dane firmy';
+$_LANG['Company name'] = 'Nazwa firmy';
+$_LANG['Information about the subscriber'] = 'Informacje o abonencie';
 
 ?>
