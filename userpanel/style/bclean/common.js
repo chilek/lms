c20384c84f07256b04af2902c4d8079934eb284d
Łukasz Kopiszka <lukasz@alfa-system.pl>
2016-11-09 14:04:04 +0100

    add common.js need for select all

---
+++ userpanel/style/bclean/common.js
@@ -0,0 +1,47 @@
+// $Id$
+
+function CheckAll(form, elem, excl)
+{
+    var i, len, n, e, f,
+        form = document.forms[form] ? document.forms[form] : document.getElementById(form),
+        inputs = form.getElementsByTagName('INPUT');
+
+    for (i=0, len=inputs.length; i<len; i++) {
+        e = inputs[i];
+
+        if (e.type != 'checkbox' || e == elem)
+            continue;
+
+        if (excl && excl.length) {
+            f = 0;
+            for (n=0; n<excl.length; n++)
+                if (e.name == excl[n])
+                    f = 1;
+            if (f)
+                continue;
+        }
+
+        e.checked = elem.checked;
+    }
+}
+
+function showOrHide(elementslist)
+{
+	var elements_array = elementslist.split(" ");
+	var part_num = 0;
+	while (part_num < elements_array.length)
+	{
+		var elementid = elements_array[part_num];
+		if(document.getElementById(elementid).style.display != 'none')
+		{
+			document.getElementById(elementid).style.display = 'none';
+			setCookie(elementid, '0');
+		}
+		else
+		{
+			document.getElementById(elementid).style.display = '';
+			setCookie(elementid, '1');
+		}
+		part_num += 1;
+	}
+}
