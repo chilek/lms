67ddc1e995c09bf4d761563635f1f7be84ea5681
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2016-11-28 15:27:33 +0100

    bugfix: got rid of warning where cash package is deleted while payments have not been imported yet

--- modules/cashpackagedel.php
+++ modules/cashpackagedel.php
@@ -53,14 +53,15 @@ elseif ($_GET['is_sure'] != 1) {
 				$importids[] = $import['id'];
 			}
 			$cash = $DB->GetAll('SELECT id, customerid, docid FROM cash WHERE importid IN (' . implode(',', $importids) . ')');
-			foreach ($cash as $item) {
-				$args = array(
-					SYSLOG::RES_CASH => $item['id'],
-					SYSLOG::RES_CUST => $item['customerid'],
-					SYSLOG::RES_DOC => $item['docid'],
-				);
-				$SYSLOG->AddMessage(SYSLOG::RES_CASH, SYSLOG::OPER_DELETE, $args);
-			}
+			if (!empty($cash))
+				foreach ($cash as $item) {
+					$args = array(
+						SYSLOG::RES_CASH => $item['id'],
+						SYSLOG::RES_CUST => $item['customerid'],
+						SYSLOG::RES_DOC => $item['docid'],
+					);
+					$SYSLOG->AddMessage(SYSLOG::RES_CASH, SYSLOG::OPER_DELETE, $args);
+				}
 			$userid = $DB->GetOne('SELECT userid FROM sourcefiles WHERE id = ?', array($sourcefileid));
 			$args = array(
 				SYSLOG::RES_SOURCEFILE => $sourcefileid,
