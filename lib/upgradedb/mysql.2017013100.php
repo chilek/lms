10747c839201db5a5931f1fd1fc02dfcfa90cb2f
Łukasz Kosicki <lukasz.kosicki@chilan.com>
2017-02-01 09:34:03 +0100

    bugfix: mysql upgrade db

--- lib/upgradedb/mysql.2017013100.php
+++ lib/upgradedb/mysql.2017013100.php
@@ -388,7 +388,7 @@ if ( $customers_loc ) {
         }
 
         if ( $any_to_up === true ) {
-            $this->Execute('INSERT INTO addresses (name, city, street, zip, country_id, house, flat)
+            $this->Execute('INSERT INTO addresses (name, city, street, zip, cou1ntry_id, house, flat)
                             VALUES (' . "$post_name,$post_city,$post_street,$post_zip,$post_countryid,$post_building,$post_apartment" . ');');
 
             $this->Execute('INSERT INTO customer_addresses (customer_id,address_id, type) VALUES (?,?,?);', array($v['id'], $this->GetLastInsertID('addresses'), POSTAL_ADDRESS));
@@ -461,7 +461,8 @@ $this->Execute("ALTER TABLE netdevices DROP FOREIGN KEY `netdevices_ibfk_1`;");
 $this->Execute("ALTER TABLE netdevices DROP FOREIGN KEY `netdevices_ibfk_2`;");
 $this->Execute("ALTER TABLE netnodes DROP FOREIGN KEY `netnodes_ibfk_2`;");
 $this->Execute("ALTER TABLE netnodes DROP FOREIGN KEY `netnodes_ibfk_3`;");
-$this->Execute("ALTER TABLE voipaccounts DROP FOREIGN KEY `voipaccounts_address_id_fk`;");
+$this->Execute("ALTER TABLE voipaccounts DROP FOREIGN KEY IF EXISTS `voipaccounts_ibfk_1`;");
+$this->Execute("ALTER TABLE voipaccounts DROP FOREIGN KEY IF EXISTS `voipaccounts_ibfk_2`;");
 $this->Execute("ALTER TABLE divisions DROP FOREIGN KEY `divisions_ibfk_1`;");
 $this->Execute("ALTER TABLE divisions DROP FOREIGN KEY `divisions_ibfk_2`;");
 
@@ -485,6 +486,7 @@ $this->Execute("ALTER TABLE customers  DROP IF EXISTS zip,           DROP IF EXI
 $this->Execute("ALTER TABLE nodes      DROP IF EXISTS location_city, DROP IF EXISTS location_street, DROP IF EXISTS location_house, DROP IF EXISTS location_flat, DROP IF EXISTS location;");
 $this->Execute("ALTER TABLE netnodes   DROP IF EXISTS location_city, DROP IF EXISTS location_street, DROP IF EXISTS location_house, DROP IF EXISTS location_flat, DROP IF EXISTS location;");
 $this->Execute("ALTER TABLE netdevices DROP IF EXISTS location_city, DROP IF EXISTS location_street, DROP IF EXISTS location_house, DROP IF EXISTS location_flat, DROP IF EXISTS location;");
+$this->Execute("ALTER TABLE voipaccounts DROP IF EXISTS location_city, DROP IF EXISTS location_street, DROP IF EXISTS location_house, DROP IF EXISTS location_flat, DROP IF EXISTS location;");
 
 $this->Execute("ALTER TABLE divisions  DROP IF EXISTS location_city, DROP IF EXISTS location_street, DROP IF EXISTS location_house, DROP IF EXISTS location_flat, DROP IF EXISTS countryid,
                                        DROP IF EXISTS address, DROP IF EXISTS city, DROP IF EXISTS zip;");
