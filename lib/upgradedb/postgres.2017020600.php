a845d92bc71baf20b6ff3d55544fff13efb9e27d
Łukasz Kosicki <icetea1975@hotmail.com>
2017-02-08 11:01:04 +0100

    Update postgres.2017020600.php

--- lib/upgradedb/postgres.2017020600.php
+++ lib/upgradedb/postgres.2017020600.php
@@ -21,6 +21,8 @@
  *
  */
 
+$this->BeginTrans();
+
 $this->Execute("ALTER TABLE assignments ADD COLUMN address_id int;");
 $this->Execute("ALTER TABLE assignments ADD CONSTRAINT address_id_fk FOREIGN KEY (address_id) REFERENCES addresses (id) ON DELETE SET NULL ON UPDATE CASCADE;");
 
