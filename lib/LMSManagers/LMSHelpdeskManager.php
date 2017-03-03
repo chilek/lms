0a258e6639824af1a96b71790bb8689fddd5368a
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2017-03-03 14:30:12 +0100

    bugfix: missed owner input parameter validation

--- lib/LMSManagers/LMSHelpdeskManager.php
+++ lib/LMSManagers/LMSHelpdeskManager.php
@@ -330,7 +330,7 @@ class LMSHelpdeskManager extends LMSManager implements LMSHelpdeskManagerInterfa
             $ticket['customerid'],
             $ticket['requestor'],
             $ticket['subject'],
-            $ticket['owner'],
+            isset($ticket['owner']) ? $ticket['owner'] : 0,
             isset($ticket['cause']) ? $ticket['cause'] : 0,
             isset($this->auth->id) ? $this->auth->id : 0
         ));
