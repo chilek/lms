502e8b295681bb951d9a910748e0afcaae168927
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2017-03-15 17:18:46 +0100

    bugfix: reserved sql word is quoted in double quotes

--- lib/upgradedb/mysql.2016100600.php
+++ lib/upgradedb/mysql.2016100600.php
@@ -23,7 +23,7 @@
 
 $this->BeginTrans();
 
-$this->Execute("ALTER TABLE voip_numbers CHANGE index number_index smallint;");
+$this->Execute("ALTER TABLE voip_numbers CHANGE \"index\" number_index smallint;");
 
 $this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2016100600', 'dbversion'));
 
