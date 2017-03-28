2903c7743820b6a322a2ecbbb032035d66bae302
Łukasz Kosicki <lukasz.kosicki@chilan.com>
2017-03-28 12:52:13 +0200

    added request delay for navbar serach fields

--- img/autosuggest.js
+++ img/autosuggest.js
@@ -17,6 +17,9 @@ function AutoSuggest(form,elem,uri,autosubmit, onsubmit) {
 	//A reference to the element we're binding the list to.
 	this.elem = elem;
 
+	this.request_delay = 200; // time in milliseconds
+	this.timer;               // delay handler
+
 	if (/autosuggest-(left|top|right|bottom)/i.exec(elem.className) !== null)
 		this.placement = RegExp.$1;
 	else
@@ -136,6 +139,9 @@ function AutoSuggest(form,elem,uri,autosubmit, onsubmit) {
 					me.changeHighlight(key);
 				}
 			break;
+
+			default:
+				clearTimeout(me.timer);
 		}
 	};
 
@@ -158,7 +164,7 @@ function AutoSuggest(form,elem,uri,autosubmit, onsubmit) {
 		default:
 
 			if (this.value != me.inputText && this.value.length > 0) {
-				me.HTTPpreload();
+				me.timer = setTimeout(function(){ me.HTTPpreload(); }, me.request_delay);
 			} else {
 				me.hideDiv();
 			}
