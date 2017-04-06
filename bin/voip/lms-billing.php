9f6aec03d675f3e062a9feed6290a9d200e324c1
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2017-04-06 15:41:58 +0200

    bugfix: taking billing records from standard input didn't work

--- bin/voip/lms-billing.php
+++ bin/voip/lms-billing.php
@@ -35,6 +35,7 @@ $parameters = array(
 	'd'  => 'debug',
 	'e:' => 'callee:',
 	'f:' => 'file:',
+	'i'  => 'stdin',
 	'l:' => 'calltime:',
 	'o:' => 'totaltime:',
 	'r:' => 'caller:',
@@ -76,6 +77,9 @@ lms-billing.php
 -v, --version                   print version info and exit;
 -q, --quiet                     suppress any output, except errors
 -c, --cache-dir=<cache-directory>       explicitly sets cache directory
+-f, --file=<file>               get billing records from csv file
+-i, --stdin                     get billing records from standard input
+-a, --action=<estimate|account> action which script should take
 
 EOF;
 	exit(0);
@@ -171,7 +175,7 @@ switch (strtolower($options['action'])) {
     break;
 
     case 'account':
-        if (isset($options['file'])) {
+        if (isset($options['file']) || isset($options['stdin'])) {
             $fh    = (isset($options['file'])) ? fopen($options['file'], 'r') : fopen('php://stdin', 'r');
             $error = array();
             $i     = 0;
