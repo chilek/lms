8de22d0139d3dc582494a2fbebf1aa8ad10a11b0
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2017-04-18 15:04:07 +0200

    bugfix: prevent location field name ambiguity

--- lib/LMSDB_common.class.php
+++ lib/LMSDB_common.class.php
@@ -3,7 +3,7 @@
 /*
  * LMS version 1.11-git
  *
- *  (C) Copyright 2001-2016 LMS Developers
+ *  (C) Copyright 2001-2017 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
@@ -24,7 +24,7 @@
  *  $Id$
  */
 
-define('DBVERSION', '2017041800'); // here should be always the newest version of database!
+define('DBVERSION', '2017041801'); // here should be always the newest version of database!
 				 // it placed here to avoid read disk every time when we call this file.
 
 /**
