4d036ff72708ee3b2fb4067713a0d2cd6824670d
Łukasz Kosicki <icetea1975@hotmail.com>
2017-04-20 14:16:16 +0200

    stable: undefined variable increment

--- lib/LMSManagers/LMSCashManager.php
+++ lib/LMSManagers/LMSCashManager.php
@@ -343,7 +343,9 @@ class LMSCashManager extends LMSManager implements LMSCashManagerInterface
 								WHERE d.customerid = ?
 						) x ORDER BY x.cdate',
 						array($balance['customerid'], DOC_INVOICE, $balance['customerid']))) {
-						foreach ($invoices as $inv)
+
+					    $sum = 0;
+					    foreach ($invoices as $inv)
 							$sum += $inv['value'];
 
 						$bval = $customer_manager->GetCustomerBalance($balance['customerid']);
