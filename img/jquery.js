a7993c39a6d2bef20a987a74a7d998983140432d
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2017-05-17 11:50:06 +0200

    improvement: show 'today' button in datepicker which allows to quickly select current date

--- img/jquery.js
+++ img/jquery.js
@@ -47,7 +47,28 @@ $.ajax("img/jquery-datatables-i18n/" + lmsSettings.language + ".json", {
 $(function() {
 	var autocomplete = "off";
 
+	$.datepicker._gotoToday = function(id) {
+		var target = $(id);
+		var inst = this._getInst(target[0]);
+		if (this._get(inst, 'gotoCurrent') && inst.currentDay) {
+			inst.selectedDay = inst.currentDay;
+			inst.drawMonth = inst.selectedMonth = inst.currentMonth;
+			inst.drawYear = inst.selectedYear = inst.currentYear;
+		} else {
+			var date = new Date();
+			inst.selectedDay = date.getDate();
+			inst.drawMonth = inst.selectedMonth = date.getMonth();
+			inst.drawYear = inst.selectedYear = date.getFullYear();
+			// the below two lines are new
+			this._setDateDatepicker(target, date);
+			this._selectDate(id, this._getDateDatepicker(target));
+		}
+		this._notifyChange(inst);
+		this._adjustDate(target);
+	}
+
 	$('div.calendar input,input.calendar').datepicker({
+		showButtonPanel: true,
 		dateFormat: "yy/mm/dd",
 		changeYear: true,
 		beforeShow: function(input, inst) {
