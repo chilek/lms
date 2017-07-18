aa51d8b24f0b84a26f396b1d683f6cc7efebd7dd
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2017-07-18 16:32:33 +0200

    improvement: take customer location addresses into account in customer quick search field (fixed LMS #1175)

--- modules/quicksearch.php
+++ modules/quicksearch.php
@@ -78,17 +78,20 @@ switch ($mode) {
 		if(isset($_GET['ajax'])) // support for AutoSuggest
 		{
 			$candidates = $DB->GetAll("SELECT c.id, cc.contact AS email, full_address AS address, post_name, post_full_address AS post_address, deleted,
-			    ".$DB->Concat('UPPER(lastname)',"' '",'c.name')." AS username
+			    ".$DB->Concat('UPPER(lastname)',"' '",'c.name')." AS username, va.address AS location_address
 				FROM customerview c
-				LEFT JOIN customercontacts cc ON cc.customerid = c.id AND (cc.type & " . CONTACT_EMAIL . " = " . CONTACT_EMAIL . ")    
+				LEFT JOIN customer_addresses ca ON ca.customer_id = c.id AND ca.type = ?
+				LEFT JOIN vaddresses va ON va.id = ca.address_id
+				LEFT JOIN customercontacts cc ON cc.customerid = c.id AND (cc.type & ?) > 0
 				WHERE ".(preg_match('/^[0-9]+$/', $search) ? 'c.id = '.intval($search).' OR ' : '')."
 					LOWER(".$DB->Concat('lastname',"' '",'c.name').") ?LIKE? LOWER($sql_search)
 					OR LOWER(full_address) ?LIKE? LOWER($sql_search)
 					OR LOWER(post_name) ?LIKE? LOWER($sql_search)
 					OR LOWER(post_full_address) ?LIKE? LOWER($sql_search)
+					OR LOWER(va.address) ?LIKE? LOWER($sql_search)
 					OR LOWER(cc.contact) ?LIKE? LOWER($sql_search)
 				ORDER by deleted, username, cc.contact, full_address
-				LIMIT ?", array(intval(ConfigHelper::getConfig('phpui.quicksearch_limit', 15))));
+				LIMIT ?", array(LOCATION_ADDRESS, CONTACT_EMAIL, intval(ConfigHelper::getConfig('phpui.quicksearch_limit', 15))));
 
 			$eglible=array(); $actions=array(); $descriptions=array();
 			if ($candidates)
@@ -115,6 +118,10 @@ switch ($mode) {
 				}
 				else if (preg_match("~$search~i",$row['post_address'])) {
 				    $descriptions[$row['id']] = escape_js(trans('Address:').' '.$row['post_address']);
+				    continue;
+				}
+				else if (preg_match("~$search~i",$row['location_address'])) {
+				    $descriptions[$row['id']] = escape_js(trans('Address:').' '.$row['location_address']);
 				    continue;
 				}
 				if (preg_match("~$search~i",$row['email'])) {
