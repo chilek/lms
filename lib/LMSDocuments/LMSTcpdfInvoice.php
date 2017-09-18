781cb7c399f4a627cf8f23bd1f398ad0a59c7e79
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2017-09-18 21:33:38 +0200

    bugfix: payment title on tcpdf invoice is automatically wrapped (align to left)

--- lib/LMSDocuments/LMSTcpdfInvoice.php
+++ lib/LMSDocuments/LMSTcpdfInvoice.php
@@ -557,12 +557,12 @@ class LMSTcpdfInvoice extends LMSInvoice {
 			/* title */
 			$this->backend->SetFont('arial', '', 9);
 			$this->backend->writeHTMLCell(70, 30, 131, 262,
-				trans('Payment for liabilities'), $border = 0, $ln = 0, $fill = false, $reseth = true, $align = 'C', $autopadding = true);
+				trans('Payment for liabilities'), $border = 0, $ln = 0, $fill = false, $reseth = true, $align = 'L', $autopadding = true);
 		} else {
 			/* title */
 			$this->backend->SetFont('arial', 'B', 9);
 			$this->backend->writeHTMLCell(70, 30, 131, 262,
-				trans('Payment for invoice No. $a', $barcode), $border = 0, $ln = 0, $fill = false, $reseth = true, $align = 'C', $autopadding = true);
+				trans('Payment for invoice No. $a', $barcode), $border = 0, $ln = 0, $fill = false, $reseth = true, $align = 'L', $autopadding = true);
 		}
 
 		/* deadline */
