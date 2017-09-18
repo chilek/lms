8aa187a2bd62559e466fd341d19f9616d5f206f4
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2017-09-18 17:59:12 +0200

    bugfix: user can see cash registries if he has cash_registry_administration privilege or he has any right to given cash registry

--- modules/cashreglist.php
+++ modules/cashreglist.php
@@ -21,10 +21,17 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
  *  USA.
  *
- *  $Id$
+ *  $Id: 53048ec0e5c24f4148a2f925ac72c28fc7678385 $
  */
 
 $layout['pagetitle'] = trans('Cash Registries List');
+
+$allowed = ConfigHelper::checkPrivilege('cash_registry_administration');
+
+if ($allowed)
+	$args = array();
+else
+	$args = array(Auth::GetCurrentUser());
 
 $reglist = $DB->GetAll('SELECT cashregs.id AS id, cashregs.name AS name, 
 			cashregs.description AS description, disabled,
@@ -34,8 +41,9 @@ $reglist = $DB->GetAll('SELECT cashregs.id AS id, cashregs.name AS name,
 				WHERE in_numberplanid = numberplans.id ) AS in_template,
 			(SELECT template FROM numberplans 
 				WHERE out_numberplanid = numberplans.id) AS out_template
-		FROM cashregs 
-		ORDER BY cashregs.name');
+		FROM cashregs
+		' . ($allowed ? '' : 'JOIN cashrights r ON r.regid = cashregs.id AND (r.rights & 1) > 0 AND r.userid = ?') . '
+		ORDER BY cashregs.name', $args);
 
 $listdata['sum'] = 0;
 if($reglist)
