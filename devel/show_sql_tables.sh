2e1d4ab0057a8c96433cc4c26f97b22b387ce0d5
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2017-10-12 18:56:40 +0200

    devel: added helper shell scripts which generates sql table list in different useful formats

---
+++ devel/show_sql_tables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PATH="/bin:/sbin:/usr/bin:/usr/sbin"
+
+CWD=$(dirname $(readlink -f $0))
+
+if [ $# -eq 0 -o "$1" = "-1" ]; then
+	grep "^CREATE\s\+TABLE" ${CWD}/../doc/lms.pgsql \
+		|sed -e "s/^CREATE\s\+TABLE\s\+//g" -e "s/\s\+(//g" |xargs \
+		|sed -e "s/ /', '/g" -e "s/^/'/g" -e "s/$/'/g" \
+		|fold -w 80 -s
+elif [ $# -eq 1 -a "$1" = "-2" ]; then
+	grep "^CREATE\s\+TABLE" ${CWD}/../doc/lms.pgsql \
+		|sed -e "s/^CREATE\s\+TABLE\s\+/DROP TABLE IF EXISTS /g" -e "s/\s\+(/;/g" \
+		|tac
+fi
