543dad9973dc2e8b8aaee595be993de6b707c227
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2018-01-17 19:52:19 +0100

    bugfix: missed locale

--- lib/locale/pl/strings.php
+++ lib/locale/pl/strings.php
@@ -3107,6 +3107,7 @@ $_LANG['Ticket\'s source has been changed from $a to $b.'] = 'Źródło zgłosze
 $_LANG['Ticket has been moved from queue $a to queue $b.'] = 'Zgłoszenie zostało przeniesione z kolejki $a do kolejki $b.';
 $_LANG['Ticket\'s location has been changed from $a to $b.'] = 'Lokalizacja zgłoszenia została zmieniona z $a na $b.';
 $_LANG['Ticket\'s node has been changed from $a ($b) to $c ($d).'] = 'Komputer powiązany ze zgłoszeniem został zmieniony z $a ($b) na $c ($d).';
+$_LANG['Ticket\'s priority has been changed from $a to $b.'] = 'Priorytet zgłoszenia został zmieniony z $a na $b.';
 
 $_LANG['www'] = 'www';
 $_LANG['any sms'] = 'dowolny sms';
