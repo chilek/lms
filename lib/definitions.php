8e851dccd4308387ee7a05b86de458346beb147c
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2018-01-17 18:47:52 +0100

    bugfix: don't use upper cases

--- lib/definitions.php
+++ lib/definitions.php
@@ -198,8 +198,8 @@ $RT_PRIORITIES = array(
 	RT_PRIORITY_VERYLOW => trans('very low'),
 	RT_PRIORITY_LOW => trans('low'),
 	RT_PRIORITY_NORMAL => trans('normal'),
-	RT_PRIORITY_URGENT => trans('URGENT'),
-	RT_PRIORITY_CRITICAL => trans('CRITICAL'),
+	RT_PRIORITY_URGENT => trans('urgent'),
+	RT_PRIORITY_CRITICAL => trans('critical'),
 );
 
 $RT_PRIORITY_STYLES = array(
