87595ab1c97e4784d2b6908b4cc600c7db89171d
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2018-03-07 12:43:57 +0100

    bugfix: fixed old unique key name

--- lib/upgradedb/postgres.2018022200.php
+++ lib/upgradedb/postgres.2018022200.php
@@ -38,7 +38,7 @@ $this->Execute("
 	ALTER TABLE pna RENAME COLUMN tohouse TO tonumber;
 	ALTER TABLE pna ADD COLUMN toletter varchar(8) DEFAULT NULL;
 
-	ALTER TABLE pna DROP CONSTRAINT pna_zip_cityid_streetid_fromhouse_tohouse_parity_key;
+	ALTER TABLE pna DROP CONSTRAINT pna_zip_key;
 	ALTER TABLE pna ADD CONSTRAINT pna_zip_cityid_streetid_fromnumber_tonumber_parity_key
 		UNIQUE (zip, cityid, streetid, fromnumber, fromletter, tonumber, toletter, parity);
 
