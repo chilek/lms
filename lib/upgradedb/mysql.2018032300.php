11d53f46fb5e40179998658307a8e708de5bcd17
Interduo <lmsplus@interduo.pl>
2018-03-26 16:11:11 +0200

    bugfix: #519 - forgot to git add dbschema upgrade scripts (#520)

---
+++ lib/upgradedb/mysql.2018032300.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * LMS version 1.11-git
+ *
+ *  (C) Copyright 2001-2018 LMS Developers
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ */
+
+$this->BeginTrans();
+
+$this->Execute("ALTER TABLE networks ADD COLUMN snat int(16) unsigned NOT NULL DEFAULT 0");
+$this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2018032300', 'dbversion'));
+
+$this->CommitTrans();
+
+?>
