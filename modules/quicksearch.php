dc2b8129f09c60922d408892dfb5cd8531bf7ea9
Interduo <lmsplus@interduo.pl>
2018-06-05 12:33:13 +0200

    improvement: quicksearch look polishing (#1274)

--- modules/quicksearch.php
+++ modules/quicksearch.php
@@ -118,6 +118,8 @@ switch ($mode) {
 
 					if ($customer_count[$row['customername']] > 1) {
 						$description = trans('Address:') . ' ' . $row['address'];
+						if (!empty($row['post_address']))
+							$description .= '<BR>' . trans('Postal address:') . ' '.$row['post_address'];
 					} else if (preg_match("~^$search\$~i", $row['id'])) {
 						$description = trans('Id:') . ' ' . $row['id'];
 					} else if (preg_match("~$search~i", $row['customername'])) {
