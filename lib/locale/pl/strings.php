aa3671becf9f8e1fafc3b43d991a88a09bd2a136
Interduo <lmsplus@interduo.pl>
2018-06-11 16:36:07 +0200

    bugfix: translation fix (#1283)

--- lib/locale/pl/strings.php
+++ lib/locale/pl/strings.php
@@ -3169,6 +3169,7 @@ $_LANG['backup access forbidden'] = 'brak dostępu do kopii zapasowych';
 $_LANG['Tariff type:'] = 'Typ taryfy:';
 
 $_LANG['Consent required:'] = 'Wymagana zgoda:';
+$_LANG['Check if ticket is expired'] = 'Zaznacz jeśli zgłoszenie ma przekroczony termin ostateczny';
 $_LANG['Check if customer consent to messages delivery via e-mail or sms is required'] = 'Zaznacz jeśli wymagana jest zgoda klienta na dostarczanie informacji pocztą elektr. lub sms-em';
 
 $_LANG['Select link technology'] = 'Wybierz technologię łącza';
