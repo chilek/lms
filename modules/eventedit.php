5d64dcd23ffb727230940a4f7182b07e4e40e61b
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2018-07-19 14:30:46 +0200

    bugfix: fixed selected customer restore during event edit (fixes LMS+ #577)

--- modules/eventedit.php
+++ modules/eventedit.php
@@ -107,12 +107,11 @@ if(isset($_POST['event']))
 		$event['overlapwarned'] = 1;
 	}
 
+	if (!isset($event['customerid']))
+		$event['customerid'] = $event['custid'];
+
 	if (!$error) {
 		$event['private'] = isset($event['private']) ? 1 : 0;
-		if (isset($event['customerid']))
-			$event['custid'] = $event['customerid'];
-		if ($event['custid'] == '')
-			$event['custid'] = 0;
 
 		$event['address_id'] = !isset($event['address_id']) || $event['address_id'] == -1 ? null : $event['address_id'];
 		$event['nodeid'] = !isset($event['nodeid']) || empty($event['nodeid']) ? null : $event['nodeid'];
