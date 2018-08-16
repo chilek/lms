b080d87cf4960a7a313aa57e017ef4e4bffc0787
Jarosław Kłopotek <jkl@interduo.pl>
2018-08-16 19:07:38 +0200

    fixup

--- lib/definitions.php
+++ lib/definitions.php
@@ -214,7 +214,7 @@ define('RT_PRIORITY_IDLE', -3);
 define('RT_PRIORITY_VERYLOW', -2);
 define('RT_PRIORITY_LOW', -1);
 define('RT_PRIORITY_NORMAL', 0);
-define('RT_PRIORITY_INCREASED', 1);
+define('RT_PRIORITY_HIGHER', 1);
 define('RT_PRIORITY_URGENT', 2);
 define('RT_PRIORITY_CRITICAL', 3);
 
@@ -223,7 +223,7 @@ $RT_PRIORITIES = array(
 	RT_PRIORITY_VERYLOW => trans('very low'),
 	RT_PRIORITY_LOW => trans('low'),
 	RT_PRIORITY_NORMAL => trans('normal'),
-	RT_PRIORITY_INCREASED => trans('increased'),
+	RT_PRIORITY_HIGHER => trans('higher'),
 	RT_PRIORITY_URGENT => trans('urgent'),
 	RT_PRIORITY_CRITICAL => trans('critical'),
 );
@@ -233,7 +233,7 @@ $RT_PRIORITY_STYLES = array(
 	RT_PRIORITY_VERYLOW => 'background-color: transparent; color: silver;',
 	RT_PRIORITY_LOW => 'background-color: transparent; color: grey;',
 	RT_PRIORITY_NORMAL => 'background-color: transparent; color: black;',
-	RT_PRIORITY_INCREASED => 'background-color: gold; color: black;',
+	RT_PRIORITY_HIGHER => 'background-color: gold; color: black;',
 	RT_PRIORITY_URGENT => 'background-color: darkorange; color: black;',
 	RT_PRIORITY_CRITICAL => 'background-color: red; color: white;',
 );
@@ -243,7 +243,7 @@ $RT_MAIL_PRIORITIES = array(
 	RT_PRIORITY_VERYLOW => 6,
 	RT_PRIORITY_LOW => 5,
 	RT_PRIORITY_NORMAL => 4,
-	RT_PRIORITY_INCREASED => 3,
+	RT_PRIORITY_HIGHER => 3,
 	RT_PRIORITY_URGENT => 2,
 	RT_PRIORITY_CRITICAL => 1,
 );
