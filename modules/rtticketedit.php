b2411964996013048adedd1363431341b4c4fe63
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2018-08-20 15:12:05 +0200

    fixup

--- modules/rtticketedit.php
+++ modules/rtticketedit.php
@@ -189,7 +189,7 @@ if(isset($_POST['ticket']))
 	if (!empty($dtime)) {
 		if ($dtime != $ticket['deadline']) {
             if ($ticket['verifierid'] != Auth::GetCurrentUser())
-                $error['deadline'] = trans("Ticket deadline can be changed only by verifier if it is set");
+                $error['deadline'] = trans("If verifier is set then he\'s the only person who can change deadline");
 			if ($dtime < time())
 				$error['deadline'] = trans("Ticket deadline could not be set in past");
 		}
