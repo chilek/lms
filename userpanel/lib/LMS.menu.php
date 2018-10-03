b13f6a5c8b2a82c64b61aa95b0e0c5ce4f6cfbd5
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2018-10-03 17:59:15 +0200

    forgot to add menu class here

--- userpanel/lib/LMS.menu.php
+++ userpanel/lib/LMS.menu.php
@@ -80,7 +80,7 @@ foreach($USERPANEL->MODULES as $menupos)
 // Add Userpanel menu to LMS main menu
 $menu['userpanel'] = array(
 	'name' => trans('Userpanel'),
-	'css' => 'lms-ui-userpanel-icon',
+	'css' => 'lms-ui-menu-item-icon lms-ui-userpanel-icon',
 	'link' => '?m=userpanel',
 	'tip' => trans('Userpanel'),
 	'prio' => '80',
