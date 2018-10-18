d43c43c4d12dad32864f87533f2c183d4b019bdd
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2018-10-18 15:04:14 +0200

    bugfix: fixed form param support in js code

--- lib/SmartyPlugins/function.fileupload.php
+++ lib/SmartyPlugins/function.fileupload.php
@@ -153,7 +153,7 @@ function smarty_function_fileupload($params, $template) {
 					e.stopPropagation();
 					$(this).removeClass("lms-ui-fileupload-dropzone");
 					var files = e.originalEvent.dataTransfer.files;
-					var form = ' . ($form ? '$("#" +' . $form . ')' : '$(this).closest("form");') . '
+					var form = ' . ($form ? '$("#' . $form . '")' : '$(this).closest("form");') . '
 					var formdata = new FormData(form.get(0));
 					formdata.delete("' . $id . '[]");
 					$(files).each(function(index, file) {
@@ -184,7 +184,7 @@ function smarty_function_fileupload($params, $template) {
 					value: false
 				});
 				elem.find("input[type=file]").on("change", function() {
-					var form = ' . ($form ? '$("#" +' . $form . ')' : '$(this).closest("form");') . '
+					var form = ' . ($form ? '$("#' . $form . '")' : '$(this).closest("form");') . '
 					var formdata = new FormData(form.get(0));
 					upload_files(form, formdata);
 				});
