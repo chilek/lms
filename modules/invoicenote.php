73b2b8d9ef1a55e807c00323029315ecf36d1196
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2018-11-05 17:25:41 +0100

    bugfix: invoice note support fixes

--- modules/invoicenote.php
+++ modules/invoicenote.php
@@ -269,8 +269,13 @@ switch($action)
 			} elseif ($contents[$idx]['count'] != $item['count']
 				|| $contents[$idx]['valuebrutto'] != $item['valuebrutto']) {
 				$contents[$idx]['valuebrutto'] = f_round($contents[$idx]['valuebrutto'] - $invoicecontents[$idx]['valuebrutto']);
-				$contents[$idx]['cash'] = f_round(-1 * $contents[$idx]['valuebrutto'] * $contents[$idx]['count'], 2);
 				$contents[$idx]['count'] = f_round($contents[$idx]['count'] - $invoicecontents[$idx]['count'], 3);
+				if (empty($contents[$idx]['count']))
+					$contents[$idx]['cash'] = f_round(-1 * $contents[$idx]['valuebrutto'] * $invoicecontents[$idx]['count'], 2);
+				elseif (empty($contents[$idx]['valuebrutto']))
+					$contents[$idx]['cash'] = f_round(-1 * $invoicecontents[$idx]['valuebrutto'] * $contents[$idx]['count'], 2);
+				else
+					$contents[$idx]['cash'] = f_round(-1 * $invoicecontents[$idx]['valuebrutto'] * $invoicecontents[$idx]['count'], 2);
 			} else {
 				$contents[$idx]['cash'] = 0;
 				$contents[$idx]['valuebrutto'] = 0;
