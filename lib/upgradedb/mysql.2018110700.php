52cb6dd75cad5d0bb763154985e0087cf141a393
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2018-11-07 19:56:04 +0100

    bugfix: missed phpui.networklist_pagelimit default value (should fix LMS #1427)

--- lib/upgradedb/mysql.2018110700.php
+++ lib/upgradedb/mysql.2018110700.php
@@ -23,6 +23,7 @@
 
 $this->BeginTrans();
 
+define('CONFIG_TYPE_POSITIVE_INTEGER', 2);
 $this->Execute("INSERT INTO uiconfig (section, var, value, type) VALUES (?, ?, ?, ?)",
 	array('phpui', 'networklist_pagelimit', 100, CONFIG_TYPE_POSITIVE_INTEGER));
 
