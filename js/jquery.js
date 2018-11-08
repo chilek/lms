eebed30b682e5dc652cb6d0e020b4b5357b9a4a3
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2018-11-08 09:51:53 +0100

    improvement: click on table row with ctrl key pressed open link in new window (probably resolves LMS+ #658)

--- js/jquery.js
+++ js/jquery.js
@@ -286,12 +286,16 @@ $(function() {
 
 	init_multiselects('select.lms-ui-multiselect');
 
-	$('[data-target-url] > :not(.lms-ui-buttons):not(.buttons),.lms-ui-tab-table-column[data-target-url]').click(function() {
+	$('[data-target-url] > :not(.lms-ui-buttons):not(.buttons),.lms-ui-tab-table-column[data-target-url]').click(function(e) {
 		var url = $(this).attr('data-target-url');
 		if (!url) {
 			url = $(this).parent().attr('data-target-url');
 		}
-		location.href = url;
+		if (e.ctrlKey) {
+			window.open(url);
+		} else {
+			location.href = url;
+		}
 	});
 
 	$('.lms-ui-button-clipboard').attr('title', $t('Click copies to clipboard'));
