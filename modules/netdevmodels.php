9e22010ed450476559915b19c2564088efa6e91f
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2018-11-13 16:32:21 +0100

    bugfix: fixed php warnings (LMS+ #665)

--- modules/netdevmodels.php
+++ modules/netdevmodels.php
@@ -400,7 +400,7 @@ function GetModelList($pid = NULL) {
 
 $modellist = GetModelList($pid);
 
-$listdata['total'] = count($modellist);
+$listdata['total'] = empty($modellist) ? 0 : count($modellist);
 
 $page = (!$_GET['page'] ? 1 : $_GET['page']);
 $pagelimit = ConfigHelper::getConfig('phpui.netdevmodel_pagelimit', $listdata['total']);
