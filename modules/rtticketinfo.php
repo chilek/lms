fb9d46a935e3d45e9501a4176ae686e0298e9a47
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-02-11 19:36:13 +0100

    improvement: phpui.helpdesk_additional_user_permission_checks ('false' by default) configuration variable controls if additional ticket properties are checked to verify if users has access to some more tickets - ticket tooltip support (LMS+ #323)

--- modules/rtticketinfo.php
+++ modules/rtticketinfo.php
@@ -3,7 +3,7 @@
 /*
  * LMS version 1.11-git
  *
- *  (C) Copyright 2001-2017 LMS Developers
+ *  (C) Copyright 2001-2019 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
@@ -31,14 +31,8 @@ if(! $LMS->TicketExists($id))
 	$SESSION->redirect('?m=rtqueuelist');
 }
 
-$rights = $LMS->GetUserRightsRT(Auth::GetCurrentUser(), 0, $id);
-$catrights = $LMS->GetUserRightsToCategory(Auth::GetCurrentUser(), 0, $id);
-if(!$rights || !$catrights)
-{
-	$SMARTY->display('noaccess.html');
-	$SESSION->close();
-	die;
-}
+if (!$LMS->CheckTicketAccess($id))
+	access_denied();
 
 //$SESSION->save('backto', $_SERVER['QUERY_STRING']);
 
