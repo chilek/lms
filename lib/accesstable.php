c013b9019403227e0f0ad1e4e712f29c646a48a0
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-02-21 09:44:20 +0100

    bugfix: fixed nodesessionlist module persmission if node_management privilege is set

--- lib/accesstable.php
+++ lib/accesstable.php
@@ -57,7 +57,7 @@ $access_table = array(
 	),
 	'node_management' => array(
 		'label' => trans('nodes management'),
-		'allow_regexp' => '^(node(add|info|infoshort|list|listshort|scan|search|del|edit|print|warn)|choose(mac|ip|location|gpscoords|netdevice)|ping|sessionlist)$',
+		'allow_regexp' => '^(node(add|info|infoshort|list|listshort|scan|search|del|edit|print|warn|sessionlist)|choose(mac|ip|location|gpscoords|netdevice)|ping)$',
 	),
 	'traffic_stats' => array(
 		'label' => trans('traffic stats'),
