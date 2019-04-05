be803348ed9b010c2abefee30aae804f74b0c976
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-04-05 10:53:18 +0200

    enhancement: added support for archiving documents (LMS+ #535) - remember archivizator and archivization date

---
+++ lib/upgradedb/mysql.2019040500.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * LMS version 1.11-git
+ *
+ *  (C) Copyright 2001-2019 LMS Developers
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ */
+
+$this->BeginTrans();
+
+$this->Execute("ALTER TABLE documents ADD COLUMN auserid int(11) DEFAULT NULL");
+$this->Execute("ALTER TABLE documents ADD CONSTRAINT documents_auserid_fkey
+	FOREIGN KEY (auserid) REFERENCES users (id) ON DELETE SET NULL ON UPDATE CASCADE");
+$this->Execute("ALTER TABLE documents ADD COLUMN adate int(11) DEFAULT 0 NOT NULL");
+
+$this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2019040500', 'dbversion'));
+
+$this->CommitTrans();
+
+?>
