c25b7387b050885a7fec02a913d608471cf8cc69
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-07-15 15:48:24 +0200

    bugfix: daemon/ewx: nodeid and channelid can be null, but not 0

--- daemon/modules/ewx-stm-channels/ewx-stm-channels.c
+++ daemon/modules/ewx-stm-channels/ewx-stm-channels.c
@@ -1630,7 +1630,10 @@ int del_node(GLOBAL *g, struct ewx_module *ewx, struct snmp_session *sh, struct 
    		for(vars = response->variables; vars; vars = vars->next_variable)
    			print_variable(vars->name, vars->name_length, vars);
 #endif
-		g->db->pexec(g->db->conn, "DELETE FROM ewx_stm_nodes WHERE nodeid = ?", itoa(h.id));
+		if (h.id)
+			g->db->pexec(g->db->conn, "DELETE FROM ewx_stm_nodes WHERE nodeid = ?", itoa(h.id));
+		else
+			g->db->pexec(g->db->conn, "DELETE FROM ewx_stm_nodes WHERE nodeid IS NULL");
 #ifdef DEBUG1
 		syslog(LOG_INFO, "DEBUG: [%s/ewx-stm-channels] Deleted node %s/%s (%05d)",
 		    ewx->base.instance, h.ip, h.mac, h.id);
