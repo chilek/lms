d5dadd5de40ad5df1fd5de240043624ab748186a
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-07-15 16:02:27 +0200

    bugfix: daemon/ewx: ewx_stm_channels cid field should point to customers(id)

--- lib/LMSDB_common.class.php
+++ lib/LMSDB_common.class.php
@@ -24,7 +24,7 @@
  *  $Id$
  */
 
-define('DBVERSION', '2019060300'); // here should be always the newest version of database!
+define('DBVERSION', '2019071500'); // here should be always the newest version of database!
                  // it placed here to avoid read disk every time when we call this file.
 
 /**
