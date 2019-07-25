667166a1f98a5718dbe208630fc8fb47ff2575b5
Rafał Pietraszewicz <r.pietraszewicz@gmail.com>
2019-07-25 16:22:03 +0200

    devel/cleanup: change ConfirmLink to ConfirmDialog (#1519)

--- modules/ewxchdel.php
+++ modules/ewxchdel.php
@@ -3,7 +3,7 @@
 /*
  * LMS version 1.11-git
  *
- *  (C) Copyright 2001-2013 LMS Developers
+ *  (C) Copyright 2001-2019 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
@@ -24,16 +24,5 @@
  *  $Id$
  */
 
-if (!empty($_GET['is_sure'])) {
-    $DB->Execute('DELETE FROM ewx_channels WHERE id = ?', array(intval($_GET['id'])));
-    $SESSION->redirect('?'.$SESSION->get('backto'));
-} else if ($channel = $DB->GetRow('SELECT id, name FROM ewx_channels WHERE id = ?', array(intval($_GET['id'])))) {
-    $layout['pagetitle'] = trans('Removing channel $a', strtoupper($channel['name']));
-    $SMARTY->display('header.html');
-    echo '<H1>'.$layout['pagetitle'].'</H1>';
-    echo '<P>'.trans('Are you sure, you want to delete this channel?').'</P>';
-    echo '<A href="?m=ewxchdel&id='.$channel['id'].'&is_sure=1">'.trans('Yes, I am sure.').'</A>';
-    $SMARTY->display('footer.html');
-} else {
-    $SESSION->redirect('?m=ewxchlist');
-}
+$DB->Execute('DELETE FROM ewx_channels WHERE id = ?', array(intval($_GET['id'])));
+$SESSION->redirect('?' . $SESSION->get('backto'));
