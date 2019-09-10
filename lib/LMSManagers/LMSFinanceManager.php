d606ebe13b2fdbe5dc3e0c42fdc93e26aaf73e0d
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-09-10 10:37:15 +0200

    bugfix: fixed end period of first settlement assignment in current month if end date for assignment as whole was not specified (promotion schema scenario)

--- lib/LMSManagers/LMSFinanceManager.php
+++ lib/LMSManagers/LMSFinanceManager.php
@@ -444,7 +444,7 @@ class LMSFinanceManager extends LMSManager implements LMSFinanceManagerInterface
 
                         list ($year, $month, $dom) = explode('/', date('Y/m/d', $data['datefrom']));
                         $nextperiod = mktime(0, 0, 0, $month + 1, 1, $year);
-                        $partial_dateto = $nextperiod > $data['dateto'] ? $data['dateto'] + 1: $nextperiod;
+                        $partial_dateto = !empty($data['dateto']) && $nextperiod > $data['dateto'] ? $data['dateto'] + 1: $nextperiod;
                         $diffdays = ($partial_dateto - $data['datefrom']) / 86400;
                         if ($diffdays > 0) {
                             list ($y, $m) = explode('/', date('Y/m', $partial_dateto - 1));
