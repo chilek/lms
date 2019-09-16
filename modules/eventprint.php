dd3650af694f5a7d184751470fefbe011a067f85
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-09-16 13:09:45 +0200

    devel/cleanup: use LMS::GetEventList() in event print - removed unused code (probably LMS+ fixes #856)

--- modules/eventprint.php
+++ modules/eventprint.php
@@ -3,7 +3,7 @@
 /*
  * LMS version 1.11-git
  *
- *  (C) Copyright 2001-2017 LMS Developers
+ *  (C) Copyright 2001-2019 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
@@ -23,90 +23,6 @@
  *
  *  $Id$
  */
-
-function GetEvents($date = null, $userid = 0, $type = 0, $customerid = 0, $privacy = 0, $closed = '')
-{
-    global $AUTH;
-
-    $DB = LMSDB::getInstance();
-
-    switch ($privacy) {
-        case 0:
-            $privacy_condition = '(private = 0 OR (private = 1 AND userid = ' . intval(Auth::GetCurrentUser()) . '))';
-            break;
-        case 1:
-            $privacy_condition = 'private = 0';
-            break;
-        case 2:
-            $privacy_condition = 'private = 1 AND userid = ' . intval(Auth::GetCurrentUser());
-            break;
-    }
-
-    $enddate = $date + 86400;
-    $list = $DB->GetAll(
-        'SELECT events.id AS id, title, note, description, date, begintime, enddate, endtime, closed, events.type, c.id AS customerid,'
-        .$DB->Concat('UPPER(c.lastname)', "' '", 'c.name'). ' AS customername, '
-        .$DB->Concat('c.city', "', '", 'c.address').' AS customerlocation, 
-		events.address_id, va.location, events.nodeid, nodes.location AS nodelocation, cc.customerphone, nn.id AS netnode_id,
-		nn.name AS netnode_name, vd.address AS netnode_location, ticketid
-		 FROM events
-		 LEFT JOIN vaddresses va ON va.id = events.address_id
-		 LEFT JOIN customerview c ON (customerid = c.id)
-		 LEFT JOIN vnodes nodes ON (events.nodeid = nodes.id)
-		 LEFT JOIN rttickets as rtt ON (rtt.id = events.ticketid)
-		 LEFT JOIN netnodes as nn ON (nn.id = rtt.netnodeid)
-		 LEFT JOIN vaddresses as vd ON (vd.id = nn.address_id)
-		 LEFT JOIN (
-			SELECT ' . $DB->GroupConcat('contact', ', ') . ' AS customerphone, customerid
-			FROM customercontacts
-			WHERE type & ? > 0 AND type & ? = 0
-			GROUP BY customerid
-		) cc ON cc.customerid = c.id
-		 WHERE ((date >= ? AND date < ?) OR (enddate <> 0 AND date < ? AND enddate >= ?)) AND ' . $privacy_condition
-         .($customerid ? ' AND events.customerid = '.intval($customerid) : '')
-        .(!empty($userid) ? ' AND EXISTS (
-			SELECT 1 FROM eventassignments
-			WHERE eventid = events.id AND userid ' . (is_array($userid) ? 'IN (' . implode(',', Utils::filterIntegers($userid)) . ')' : '=' . intval($userid)) . '
-			)' : '')
-        . (!empty($type) ? ' AND events.type ' . (is_array($type) ? 'IN (' . implode(',', Utils::filterIntegers($type)) . ')' : '=' . intval($type)) : '')
-         . ($closed != '' ? ' AND closed = ' . intval($closed) : '')
-         .' ORDER BY date, begintime',
-        array(CONTACT_MOBILE | CONTACT_FAX | CONTACT_LANDLINE, CONTACT_DISABLED,
-            $date,
-        $enddate,
-        $enddate,
-        $date)
-    );
-
-    $list2 = array();
-    if ($list) {
-        foreach ($list as $idx => $row) {
-            $row['userlist'] = $DB->GetAll(
-                'SELECT userid AS id, vusers.name
-				FROM eventassignments, vusers
-				WHERE userid = vusers.id AND eventid = ? ',
-                array($row['id'])
-            );
-
-            $endtime = $row['endtime'];
-
-            $row['wholeday'] = $endtime == 86400;
-            $row['multiday'] = false;
-
-            if ($row['enddate'] && $row['enddate'] - $row['date']) {
-                $days = round(($row['enddate'] - $row['date']) / 86400);
-                $row['multiday'] = $days > 0;
-                $row['enddate'] = $row['date'] + 86400;
-                //$row['endtime'] = 0;
-                $list2[] = $row;
-            } else {
-                $list2[] = $row;
-            }
-        }
-    }
-
-    return $list2;
-}
 
 $date = $_GET['day'];
 
