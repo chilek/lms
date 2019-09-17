874b16aac270a7e8b7e58183d71c3fb63d1a308d
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-09-17 06:46:36 +0200

    bugfix: fixed translation

--- lib/locale/pl/strings.php
+++ lib/locale/pl/strings.php
@@ -4446,4 +4446,4 @@ $_LANG['Enter current password'] = 'Wprowadź aktualne hasło';
 
 $_LANG['trust this device till $a'] = 'ufaj temu urządzeniu do $a';
 $_LANG['Trusted devices:'] = 'Zaufane urządzenia:';
-$_LANG['Are you sure you want to remove all trusted devices?'] = 'Jesteś pewien, że chcesz usunąć wszystkie zaufane urządzenia';
+$_LANG['Are you sure you want to remove all trusted devices?'] = 'Jesteś pewien, że chcesz usunąć wszystkie zaufane urządzenia?';
