90edeeae483345b7f64675707564defddd85540d
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-10-22 13:00:39 +0200

    bugfix: fixed typo in invoices.protection_password configuration variable section name

--- lib/LMSManagers/LMSFinanceManager.php
+++ lib/LMSManagers/LMSFinanceManager.php
@@ -2122,7 +2122,7 @@ class LMSFinanceManager extends LMSManager implements LMSFinanceManagerInterface
             }
 
             $result['disable_protection'] = ConfigHelper::checkConfig('invoices.disable_protection');
-            $result['protection_password'] = ConfigHelper::getConfig('invoice.protection_password');
+            $result['protection_password'] = ConfigHelper::getConfig('invoices.protection_password');
 
             return $result;
         } else {
