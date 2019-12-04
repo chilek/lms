a4e8191ecbe950c929cbfac6c4ff2179877d3643
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-12-04 11:02:29 +0100

    bugfix: fixed php warning (LMS+ #928)

--- modules/eventschedule.php
+++ modules/eventschedule.php
@@ -158,7 +158,7 @@ $userid = $filter['userid'];
 
 $userlist = $LMS->GetUserNames();
 $SMARTY->assign('userlist', $userlist);
-if (in_array('-1', $userid)) {
+if (is_array($userid) && in_array('-1', $userid)) {
     $userlist[-1]['id'] = -1;
     $userlist[-1]['name'] = trans("unassigned");
 }
