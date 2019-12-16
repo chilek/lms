4280e83e9bd0c242ce3cb4f25e27eecac78625c7
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2019-12-16 18:45:24 +0100

    bugfix: fixed balance value comparison for customer list

--- lib/LMSManagers/LMSCustomerManager.php
+++ lib/LMSManagers/LMSCustomerManager.php
@@ -826,7 +826,7 @@ class LMSCustomerManager extends LMSManager implements LMSCustomerManagerInterfa
                         case 'balance_relation':
                             if ($key == 'balance' && isset($search['balance_relation'])) {
                                 $balance_relation = intval($search['balance_relation']);
-                                $searchargs[] = 'b.value' . ($balance_relation == -1 ? '<=' : '>=') . ' ' . floatval($value);
+                                $searchargs[] = 'b.value' . ($balance_relation == -1 ? '<=' : '>=') . ' ' . str_replace(',', '.', floatval($value));
                             }
                             break;
                         case 'balance_date':
