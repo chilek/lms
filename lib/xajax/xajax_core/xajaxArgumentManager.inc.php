f3a8f98d0b504a990a804594f04efb21ed992d08
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2020-02-24 19:09:08 +0100

    bugfix: fixed PHP 7.4 get_magic_quotes_gpc() warning

--- lib/xajax/xajax_core/xajaxArgumentManager.inc.php
+++ lib/xajax/xajax_core/xajaxArgumentManager.inc.php
@@ -254,9 +254,11 @@ final class xajaxArgumentManager
             $this->nMethod = XAJAX_METHOD_GET;
             $this->aArgs = $_GET['xjxargs'];
         }
+/*
         if (1 == get_magic_quotes_gpc()) {
             array_walk($this->aArgs, array($this, '__argumentStripSlashes'));
         }
+*/
             
 
         array_walk($this->aArgs, array($this, '__argumentDecode'));
