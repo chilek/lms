f8d19f2a669f90bd260a90d6dfa9730784dbfbc2
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2020-03-09 13:04:15 +0100

    improvement: phpui.auto_remove_investment_project changed default value from 'true' to 'false'

--- lib/LMSManagers/LMSProjectManager.php
+++ lib/LMSManagers/LMSProjectManager.php
@@ -32,7 +32,7 @@ class LMSProjectManager extends LMSManager implements LMSProjectManagerInterface
 {
     public function CleanupProjects()
     {
-        if (ConfigHelper::checkValue(ConfigHelper::getConfig('phpui.auto_remove_investment_project', true))) {
+        if (ConfigHelper::checkConfig('phpui.auto_remove_investment_project')) {
             $this->db->Execute(
                 "DELETE FROM invprojects WHERE type <> ? AND id NOT IN
 				(SELECT DISTINCT invprojectid FROM netdevices WHERE invprojectid IS NOT NULL
