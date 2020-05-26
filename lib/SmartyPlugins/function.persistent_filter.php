12b0f894aafbd00bdf24cc5589d589b52cc06d54
Interduo <jkl@interduo.pl>
2020-05-26 17:19:54 +0200

    improvement: ux/ui, sort persistent filters in UI alphabetically-ascending (#1682)

--- lib/SmartyPlugins/function.persistent_filter.php
+++ lib/SmartyPlugins/function.persistent_filter.php
@@ -30,6 +30,11 @@ function smarty_function_persistent_filter($params, $template)
     $persistent_filters = $template->getTemplateVars('persistent_filters');
     $persistent_filter = $template->getTemplateVars('persistent_filter');
 
+    foreach ($persistent_filters as $key => $row) {
+        $text[$key] = $row['text'];
+    }
+    array_multisort($text, SORT_ASC, $persistent_filters);
+
     $filters = '';
     foreach ($persistent_filters as $filter) {
         $filters .= '<option value="' . $filter['value'] . '"' . ($filter['value'] == $persistent_filter ? ' selected' : '')
