eb052bda992bdcdcdd318ce5b6a03c7653613591
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2020-06-10 14:24:48 +0200

    bugfix: don't allow to send documents to customer which has not assigned any email contact with 'documents' flag (LMS+ #1346)

--- js/locale/pl_PL.js
+++ js/locale/pl_PL.js
@@ -23,6 +23,8 @@
  *  $Id$
  */
 
+$_LANG['Customer has not any assigned email contacts with "documents" flag!'] = 'Klient nie posiada przypisanych żadnych adresów poczty elektronicznej z ustawioną flagą "dokumenty"!';
+
 $_LANG['show average speed'] = 'pokaż prędkość średnią';
 $_LANG['show maximum speed'] = 'pokaż prędkość maksymalną';
 
