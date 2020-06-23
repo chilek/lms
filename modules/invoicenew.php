efbaf04143236b89db5076dc369ec042931055fc
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2020-06-23 17:01:05 +0200

    enhancement: allow to quickly clone existing invoice (LMS+ #1354)

--- modules/invoicenew.php
+++ modules/invoicenew.php
@@ -118,15 +118,17 @@ switch ($action) {
                 );
             }
 
-            $customer = $LMS->GetCustomer($invoice['customerid']);
-            $invoice['proformaid'] = $_GET['id'];
-            $invoice['proformanumber'] = docnumber(array(
-                'doctype' => DOC_INVOICE_PRO,
-                'cdate' => $invoice['cdate'],
-                'template' => $invoice['template'],
-                'customerid' => $invoice['customerid'],
-            ));
-            $invoice['preserve-proforma'] = ConfigHelper::checkConfig('phpui.default_preserve_proforma_invoice');
+            if (!isset($_GET['clone'])) {
+                $customer = $LMS->GetCustomer($invoice['customerid']);
+                $invoice['proformaid'] = $_GET['id'];
+                $invoice['proformanumber'] = docnumber(array(
+                    'doctype' => DOC_INVOICE_PRO,
+                    'cdate' => $invoice['cdate'],
+                    'template' => $invoice['template'],
+                    'customerid' => $invoice['customerid'],
+                ));
+                $invoice['preserve-proforma'] = ConfigHelper::checkConfig('phpui.default_preserve_proforma_invoice');
+            }
         } else {
             if (!empty($_GET['customerid']) && $LMS->CustomerExists($_GET['customerid'])) {
                 $customer = $LMS->GetCustomer($_GET['customerid'], true);
