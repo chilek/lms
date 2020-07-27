2398f39440d51fd3977acaacd437a8840053ce1f
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2020-07-27 17:33:53 +0200

    Revert "bugfix: fixed state support"
    This reverts commit 8203956da475c907144b6313d6e1524ccfa73a91.

--- lib/SmartyPlugins/LMSSmartyPlugins.php
+++ lib/SmartyPlugins/LMSSmartyPlugins.php
@@ -377,7 +377,7 @@ class LMSSmartyPlugins
             $tmp_state = mb_strtolower($params['location_state_name']);
 
             foreach ($states as $v) {
-                echo '<option value="' . $v['id'] . '"' . (!empty($v) && mb_strtolower($v['name']) == $tmp_state ? ' selected' : '')  . '>' . $v['name'] . '</option>';
+                echo '<option ' . (!empty($v) && mb_strtolower($v) == $tmp_state ? 'selected' : '')  . '>' . $v . '</option>';
             }
 
             unset($tmp_state);
