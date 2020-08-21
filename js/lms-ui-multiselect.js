82a361ed637e05c65b42cf16523f5e9f79347a51
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2020-08-21 18:48:46 +0200

    cleanup: removed reduntant code

--- js/lms-ui-multiselect.js
+++ js/lms-ui-multiselect.js
@@ -156,7 +156,7 @@ function multiselect(options) {
 
 	var container = $('<div class="lms-ui-multiselect-container' + (tiny ? ' tiny' : '') +
 		(bottom ? ' bottom' : '') +
-		(old_class && old_class.length ? ' ' + old_class : '') + '"/>').data('multiselect-object', this);
+		(old_class && old_class.length ? ' ' + old_class : '') + '"/>');
 	var launcher = $('<div class="lms-ui-multiselect-launcher" title="' + old_element.attr('title') + '" tabindex="0"/>')
 		.attr('style', old_element.attr('style')).appendTo(container);
 
