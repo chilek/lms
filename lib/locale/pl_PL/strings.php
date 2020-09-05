26d82fc642b91f22020da675bfa15984cb6a27e0
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2020-09-05 10:12:06 +0200

    improvement: event status changed to event avability

--- lib/locale/pl_PL/strings.php
+++ lib/locale/pl_PL/strings.php
@@ -4104,6 +4104,7 @@ $_LANG['Source:'] = 'Źródło:';
 $_LANG['Select ticket source'] = 'Wybierz źródło zgłoszenia';
 $_LANG['Set ticket priority'] = 'Wybierz priorytet zgłoszenia';
 $_LANG['Remove ticket priority'] = 'Usuń priorytet zgłoszenia';
+$_LANG['Availability:'] = 'Dostępność:';
 
 $_LANG['Create ticket'] = 'Utwórz zgłoszenie';
 $_LANG['Request Number'] = 'Nr zgłoszenia';
