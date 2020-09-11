695cc33af5045a07f05cd5eb64c5a01312faecd6
Rafał Wójcik <rafal.wojcik@gmail.com>
2020-09-11 19:29:38 +0200

    Oops

--- lib/LMSDocuments/LMSTcpdfDebitNote.php
+++ lib/LMSDocuments/LMSTcpdfDebitNote.php
@@ -3,7 +3,7 @@
 /*
  * LMS version 1.11-git
  *
- *  (C) Copyright 2001-2016 LMS Developers
+ *  (C) Copyright 2001-2020 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
