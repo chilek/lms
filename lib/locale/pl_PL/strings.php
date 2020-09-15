a32abdc3f2bce093c9c217abcceaba6f02f46e3d
Interduo <jkl@interduo.pl>
2020-09-15 11:55:46 +0200

    better translation

--- lib/locale/pl_PL/strings.php
+++ lib/locale/pl_PL/strings.php
@@ -4108,7 +4108,7 @@ $_LANG['SMS from $a'] = 'SMS od $a';
 $_LANG['Source:'] = 'Źródło:';
 $_LANG['Select ticket source'] = 'Wybierz źródło zgłoszenia';
 $_LANG['Set ticket priority'] = 'Wybierz priorytet zgłoszenia';
-$_LANG['Remove ticket priority'] = 'Usuń priorytet zgłoszenia';
+$_LANG['Reset ticket priority'] = 'Usuń priorytet zgłoszenia';
 
 $_LANG['Create ticket'] = 'Utwórz zgłoszenie';
 $_LANG['Request Number'] = 'Nr zgłoszenia';
