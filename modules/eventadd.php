79ce7554ba5e82291a3256357e17b46919daa2b2
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2020-10-02 17:48:52 +0200

    bugfix: show conflicted user names in event overlap event warning tooltip (LMS+ #1472)

--- modules/eventadd.php
+++ modules/eventadd.php
@@ -116,8 +116,9 @@ if (isset($_POST['event'])) {
             'endtime' => $endtime,
             'users' => $event['userlist'],
         )))) {
-        $users = array_map(function ($userid) use ($userlist) {
-                return $userlist[$userid]['rname'];
+        $users_by_id = Utils::array_column($userlist, 'rname', 'id');
+        $users = array_map(function ($userid) use ($users_by_id) {
+                return $users_by_id[$userid];
         }, $users);
         $error['begin'] = $error['end'] =
             trans(
