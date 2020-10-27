2a3bf3d3925a196e04a8c5c35f3fe63ef7a63bc3
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2020-10-27 13:32:38 +0100

    bugfix: box_header Smarty function

--- lib/SmartyPlugins/block.box_header.php
+++ lib/SmartyPlugins/block.box_header.php
@@ -41,8 +41,8 @@ function smarty_block_box_header($params, $content, $template, $repeat)
             return '
 		<div' . ($id ? ' id="' . $id . '"' : '') . ' class="lms-ui-box-header">
 		' . (strpos($icon, '/') !== false ? '<IMG src="' . $icon . '" alt="">'
-                    : (strpos($icon, 'lms-ui-icon-') === 0 ? '<i class="' . $icon : '<i class="lms-ui-icon-' . $icon))
-                    . (!empty($icon_class) ? ' ' . $icon_class : '') . '"></i>'
+                    : (strpos($icon, 'lms-ui-icon-') === 0 ? '<i class="' . $icon : '<i class="lms-ui-icon-' . $icon . '">'))
+                    . (!empty($icon_class) ? ' ' . $icon_class : '') . '</i>'
                   . trans($label)
                   . $content . '
 				</div>';
