fb07e5da4cd91d5350f06b8cace78edde8d7f2fa
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2020-11-10 21:12:09 +0100

    enhancement: lms-rtparser.php supports html message receive (small optimaisation)

--- lib/LMSManagers/LMSHelpdeskManager.php
+++ lib/LMSManagers/LMSHelpdeskManager.php
@@ -1270,7 +1270,7 @@ class LMSHelpdeskManager extends LMSManager implements LMSHelpdeskManagerInterfa
                         } elseif ($message['contenttype'] == 'text/html') {
                             $message['body'] = str_ireplace(
                                 '"CID:' . $attachment['cid'] . '"',
-                                '"' . $url_prefix . '/?m=rtmessageview&cid=' . $attachment['cid'] . '&tid=' . $id . '&mid=' . $message['id'] . '"',
+                                '"' . $url_prefix . '/?m=rtmessageview&api=1&cid=' . $attachment['cid'] . '&tid=' . $id . '&mid=' . $message['id'] . '"',
                                 $message['body']
                             );
                         }
@@ -1307,7 +1307,7 @@ class LMSHelpdeskManager extends LMSManager implements LMSHelpdeskManagerInterfa
                     } elseif ($message['contenttype'] == 'text/html') {
                         $message['body'] = str_ireplace(
                             '"CID:' . $attachment['cid'] . '"',
-                            '"' . $url_prefix . '/?m=rtmessageview&cid=' . $attachment['cid'] . '&tid=' . $id . '&mid=' . $message['id'] . '"',
+                            '"' . $url_prefix . '/?m=rtmessageview&api=1&cid=' . $attachment['cid'] . '&tid=' . $id . '&mid=' . $message['id'] . '"',
                             $message['body']
                         );
                     }
