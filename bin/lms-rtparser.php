b3568055044c832d4e8cb4595414da2da8068f05
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2020-11-24 14:47:38 +0100

    bugfix: fixed infinite loop

--- bin/lms-rtparser.php
+++ bin/lms-rtparser.php
@@ -623,6 +623,7 @@ while (isset($buffer) || $postid !== false) {
         if (!$queue) {
             if ($mode == MODE_IMAP) {
                 $postid = next($posts);
+                $buffer = null;
                 continue;
             }
             fprintf($stderr, "Fatal error: Queue ID not found, exiting." . PHP_EOL);
