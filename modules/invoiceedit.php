f1675bef7bfb0bc6eb2ae87b0e1515b1ecce2c4e
Rafał Pietraszewicz <r.pietraszewicz@gmail.com>
2020-12-09 15:31:43 +0100

    devel: place invoiceedit_savepos_validation hook after discounted values calculation

--- modules/invoiceedit.php
+++ modules/invoiceedit.php
@@ -214,26 +214,6 @@ switch ($action) {
                 trans('Tax category selection is required!');
         }
 
-        $hook_data = array(
-            'contents' => $contents,
-            'itemdata' => $itemdata,
-            'invoice' => $invoice,
-        );
-        $hook_data = $LMS->ExecuteHook('invoiceedit_savepos_validation', $hook_data);
-        if (isset($hook_data['error']) && is_array($hook_data['error'])) {
-            $error = array_merge($error, $hook_data['error']);
-        }
-
-        if (!empty($error)) {
-            $SMARTY->assign('itemdata', $hook_data['itemdata']);
-            if (isset($posuid)) {
-                $error['posuid'] = $posuid;
-            }
-            break;
-        }
-
-        $itemdata = $hook_data['itemdata'];
-
         foreach (array('discount', 'pdiscount', 'vdiscount', 'valuenetto', 'valuebrutto') as $key) {
             $itemdata[$key] = f_round($itemdata[$key]);
         }
@@ -262,6 +242,29 @@ switch ($action) {
             $itemdata['pdiscount'] = f_round($itemdata['pdiscount']);
             $itemdata['vdiscount'] = f_round($itemdata['vdiscount']);
             $itemdata['tax'] = $taxeslist[$itemdata['taxid']]['label'];
+        }
+
+        $hook_data = array(
+            'contents' => $contents,
+            'itemdata' => $itemdata,
+            'invoice' => $invoice,
+        );
+        $hook_data = $LMS->ExecuteHook('invoiceedit_savepos_validation', $hook_data);
+        if (isset($hook_data['error']) && is_array($hook_data['error'])) {
+            $error = array_merge($error, $hook_data['error']);
+        }
+
+        if (!empty($error)) {
+            $SMARTY->assign('itemdata', $hook_data['itemdata']);
+            if (isset($posuid)) {
+                $error['posuid'] = $posuid;
+            }
+            break;
+        }
+
+        $itemdata = $hook_data['itemdata'];
+
+        if ($itemdata['count'] > 0 && $itemdata['name'] != '') {
             if ($action == 'savepos') {
                 $contents[$posuid] = $itemdata;
             } else {
