79f87098359c4df2f738b84f497ffa8a1e96740c
Rafał Pietraszewicz <r.pietraszewicz@gmail.com>
2021-01-14 19:51:08 +0100

    improvement: allow to control showing events in the schedule and timetable for inactive or deleted users (LMS+ #1461) - user filter behaviour

--- js/lms-ui-multiselect.js
+++ js/lms-ui-multiselect.js
@@ -278,6 +278,7 @@ function multiselect(options) {
 			var exclusive = $(this).attr('data-exclusive');
 			var selected = $(this).is(':selected');
 			var disabled = $(this).is(':disabled');
+			var blended = $(this).attr('data-blended');
 			var class_name = 'visible' + (exclusive === '' ? ' exclusive' : '');
 
 			var data = '';
@@ -298,7 +299,7 @@ function multiselect(options) {
 			if (!text) {
 				text = $(this).text();
 			}
-			list += '<span>' + text.trim() + '</span>';
+			list += '<span '+ (blended === '' ? ' class="blend"' : '')+'>' + text.trim() + '</span>';
 
 			list += '</li>';
 		});
