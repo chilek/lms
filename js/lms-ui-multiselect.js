2e76bbf1409723aebd686432df75c9926c04d47e
Rafał Pietraszewicz <r.pietraszewicz@gmail.com>
2021-01-18 10:09:35 +0100

    improvement: change text crossing style in timetable and schedule

--- js/lms-ui-multiselect.js
+++ js/lms-ui-multiselect.js
@@ -299,7 +299,7 @@ function multiselect(options) {
 			if (!text) {
 				text = $(this).text();
 			}
-			list += '<span '+ (crossed === '' ? ' class="crossed"' : '')+'>' + escapeHtml(text.trim()) + '</span>';
+			list += '<span '+ (crossed === '' ? ' class="lms-ui-crossed"' : '')+'>' + escapeHtml(text.trim()) + '</span>';
 
 			list += '</li>';
 		});
