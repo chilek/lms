7be8692c16153302123a12f5e812503177fd8e86
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-02-04 13:25:22 +0100

    enhancement: number plan user assignment support (document support) (LMS+ #1681)

--- lib/LMSManagers/LMSDocumentManagerInterface.php
+++ lib/LMSManagers/LMSDocumentManagerInterface.php
@@ -3,7 +3,7 @@
 /*
  *  LMS version 1.11-git
  *
- *  Copyright (C) 2001-2020 LMS Developers
+ *  Copyright (C) 2001-2021 LMS Developers
  *
  *  Please, see the doc/AUTHORS for more information about authors!
  *
@@ -77,4 +77,18 @@ interface LMSDocumentManagerInterface
     public function getDocumentsByChecksum($checksum, $all_types = false);
 
     public function isDocumentAccessible($docid);
+
+    public function checkNumberPlanAccess($id);
+
+    public function getNumberPlan($id);
+
+    public function getNumberPlanList(array $params);
+
+    public function validateNumberPlan(array $numberplan);
+
+    public function addNumberPlan(array $numberplan);
+
+    public function updateNumberPlan(array $numberplan);
+
+    public function deleteNumberPlan($id);
 }
