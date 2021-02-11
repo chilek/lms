02c92c0f05a68899740b09696319d5c501163550
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-02-11 09:56:06 +0100

    improvement: conditional CDATA usage - only when special xml characters are used in name

--- modules/invoice.php
+++ modules/invoice.php
@@ -158,6 +158,15 @@ function try_generate_archive_invoices($ids)
     }
 }
 
+function escapeJpkText($text)
+{
+    if (mb_strlen($text) != mb_strlen(htmlspecialchars($text))) {
+        return '<![CDATA[' . $text . ']]';
+    } else {
+        return $text;
+    }
+}
+
 switch (intval($_GET['customertype'])) {
     case CTYPES_PRIVATE:
     case CTYPES_COMPANY:
@@ -545,7 +554,7 @@ if (isset($_GET['print']) && $_GET['print'] == 'cached') {
                 $jpkrow++;
                 $ten = empty($invoice['ten']) ? 'brak' : preg_replace('/[\s\-]/', '', $invoice['ten']);
                 $jpk_data .= "\t\t<NrKontrahenta>" . $ten . "</NrKontrahenta>\n";
-                $jpk_data .= "\t\t<NazwaKontrahenta><![CDATA[" . $invoice['name'] . "]]></NazwaKontrahenta>\n";
+                $jpk_data .= "\t\t<NazwaKontrahenta>" . escapeJpkText($invoice['name']) . "</NazwaKontrahenta>\n";
                 if ($jpk_vat_version == 3) {
                     $jpk_data .= "\t\t<AdresKontrahenta>" . ($invoice['postoffice'] && $invoice['postoffice'] != $invoice['city'] && $invoice['street'] ? $invoice['city'] . ', ' : '')
                         . $invoice['address'] . ', ' . (empty($invoice['zip']) ? '' : $invoice['zip'] . ' ') . ($invoice['postoffice'] ? $invoice['postoffice'] : $invoice['city']) . "</AdresKontrahenta>\n";
