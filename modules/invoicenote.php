e0074945ad91e45a167c6dd649ad6322a3749e9d
Rafał Pietraszewicz <r.pietraszewicz@gmail.com>
2021-08-25 14:02:03 +0200

    improvement: deny invoice note saving when invoice position name is emty - fix (chilek/lms-plus#1903)

--- modules/invoicenote.php
+++ modules/invoicenote.php
@@ -349,7 +349,7 @@ switch ($action) {
                 $error['discount[' . $idx . ']'] = trans('Wrong discount value!');
             }
 
-            $contents[$idx]['name'] = isset($newcontents['name'][$idx]) ? $newcontents['name'][$idx] : $item['name'];
+            $contents[$idx]['name'] = !empty($newcontents['name'][$idx]) ? $newcontents['name'][$idx] : $item['name'];
             $contents[$idx]['tariffid'] = isset($newcontents['tariffid'][$idx]) ? $newcontents['tariffid'][$idx] : $item['tariffid'];
             $contents[$idx]['servicetype'] = isset($newcontents['servicetype'][$idx]) ? $newcontents['servicetype'][$idx] : $item['servicetype'];
             $contents[$idx]['valuebrutto'] = $newcontents['valuebrutto'][$idx] != '' ? $newcontents['valuebrutto'][$idx] : $item['valuebrutto'];
