d3e6046f468ebb20cfb782f8f842fd036ceec846
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-10-06 14:57:37 +0200

    Revert "bugfix: document close procedure was not applied after document edit submit (LMS fixes #2159)"
    This reverts commit a9879cd7197d04857fc49532074100e983762ee8.

--- modules/documentedit.php
+++ modules/documentedit.php
@@ -208,11 +208,6 @@ if (isset($_POST['document'])) {
         }
 
         $allowed_archiving = ($document['docrights'] & DOCRIGHT_ARCHIVE) > 0;
-
-        if (($document['docrights'] & DOCRIGHT_CONFIRM) && !$document['closed'] && $closed > DOC_OPEN) {
-            $LMS->CommitDocuments($documentedit['id']);
-        }
-
         $DB->Execute(
             'UPDATE documents SET type=?, closed=?, sdate=?, cuserid=?, confirmdate = ?,
 			        archived = ?, adate = ?, auserid = ?, number=?, numberplanid=?, fullnumber=?
