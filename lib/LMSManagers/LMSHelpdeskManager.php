cfce48a4f3ebe46e6fd104326463b14d3c4aa831
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-10-27 14:34:43 +0200

    bugfix: use relative urls

--- lib/LMSManagers/LMSHelpdeskManager.php
+++ lib/LMSManagers/LMSHelpdeskManager.php
@@ -1334,7 +1334,7 @@ class LMSHelpdeskManager extends LMSManager implements LMSHelpdeskManagerInterfa
                         } elseif ($message['contenttype'] == 'text/html') {
                             $message['body'] = str_ireplace(
                                 '"CID:' . $attachment['cid'] . '"',
-                                '"' . $url_prefix . '/?m=rtmessageview&api=1&cid=' . $attachment['cid'] . '&tid=' . $id . '&mid=' . $message['id'] . '"',
+                                '"' . $url_prefix . '?m=rtmessageview&api=1&cid=' . $attachment['cid'] . '&tid=' . $id . '&mid=' . $message['id'] . '"',
                                 $message['body']
                             );
                         }
