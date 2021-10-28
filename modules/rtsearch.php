5eb39f1db82660a56061fe9652b55a74b024b2c0
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2021-10-28 11:03:10 +0200

    bigfix: fixed requestor_mail property usage

--- modules/rtsearch.php
+++ modules/rtsearch.php
@@ -242,9 +242,9 @@ function RTSearch($search, $order = 'createtime,desc')
     if ($result) {
         foreach ($result as &$ticket) {
             if (!$ticket['custid']) {
-                list ($ticket['requestor'], $ticket['requestoremail']) = sscanf($ticket['req'], "%[^<]<%[^>]");
+                list ($ticket['requestor'], $ticket['requestor_mail']) = sscanf($ticket['req'], "%[^<]<%[^>]");
             } else {
-                list ($ticket['requestoremail']) = sscanf($ticket['req'], "<%[^>]");
+                list ($ticket['requestor_mail']) = sscanf($ticket['req'], "<%[^>]");
             }
 
             if (!empty($ticket['deadline'])) {
