2c966fce3146d8ffb2a45bfe1ff9d5beadf170c4
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-03-17 10:27:43 +0100

    bugfix: fixed potential sql errors related to specific locales used in given installation

--- lib/upgradedb/mysql.2021072300.php
+++ lib/upgradedb/mysql.2021072300.php
@@ -24,6 +24,9 @@
 define('DOC_INVOICE', 1);
 define('DOC_CNOTE', 3);
 
+$old_locale = setlocale(LC_NUMERIC, '0');
+setlocale(LC_NUMERIC, 'C');
+
 $this->BeginTrans();
 
 $invoicecontents = $this->GetAll(
@@ -85,3 +88,5 @@ if (!empty($invoicecontents)) {
 $this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2021072300', 'dbversion'));
 
 $this->CommitTrans();
+
+setlocale(LC_NUMERIC, $old_locale);
