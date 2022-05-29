b6e3cdc1be9b431c55e886081e5a5ea60837f6b2
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2022-05-29 16:07:04 +0200

    devel: GetTicketContents() cleanup - move part of code to other private methods, some formatting fix

--- lib/locale/pl_PL/strings.php
+++ lib/locale/pl_PL/strings.php
@@ -32,6 +32,7 @@ $_LANG['Invoice value: $a'] = 'Wartość faktury: $a';
 $_LANG['Invoice value: $a (to pay)'] = 'Wartość faktury: $a (do zapłaty)';
 $_LANG['Invoice value: $a (to repay)'] = 'Wartość faktury: $a (do zwrotu)';
 
+$_LANG['Ticket ID is empty or invalid'] = 'Brak ID zgłoszenia lub jest ono nieprawidłowe';
 $_LANG['Select tariff'] = 'Wybierz taryfę';
 $_LANG['Document flags'] = 'Flagi dokumentu';
 $_LANG['The document is issued according to the $a price'] = 'Dokument wystawiony według ceny $a';
