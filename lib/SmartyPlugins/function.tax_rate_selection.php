665424504155a78dc3b29916be491615785a8bc4
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2022-06-24 12:51:54 +0200

    remove php error

--- lib/SmartyPlugins/function.tax_rate_selection.php
+++ lib/SmartyPlugins/function.tax_rate_selection.php
@@ -66,7 +66,7 @@ function smarty_function_tax_rate_selection(array $params, $template)
     }
 
     $options = '';
-    if (sizeof($taxratelist) == '0') {
+    if (empty($taxratelist)) {
         $options .= '<option selected value="">' . trans('- no tax rates defined -') . '</option>';
     } else {
         foreach ($taxratelist as $tax) {
