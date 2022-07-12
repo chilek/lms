09b28ca123bfec6f09ea3decad8c99fbddf7a4d0
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-07-12 10:01:29 +0200

    bugfix: fixed php warning

--- lib/common.php
+++ lib/common.php
@@ -1374,6 +1374,8 @@ function handle_file_uploads($elemid, &$error)
             unset($file);
             $$elemid = $fileupload[$elemid];
         }
+    } else {
+        $$elemid = array();
     }
     return compact('fileupload', 'tmppath', $elemid);
 }
