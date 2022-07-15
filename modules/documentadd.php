18802e09c447f1f496d7992bd1af3d9d388080ec
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-07-15 15:57:16 +0200

    bugfix: fixed php warnings

--- modules/documentadd.php
+++ modules/documentadd.php
@@ -383,8 +383,8 @@ if (isset($_POST['document'])) {
                     $counts = $selected_assignment['counts'][$schemaid];
                     $backwardperiods = $selected_assignment['backwardperiods'][$schemaid];
                     $copy_a = $selected_assignment;
-                    $snodes = $selected_assignment['snodes'][$schemaid];
-                    $sphones = $selected_assignment['sphones'][$schemaid];
+                    $snodes = isset($selected_assignment['snodes'][$schemaid]) ? $selected_assignment['snodes'][$schemaid] : array();
+                    $sphones = isset($selected_assignment['sphones'][$schemaid]) ? $selected_assignment['sphones'][$schemaid] : array();
 
                     foreach ($selected_assignment['sassignmentid'][$schemaid] as $label => $v) {
                         if (!$v) {
@@ -395,8 +395,8 @@ if (isset($_POST['document'])) {
                         $copy_a['modifiedvalues'] = isset($modifiedvalues[$label][$v]) ? $modifiedvalues[$label][$v] : array();
                         $copy_a['count'] = $counts[$label];
                         $copy_a['backwardperiod'] = $backwardperiods[$label][$v];
-                        $copy_a['nodes'] = $snodes[$label];
-                        $copy_a['phones'] = $sphones[$label];
+                        $copy_a['nodes'] = isset($snodes[$label]) ? $snodes[$label] : array();
+                        $copy_a['phones'] = isset($sphones[$label]) ? $sphones[$label] : array();
                         $tariffid = $LMS->AddAssignment($copy_a);
                     }
                 }
