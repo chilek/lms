9d8abff3afa230b0ee8425633db9632bcc78936c
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-07-18 17:38:20 +0200

    bugfix: fixed php warning

--- modules/eventschedule.php
+++ modules/eventschedule.php
@@ -173,7 +173,7 @@ $filter['forward'] = ConfigHelper::getConfig('timetable.default_forward_day_limi
 $eventlist = $LMS->GetEventList($filter);
 $eventlistIds = Utils::array_column($eventlist, 'id', 'id');
 
-$userid = $filter['userid'];
+$userid = isset($filter['userid']) ? $filter['userid'] : null;
 $userlistcount = empty($userid) ? 0 : count($userid);
 
 $params['short'] = 1;
