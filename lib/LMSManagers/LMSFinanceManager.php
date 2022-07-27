780545b2ec30b77ed0d582bd4359963fe2ec105e
Rafał Pietraszewicz <r.pietraszewicz@gmail.com>
2022-07-27 12:25:50 +0200

    bugfix: fix PHP warnings (#2302)

--- lib/LMSManagers/LMSFinanceManager.php
+++ lib/LMSManagers/LMSFinanceManager.php
@@ -259,14 +259,17 @@ class LMSFinanceManager extends LMSManager implements LMSFinanceManagerInterface
                     $assignments[$idx]['discounted_netvalue'] = f_round($assignments[$idx]['discounted_netprice'] * $row['count']);
                     $assignments[$idx]['unitary_netdiscount'] = f_round($row['unitary_netvalue'] - $assignments[$idx]['discounted_netprice']);
 
-                    if ($assignments[$idx]['tax_value']) {
+                    if (!empty($assignments[$idx]['tax_value'])) {
                         $assignments[$idx]['discounted_price'] = f_round($assignments[$idx]['discounted_netprice'] * ($assignments[$idx]['tax_value'] / 100 + 1));
 
                         $assignments[$idx]['tax_from_discounted_value'] = f_round($assignments[$idx]['discounted_netvalue'] * ($assignments[$idx]['tax_value'] / 100));
-                    } elseif ($assignments[$idx]['taxl_value']) {
+                    } elseif (!empty($assignments[$idx]['taxl_value'])) {
                         $assignments[$idx]['discounted_price'] = f_round($assignments[$idx]['discounted_netprice'] * ($assignments[$idx]['taxl_value'] / 100 + 1));
 
                         $assignments[$idx]['tax_from_discounted_value'] = f_round($assignments[$idx]['discounted_netvalue'] * ($assignments[$idx]['taxl_value'] / 100));
+                    } else {
+                        $assignments[$idx]['discounted_price'] = 0;
+                        $assignments[$idx]['tax_from_discounted_value'] = 0;
                     }
                     $assignments[$idx]['discounted_value'] = f_round(($assignments[$idx]['discounted_netvalue'] + $assignments[$idx]['tax_from_discounted_value']));
                     $assignments[$idx]['unitary_discount'] = f_round($row['unitary_value'] - $assignments[$idx]['discounted_price']);
@@ -278,16 +281,19 @@ class LMSFinanceManager extends LMSManager implements LMSFinanceManagerInterface
                     $assignments[$idx]['discounted_value'] = f_round($assignments[$idx]['discounted_price'] * $row['count']);
                     $assignments[$idx]['unitary_discount'] = f_round($row['unitary_value'] - $assignments[$idx]['discounted_price']);
 
-                    if ($assignments[$idx]['tax_value']) {
+                    if (!empty($assignments[$idx]['tax_value'])) {
                         $assignments[$idx]['discounted_netprice'] = f_round($assignments[$idx]['discounted_price'] / ($assignments[$idx]['tax_value'] / 100 + 1));
 
                         $assignments[$idx]['tax_from_discounted_value'] = f_round(($assignments[$idx]['discounted_value'] * $assignments[$idx]['tax_value'])
                             / (100 + $assignments[$idx]['tax_value']));
-                    } elseif ($assignments[$idx]['taxl_value']) {
+                    } elseif (!empty($assignments[$idx]['taxl_value'])) {
                         $assignments[$idx]['discounted_netprice'] = f_round($assignments[$idx]['discounted_price'] / ($assignments[$idx]['taxl_value'] / 100 + 1));
 
                         $assignments[$idx]['tax_from_discounted_value'] = f_round(($assignments[$idx]['discounted_value'] * $assignments[$idx]['taxl_value'])
                             / (100 + $assignments[$idx]['taxl_value']));
+                    } else {
+                        $assignments[$idx]['discounted_netprice'] = 0;
+                        $assignments[$idx]['tax_from_discounted_value'] = 0;
                     }
                     $assignments[$idx]['discounted_netvalue'] = f_round(($assignments[$idx]['discounted_value'] - $assignments[$idx]['tax_from_discounted_value']));
                     $assignments[$idx]['unitary_netdiscount'] = f_round($row['unitary_netvalue'] - $assignments[$idx]['discounted_netprice']);
