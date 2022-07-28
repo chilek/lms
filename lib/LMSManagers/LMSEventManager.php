8914a43a5194e2f8672ee3e13f839dbd2cef6dff
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-07-28 13:53:26 +0200

    bugfix: fixed php warning

--- lib/LMSManagers/LMSEventManager.php
+++ lib/LMSManagers/LMSEventManager.php
@@ -373,6 +373,9 @@ class LMSEventManager extends LMSManager implements LMSEventManagerInterface
                     $privacy_condition = ' AND private = 1 AND userid = ' . $current_user_id;
                 }
                 break;
+            default:
+                $privacy_condition = '';
+                break;
         }
 
         if ($forward=='-1') {
