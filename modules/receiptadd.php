118521e0e4e0f44ac7265c16b8decc83ea8c3b1b
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-08-04 16:36:32 +0200

    bugfix: fixed php warning

--- modules/receiptadd.php
+++ modules/receiptadd.php
@@ -989,7 +989,7 @@ if (!ConfigHelper::checkConfig('phpui.big_networks')) {
 
 $SMARTY->assign('invoicelist', $invoicelist);
 $SMARTY->assign('rights', $DB->GetOne('SELECT rights FROM cashrights WHERE userid=? AND regid=?', array(Auth::GetCurrentUser(), $receipt['regid'])));
-$SMARTY->assign('cashreglist', $cashreglist);
+$SMARTY->assign('cashreglist', empty($cashreglist) ? array() : $cashreglist);
 $SMARTY->assign('cashregcount', empty($cashreglist) ? 0 : count($cashreglist));
 $SMARTY->assign('contents', $contents);
 $SMARTY->assign('customer', $customer);
