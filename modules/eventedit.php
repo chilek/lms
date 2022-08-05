10a9613e0e47df3d25f8d7b153254506290d62f8
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-08-05 18:14:08 +0200

    bugfix: fixed php warning

--- modules/eventedit.php
+++ modules/eventedit.php
@@ -173,7 +173,7 @@ if (isset($_POST['event'])) {
             'begintime' => $begintime,
             'enddate' => $enddate,
             'endtime' => $endtime,
-            'users' => $event['userlist'],
+            'users' => isset($event['userlist']) ? $event['userlist'] : array(),
             'ignoredevent' => $event['id'],
         )))) {
         $users_by_id = Utils::array_column($userlist, 'rname', 'id');
