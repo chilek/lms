5a2bbdb35e635198bb82d5b4557cc4bbd515f4dc
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-08-11 19:10:33 +0200

    bugfix: fixed php fatal error/warning

--- modules/accountsearch.php
+++ modules/accountsearch.php
@@ -95,7 +95,7 @@ function GetAccountList($search, $customer = null, $type = null, $kind = null, $
         .$where
         .($sqlord != '' ? $sqlord : ''));
 
-    $list['total'] = count($list);
+    $list['total'] = empty($list) ? 0 : count($list);
     $list['order'] = $order;
     $list['type'] = $type;
     $list['kind'] = $kind;
