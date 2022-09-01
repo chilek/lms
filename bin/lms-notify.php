8553a1c27645ca3804cecb5692d8a12bc475e052
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-09-01 15:13:18 +0200

    bugfix: don't touch settlement flag of assignment record when unblock/all-assignment-suspension action is used

--- bin/lms-notify.php
+++ bin/lms-notify.php
@@ -3834,7 +3834,6 @@ if (!empty($intersect)) {
                                 $args = array(
                                     SYSLOG::RES_ASSIGN => null,
                                     SYSLOG::RES_CUST => null,
-                                    'settlement' => 1,
                                     'datefrom' => time(),
                                 );
                                 foreach ($all_customers as $cid) {
@@ -3861,7 +3860,7 @@ if (!empty($intersect)) {
                                                     }
 
                                                     if (!$debug) {
-                                                        $DB->Execute("UPDATE assignments SET settlement = 1, datefrom = ?
+                                                        $DB->Execute("UPDATE assignments SET datefrom = ?
                                                             WHERE id = ?", array($args['datefrom'], $aid));
                                                         if ($SYSLOG) {
                                                             $args[SYSLOG::RES_ASSIGN] = $aid;
