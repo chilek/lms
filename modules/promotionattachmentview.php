ce38bd6abb045931e28f90ffc5897ae5aa472fec
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-11-04 16:22:55 +0100

    enhancement: added promotion and promotionschema attachments support (promotion management part) - missed file

---
+++ modules/promotionattachmentview.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * LMS version 1.11-git
+ *
+ *  (C) Copyright 2001-2022 LMS Developers
+ *
+ *  Please, see the doc/AUTHORS for more information about authors!
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ *
+ *  $Id$
+ */
+
+$attachmentid = isset($_GET['attachmentid']) && is_numeric($_GET['attachmentid']) ? intval($_GET['attachmentid']) : null;
+if (empty($attachmentid)) {
+    die;
+}
+
+$attachment = $DB->GetRow(
+    'SELECT *
+    FROM promotionattachments
+    WHERE id = ?',
+    array($attachmentid)
+);
+if (empty($attachment)) {
+    die;
+}
+
+$file = STORAGE_DIR . DIRECTORY_SEPARATOR . 'promotions' . DIRECTORY_SEPARATOR . $attachment['promotionid']
+    . DIRECTORY_SEPARATOR . $attachment['filename'];
+if (file_exists($file)) {
+    if (isset($_GET['thumbnail']) && ($width = intval($_GET['thumbnail'])) > 0
+        && class_exists('Imagick') && strpos($attachment['contenttype'], 'image/') === 0) {
+        $imagick = new \Imagick($file);
+        $imagick->scaleImage($width, 0);
+        header('Content-Type: ' . $attachment['contenttype']);
+        header('Cache-Control: private');
+        header('Content-Disposition: ' . ($attachment['contenttype'] == 'application/pdf' ? 'inline' : 'attachment') . '; filename=' . $attachment['filename']);
+        echo $imagick->getImageBlob();
+    } else {
+        header('Content-Type: ' . $attachment['contenttype']);
+        header('Cache-Control: private');
+        header('Content-Disposition: ' . ($attachment['contenttype'] == 'application/pdf' ? 'inline' : 'attachment') . '; filename=' . $attachment['filename']);
+        echo @file_get_contents($file);
+    }
+}
