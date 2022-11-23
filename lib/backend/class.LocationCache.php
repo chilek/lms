bbdd6c192d482e2e7c507546fbe0b1e3457c65e4
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2022-11-23 12:41:17 +0100

    bugfix: fixed php warning (missed zip field get in sql query)

--- lib/backend/class.LocationCache.php
+++ lib/backend/class.LocationCache.php
@@ -262,7 +262,7 @@ class LocationCache
         if (!isset($this->buildings[ $cityid ])) {
             $tmp = $this->DB->GetAllByKey("SELECT (" . $this->DB->Concat('city_id', "'|'", "(CASE WHEN street_id IS NULL THEN 0 ELSE street_id END)", "'|'", 'UPPER(building_num)') . ")
 												AS lms_building_key,
-											longitude, latitude, id
+											longitude, latitude, zip, id
 											FROM location_buildings lb
 											WHERE city_id = ?", 'lms_building_key', array($cityid));
 
