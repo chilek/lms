4711f8748c49200179ae993d17bd58da33bbde2a
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2023-01-30 13:46:37 +0100

    bugfix: sidusis: inna technologu should be started with lower letter

--- lib/definitions.php
+++ lib/definitions.php
@@ -1075,7 +1075,7 @@ $SIDUSIS_LINKTECHNOLOGIES = array(
         118 => 'LTE-Pro',
         119 => 'NR SA',
         120 => 'NR NSA',
-        150 => 'Inna',
+        150 => 'inna',
     ),
     LINKTYPE_FIBER => array(
         250 => '(EURO)DOCSIS 1.x',
