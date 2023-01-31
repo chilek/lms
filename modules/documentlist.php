2941a89bde9d5a253df1c9816af1d8ab4bfe6441
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2023-01-31 16:57:41 +0100

    devel/cleanup: some document type definition name cleanup

--- modules/documentlist.php
+++ modules/documentlist.php
@@ -204,7 +204,7 @@ if (!ConfigHelper::checkConfig('phpui.big_networks')) {
 
 $SMARTY->assign('users', $LMS->GetUserNames());
 $SMARTY->assign('numberplans', $LMS->GetNumberPlans(array(
-    'doctype' => array(DOC_CONTRACT, DOC_ANNEX, DOC_PROTOCOL, DOC_ORDER, DOC_SHEET, -6, -7, -8, -9, -99, DOC_PRICELIST, DOC_PROMOTION, DOC_WARRANTY, DOC_REGULATIONS, DOC_OTHER),
+    'doctype' => array(DOC_CONTRACT, DOC_ANNEX, DOC_PROTOCOL, DOC_ORDER, DOC_SHEET, -6, -7, -8, -9, -99, DOC_PRICE_LIST, DOC_PROMOTION, DOC_WARRANTY, DOC_REGULATIONS, DOC_OTHER),
 )));
 $SMARTY->assign('documentlist', $documentlist);
 $SMARTY->assign('pagination', $pagination);
