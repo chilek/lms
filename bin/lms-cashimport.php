2dd310b7e85e6071f3d3bbc5c3face6a038a1134
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2023-10-19 17:53:00 +0200

    improvement: use configuration section name parameter also for 'import_config' variable

--- bin/lms-cashimport.php
+++ bin/lms-cashimport.php
@@ -183,7 +183,10 @@ if (array_key_exists('import-file', $options)) {
     $filemtime = time();
 }
 
-$import_config = ConfigHelper::getConfig('phpui.import_config', 'cashimportcfg.php');
+$import_config = ConfigHelper::getConfig(
+    (isset($options['section']) ? $config_section : 'phpui') . '.import_config',
+    'cashimportcfg.php'
+);
 if (strpos($import_config, DIRECTORY_SEPARATOR) === false) {
     $import_config = MODULES_DIR . DIRECTORY_SEPARATOR . $import_config;
 }
