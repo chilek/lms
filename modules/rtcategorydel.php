749c92199871e7548048b6a5e86cb31e339921d6
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2023-11-19 23:44:54 +0100

    bugfix: remove SQL errors when ticket was created by userpanel and userpanel default rt category was deleted

--- modules/rtcategorydel.php
+++ modules/rtcategorydel.php
@@ -34,6 +34,19 @@ if (!$LMS->CategoryExists($_GET['id'])) {
     $category = intval($_GET['id']);
 
     $DB->Execute('DELETE FROM rtcategories WHERE id=?', array($category));
+    $userpanel_rtcategories = $DB->GetOne('SELECT value FROM uiconfig WHERE section = \'userpanel\' AND var = \'default_categories\'');
+
+    // Remove userpanel helpdesk default category
+    if (!empty($userpanel_rtcategories)) {
+        $userpanel_rtcategories = explode(',', $userpanel_rtcategories);
+        if (is_array($userpanel_rtcategories) && in_array($category, $userpanel_rtcategories)) {
+                if (($key = array_search($category, $userpanel_rtcategories)) !== false) {
+                    unset($userpanel_rtcategories[$key]);
+                }
+        }
+        $userpanel_rtcategories = implode(',', $userpanel_rtcategories);
+        $DB->Execute('UPDATE uiconfig SET value = ? WHERE section = \'userpanel\' AND var = \'default_categories\'', array($userpanel_rtcategories));
+    }
 
     $SESSION->redirect('?m=rtcategorylist');
 }
