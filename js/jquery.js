2c822273f690ece7505f088375f2b923a643f913
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2024-01-17 11:41:56 +0100

    enhancement: {button data_confirmation_text="some question?"} needs to be confirmed by dialog

--- js/jquery.js
+++ js/jquery.js
@@ -2158,6 +2158,11 @@ $(function() {
 		});
 	});
 
+
+	$('a[data-confirmation-text], button[data-confirmation-text]').click(function() {
+		confirmDialog($(this).attr('data-confirmation-text')).done(function() { location.href = $(this).attr('data-href'); })
+		return false;
+	});
 	window.addEventListener('message', function(e) {
 		if (e.data.hasOwnProperty('targetValue') && e.data.hasOwnProperty('targetSelector')) {
 			var elem = $(e.data.targetSelector);
