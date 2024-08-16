29ee5b680f5842b52874d907c44e8cbc317285bf
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2024-08-16 14:48:14 +0200

    bugfix: uke/pit: validate teryt properties of all network devices not assigned explicitly to network node in single iteration

--- modules/ukepit.php
+++ modules/ukepit.php
@@ -1112,8 +1112,46 @@ if ($report_type == 'full') {
                 $netnodes[$netnodename]['parent_netnodename'] = null;
 
                 $netnodeid++;
-            } elseif (empty($netdevice['passive']) && $netnodes[$netnodename]['mode'] < ELEMENT_MODE_NETWORK_NODE) {
-                $netnodes[$netnodename]['mode'] = ELEMENT_MODE_NETWORK_NODE;
+            } else {
+                if (empty($netdevice['passive']) && $netnodes[$netnodename]['mode'] < ELEMENT_MODE_NETWORK_NODE) {
+                    $netnodes[$netnodename]['mode'] = ELEMENT_MODE_NETWORK_NODE;
+                }
+
+                if (isset($teryt_cities[$netdevice['location_city']])) {
+                    $teryt_city = $teryt_cities[$netdevice['location_city']];
+
+                    if (!empty($netdevice['location_street']) && isset($teryt_streets[$netdevice['location_street']])) {
+                        $teryt_street = $teryt_streets[$netdevice['location_street']];
+                    }
+
+                    if (!strlen($teryt_city['area_terc']) || !strlen($teryt_city['area_simc']) || !strlen($netdevice['location_house'])) {
+                        $error = array(
+                            'id' => $netdevice['id'],
+                            'name' => $netdevice['name'],
+                        );
+                        if (!strlen($teryt_city['area_terc'])) {
+                            $error['terc'] = true;
+                        }
+                        if (!strlen($teryt_city['area_simc'])) {
+                            $error['simc'] = true;
+                        }
+                        if (!strlen($netdevice['location_house'])) {
+                            $error['location_house'] = true;
+                        }
+                        $errors['netdevices'][] = $error;
+                    }
+                } else {
+                    $error = array(
+                        'id' => $netdevice['id'],
+                        'name' => $netdevice['name'],
+                        'terc' => true,
+                        'simc' => true,
+                    );
+                    if (!isset($netdevice['location_house']) || !strlen($netdevice['location_house'])) {
+                        $error['location_house'] = true;
+                    }
+                    $errors['netdevices'][] = $error;
+                }
             }
 
             $netdevice['ownership'] = $netnodes[$netnodename]['ownership'];
