1544579d4ecfe485e29834ae8e77ad113ed2e5b2
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2025-05-14 09:09:00 +0200

    bugfix: restored PSR-2 compatibility

--- lib/Google2FA/Google2FA.php
+++ lib/Google2FA/Google2FA.php
@@ -7,7 +7,7 @@ use PragmaRX\Google2FA\Exceptions\SecretKeyTooShortException;
 use PragmaRX\Google2FA\Support\Base32;
 use PragmaRX\Google2FA\Support\Constants;
 use PragmaRX\Google2FA\Support\QRCode;
-use PragmaRX\Google2FA\Google2FA AS PragmaRXGoogle2FA;
+use PragmaRX\Google2FA\Google2FA as PragmaRXGoogle2FA;
 
 class Google2FA extends PragmaRXGoogle2FA
 {
