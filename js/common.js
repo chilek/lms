d1b3fbff74121dfc5319890abe6745defa18dc31
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2026-01-14 14:03:01 +0100

    bugfix: KSeF: dedicated recipient TEN can be set for invoices and correction notes in customer location addresses in format compatible with KSeF internal ID (NIP-NNNNN)

--- js/common.js
+++ js/common.js
@@ -268,6 +268,7 @@ if ( typeof $ !== 'undefined' ) {
             if ( $( this ).is(':checked') ) {
                 $("#" + boxid + " input[type=text]").prop("readonly", true);
                 $("#" + boxid).find("input[data-address='zip']").attr('readonly', false);
+                $("#" + boxid).find("input[data-address='ten']").attr('readonly', false);
                 $("#" + boxid).find("input[data-address='postoffice']").attr('readonly', false);
                 $("#" + boxid).find("input[data-address='location-name']").attr('readonly', false);
                 $("#" + boxid).find("select[data-address='state-select']").css('display', 'none').attr('disabled', true);
