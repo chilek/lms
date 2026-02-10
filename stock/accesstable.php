12a9e75c5331bc02423ac807b1fd24af751dd8d7
SarenkaGD <sarenka@mojasiec.com>
2026-02-10 15:11:10 +0100

    Modernize accesstable

--- stock/accesstable.php
+++ stock/accesstable.php
@@ -1,18 +1,31 @@
 <?php
-$access_table['lms-stck-full'] = array(
+$access_table_stck['lms-stck-full'] = array(
 	'label' => trans('full access to lms-stck module'),
 	'allow_regexp' => '^(stck)[a-z]+$'
 	);
 
-$access_table['lms-stck-reports'] = array(
+$access_table_stck['lms-stck-reports'] = array(
 	'label' => trans('lms-stck reports'),
 	'allow_regexp' => '^printstock$'
 	);
 
-$access_table['lms-stck-xml'] = array(
+$access_table_stck['lms-stck-xml'] = array(
 	'label' => trans('lms-stck XML stock wholesale reports'),
 	'allow_regexp' => '^stckxml$'
 	);
+
+$access = AccessRights::getInstance();
+foreach ($access_table_stck as $name => $permission) {
+    $access->appendPermission(new Permission(
+        $name,
+        $permission['label'],
+        $permission['allow_regexp'] ?? null,
+        $permission['deny_regexp'] ?? null,
+        $permission['allow_menu_items'] ?? null,
+        $permission['deny_menu_items'] ?? null
+    ));
+}
+
 
 /*$access['table'][100]['name']      = 'magazyn';
 $access['table'][100]['allow_reg'] = '^(stck)[a-z]+$';
