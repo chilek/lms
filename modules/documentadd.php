570f62896eefa1e8b8c70e85f0a860e9a2c094cd
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2026-02-20 20:22:56 +0100

    devel/improvement: 'document.disable_cdate_validation' disables creation date validation and is useful for developer use

--- modules/documentadd.php
+++ modules/documentadd.php
@@ -89,7 +89,7 @@ if (isset($_POST['document'])) {
     }
 
     if (ConfigHelper::checkPrivilege('document_consent_date') && $document['cdate'] && !isset($warnings['document-cdate-'])) {
-        if ($document['type']) {
+        if (!ConfigHelper::checkConfig('documents.disable_cdate_validation') && $document['type']) {
             if (empty($document['numberplanid'])) {
                 $maxdate = $DB->GetOne(
                     'SELECT MAX(cdate) FROM documents WHERE type = ? AND numberplanid IS NULL',
