bc0a16082da9de67ee0215aa98be59fc8fae2305
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2026-04-10 15:52:19 +0200

    improvement: KSeF: add bank name in output xml if bank name is defined for division

--- lib/KSeF/KSeF.php
+++ lib/KSeF/KSeF.php
@@ -1081,18 +1081,28 @@ class KSeF
             $xml .= "\t\t\t<FormaPlatnosci>" . $this->payTypes[$invoice['paytype']] . "</FormaPlatnosci>" . PHP_EOL;
         }
 
+        $bank = null;
         if (!$this->showOnlyAlternativeAccounts || empty($invoice['bankaccounts'])) {
             $accounts = array(bankaccount($invoice['customerid'], $invoice['account'], $invoice['export']));
+            if (!empty($invoice['division_bank'])) {
+                $bank = $invoice['division_bank'];
+            }
         } else {
             $accounts = array();
         }
         if ($this->showAllAccounts || $this->showOnlyAlternativeAccounts) {
             $accounts = array_merge($accounts, $invoice['bankaccounts']);
+            if (!empty($invoice['bankaccounts'])) {
+                $bank = null;
+            }
         }
         foreach ($accounts as $account) {
             $xml .= "\t\t\t<RachunekBankowy>" . PHP_EOL;
             //$xml .= "\t\t\t\t<NrRB>". format_bankaccount($account, $invoice['export']) . "</NrRB>" . PHP_EOL;
             $xml .= "\t\t\t\t<NrRB>". $account . "</NrRB>" . PHP_EOL;
+            if (isset($bank)) {
+                $xml .= "\t\t\t\t<NazwaBanku>" . htmlspecialchars($bank) . "</NazwaBanku>" . PHP_EOL;
+            }
             $xml .= "\t\t\t</RachunekBankowy>" . PHP_EOL;
         }
         unset($account);
